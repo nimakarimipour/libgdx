8dbc6d1274f7238af246fbe12273b7a3d4067205
nimakarimipour <karimipour.nima@gmail.com>
2022-03-29 05:08:00 -0700

    Final Result

--- gdx/src/com/badlogic/gdx/graphics/glutils/MipMapTextureData.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/MipMapTextureData.java
@@ -1,74 +1,82 @@
 package com.badlogic.gdx.graphics.glutils;
 
+import javax.annotation.Nullable;
 import com.badlogic.gdx.graphics.GLTexture;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.TextureData;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 
-/** This class will load each contained TextureData to the chosen mipmap level.
- *  All the mipmap levels must be defined and cannot be null. */
-public class MipMapTextureData implements TextureData{
-	TextureData[] mips;
-	
-	/** @param mipMapData must be != null and its length must be >= 1 */
-	public MipMapTextureData(TextureData... mipMapData){
-		mips = new TextureData[mipMapData.length];
-		System.arraycopy(mipMapData, 0, mips, 0, mipMapData.length);
-	}
-	
-	@Override
-	public TextureDataType getType () {
-		return TextureDataType.Custom;
-	}
+/**
+ * This class will load each contained TextureData to the chosen mipmap level.
+ *  All the mipmap levels must be defined and cannot be null.
+ */
+public class MipMapTextureData implements TextureData {
 
-	@Override
-	public boolean isPrepared () {
-		return true;
-	}
+    TextureData[] mips;
 
-	@Override
-	public void prepare () {}
+    /**
+     * @param mipMapData must be != null and its length must be >= 1
+     */
+    public MipMapTextureData(TextureData... mipMapData) {
+        mips = new TextureData[mipMapData.length];
+        System.arraycopy(mipMapData, 0, mips, 0, mipMapData.length);
+    }
 
-	@Override
-	public Pixmap consumePixmap () {
-		throw new GdxRuntimeException("It's compressed, use the compressed method");
-	}
+    @Override
+    public TextureDataType getType() {
+        return TextureDataType.Custom;
+    }
 
-	@Override
-	public boolean disposePixmap () {
-		return false;
-	}
+    @Override
+    public boolean isPrepared() {
+        return true;
+    }
 
-	@Override
-	public void consumeCustomData (int target) {
-		for(int i=0; i < mips.length; ++i){
-			GLTexture.uploadImageData(target, mips[i], i);
-		}
-	}
+    @Override
+    public void prepare() {
+    }
 
-	@Override
-	public int getWidth () {
-		return mips[0].getWidth();
-	}
+    @Override
+    public Pixmap consumePixmap() {
+        throw new GdxRuntimeException("It's compressed, use the compressed method");
+    }
 
-	@Override
-	public int getHeight () {
-		return mips[0].getHeight();
-	}
+    @Override
+    public boolean disposePixmap() {
+        return false;
+    }
 
-	@Override
-	public Format getFormat () {
-		return mips[0].getFormat();
-	}
+    @Override
+    public void consumeCustomData(int target) {
+        for (int i = 0; i < mips.length; ++i) {
+            GLTexture.uploadImageData(target, mips[i], i);
+        }
+    }
 
-	@Override
-	public boolean useMipMaps () {
-		return false;
-	}
+    @Override
+    public int getWidth() {
+        return mips[0].getWidth();
+    }
 
-	@Override
-	public boolean isManaged () {
-		return true;
-	}
+    @Override
+    public int getHeight() {
+        return mips[0].getHeight();
+    }
+
+    @Override
+    @Nullable
+    public Format getFormat() {
+        return mips[0].getFormat();
+    }
+
+    @Override
+    public boolean useMipMaps() {
+        return false;
+    }
+
+    @Override
+    public boolean isManaged() {
+        return true;
+    }
 }
