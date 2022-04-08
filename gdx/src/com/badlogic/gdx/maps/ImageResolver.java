88a387be3d806cae93f5050f8ec0297c1abed699
nimakarimipour <karimipour.nima@gmail.com>
2022-04-07 22:20:28 -0700

    Final Result

--- gdx/src/com/badlogic/gdx/maps/ImageResolver.java
+++ gdx/src/com/badlogic/gdx/maps/ImageResolver.java
@@ -1,70 +1,80 @@
-/*******************************************************************************
- * Copyright 2011 See AUTHORS file.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
-
+/**
+ * ****************************************************************************
+ *  Copyright 2011 See AUTHORS file.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ****************************************************************************
+ */
 package com.badlogic.gdx.maps;
 
+import javax.annotation.Nullable;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ObjectMap;
 
-/** Resolves an image by a string, wrapper around a Map or AssetManager to load maps either directly or via AssetManager.
- * @author mzechner */
+/**
+ * Resolves an image by a string, wrapper around a Map or AssetManager to load maps either directly or via AssetManager.
+ * @author mzechner
+ */
 public interface ImageResolver {
-	/** @param name
-	 * @return the Texture for the given image name or null. */
-	public TextureRegion getImage (String name);
 
-	public static class DirectImageResolver implements ImageResolver {
-		private final ObjectMap<String, Texture> images;
+    /**
+     * @param name
+     * @return the Texture for the given image name or null.
+     */
+    public TextureRegion getImage(String name);
 
-		public DirectImageResolver (ObjectMap<String, Texture> images) {
-			this.images = images;
-		}
+    public static class DirectImageResolver implements ImageResolver {
 
-		@Override
-		public TextureRegion getImage (String name) {
-			return new TextureRegion(images.get(name));
-		}
-	}
+        private final ObjectMap<String, Texture> images;
 
-	public static class AssetManagerImageResolver implements ImageResolver {
-		private final AssetManager assetManager;
+        public DirectImageResolver(ObjectMap<String, Texture> images) {
+            this.images = images;
+        }
 
-		public AssetManagerImageResolver (AssetManager assetManager) {
-			this.assetManager = assetManager;
-		}
+        @Override
+        public TextureRegion getImage(String name) {
+            return new TextureRegion(images.get(name));
+        }
+    }
 
-		@Override
-		public TextureRegion getImage (String name) {
-			return new TextureRegion(assetManager.get(name, Texture.class));
-		}
-	}
+    public static class AssetManagerImageResolver implements ImageResolver {
 
-	public static class TextureAtlasImageResolver implements ImageResolver {
-		private final TextureAtlas atlas;
+        private final AssetManager assetManager;
 
-		public TextureAtlasImageResolver (TextureAtlas atlas) {
-			this.atlas = atlas;
-		}
+        public AssetManagerImageResolver(AssetManager assetManager) {
+            this.assetManager = assetManager;
+        }
 
-		@Override
-		public TextureRegion getImage (String name) {
-			return atlas.findRegion(name);
-		}
-	}
+        @Override
+        public TextureRegion getImage(String name) {
+            return new TextureRegion(assetManager.get(name, Texture.class));
+        }
+    }
+
+    public static class TextureAtlasImageResolver implements ImageResolver {
+
+        private final TextureAtlas atlas;
+
+        public TextureAtlasImageResolver(TextureAtlas atlas) {
+            this.atlas = atlas;
+        }
+
+        @Override
+        public TextureRegion getImage(String name) {
+            return atlas.findRegion(name);
+        }
+    }
 }
