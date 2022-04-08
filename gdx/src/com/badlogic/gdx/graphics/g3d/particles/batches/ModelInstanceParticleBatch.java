c6f3161eb3a2ecf973fa14687db6543ce03687c4
nimakarimipour <karimipour.nima@gmail.com>
2022-04-08 04:04:13 -0700

    Final Result

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/ModelInstanceParticleBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/ModelInstanceParticleBatch.java
@@ -1,19 +1,20 @@
-/*******************************************************************************
- * Copyright 2011 See AUTHORS file.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
-
+/**
+ * ****************************************************************************
+ *  Copyright 2011 See AUTHORS file.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ****************************************************************************
+ */
 package com.badlogic.gdx.graphics.g3d.particles.batches;
 
 import com.badlogic.gdx.assets.AssetManager;
@@ -23,50 +24,54 @@ import com.badlogic.gdx.graphics.g3d.particles.renderers.ModelInstanceController
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
 
-/*** This class is used to render particles having a model instance channel.
- * @author Inferno */
+/**
+ * This class is used to render particles having a model instance channel.
+ * @author Inferno
+ */
 public class ModelInstanceParticleBatch implements ParticleBatch<ModelInstanceControllerRenderData> {
-	Array<ModelInstanceControllerRenderData> controllersRenderData;
-	int bufferedParticlesCount;
 
-	public ModelInstanceParticleBatch () {
-		controllersRenderData = new Array<ModelInstanceControllerRenderData>(false, 5);
-	}
+    Array<ModelInstanceControllerRenderData> controllersRenderData;
 
-	@Override
-	public void getRenderables (Array<Renderable> renderables, Pool<Renderable> pool) {
-		for (ModelInstanceControllerRenderData data : controllersRenderData) {
-			for (int i = 0, count = data.controller.particles.size; i < count; ++i) {
-				data.modelInstanceChannel.data[i].getRenderables(renderables, pool);
-			}
-		}
-	}
+    int bufferedParticlesCount;
 
-	public int getBufferedCount () {
-		return bufferedParticlesCount;
-	}
+    public ModelInstanceParticleBatch() {
+        controllersRenderData = new Array<ModelInstanceControllerRenderData>(false, 5);
+    }
 
-	@Override
-	public void begin () {
-		controllersRenderData.clear();
-		bufferedParticlesCount = 0;
-	}
+    @Override
+    public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool) {
+        for (ModelInstanceControllerRenderData data : controllersRenderData) {
+            for (int i = 0, count = data.controller.particles.size; i < count; ++i) {
+                data.modelInstanceChannel.data[i].getRenderables(renderables, pool);
+            }
+        }
+    }
 
-	@Override
-	public void end () {
-	}
+    public int getBufferedCount() {
+        return bufferedParticlesCount;
+    }
 
-	@Override
-	public void draw (ModelInstanceControllerRenderData data) {
-		controllersRenderData.add(data);
-		bufferedParticlesCount += data.controller.particles.size;
-	}
+    @Override
+    public void begin() {
+        controllersRenderData.clear();
+        bufferedParticlesCount = 0;
+    }
 
-	@Override
-	public void save (AssetManager manager, ResourceData assetDependencyData) {
-	}
+    @Override
+    public void end() {
+    }
 
-	@Override
-	public void load (AssetManager manager, ResourceData assetDependencyData) {
-	}
+    @Override
+    public void draw(ModelInstanceControllerRenderData data) {
+        controllersRenderData.add(data);
+        bufferedParticlesCount += data.controller.particles.size;
+    }
+
+    @Override
+    public void save(AssetManager manager, ResourceData assetDependencyData) {
+    }
+
+    @Override
+    public void load(AssetManager manager, ResourceData assetDependencyData) {
+    }
 }
