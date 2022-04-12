7209405358801634bf476883f5f8a64ebfa9e008
nimakarimipour <karimipour.nima@gmail.com>
2022-04-12 13:18:29 -0700

    Final Result

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultShaderProvider.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultShaderProvider.java
@@ -1,21 +1,24 @@
-/*******************************************************************************
- * Copyright 2011 See AUTHORS file.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
-
+/**
+ * ****************************************************************************
+ *  Copyright 2011 See AUTHORS file.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ****************************************************************************
+ */
 package com.badlogic.gdx.graphics.g3d.utils;
 
+import androidx.annotation.Nullable;
+import javax.annotation.Nullable;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g3d.Renderable;
@@ -23,26 +26,27 @@ import com.badlogic.gdx.graphics.g3d.Shader;
 import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
 
 public class DefaultShaderProvider extends BaseShaderProvider {
-	public final DefaultShader.Config config;
 
-	public DefaultShaderProvider (final DefaultShader.Config config) {
-		this.config = (config == null) ? new DefaultShader.Config() : config;
-	}
+    public final DefaultShader.Config config;
 
-	public DefaultShaderProvider (final String vertexShader, final String fragmentShader) {
-		this(new DefaultShader.Config(vertexShader, fragmentShader));
-	}
+    public DefaultShaderProvider(@Nullable final DefaultShader.Config config) {
+        this.config = (config == null) ? new DefaultShader.Config() : config;
+    }
 
-	public DefaultShaderProvider (final FileHandle vertexShader, final FileHandle fragmentShader) {
-		this(vertexShader.readString(), fragmentShader.readString());
-	}
+    public DefaultShaderProvider(final String vertexShader, final String fragmentShader) {
+        this(new DefaultShader.Config(vertexShader, fragmentShader));
+    }
 
-	public DefaultShaderProvider () {
-		this(null);
-	}
+    public DefaultShaderProvider(final FileHandle vertexShader, final FileHandle fragmentShader) {
+        this(vertexShader.readString(), fragmentShader.readString());
+    }
 
-	@Override
-	protected Shader createShader (final Renderable renderable) {
-		return new DefaultShader(renderable, config);
-	}
+    public DefaultShaderProvider() {
+        this(null);
+    }
+
+    @Override
+    protected Shader createShader(final Renderable renderable) {
+        return new DefaultShader(renderable, config);
+    }
 }
