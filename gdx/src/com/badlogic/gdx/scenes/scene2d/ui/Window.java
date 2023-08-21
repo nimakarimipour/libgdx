834d6cb8569ee95e639511d3d7d51486bbd7afc9
nimakarimipour <karimipour.nima@gmail.com>
2023-08-21 02:01:32 -0700

    update

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
@@ -260,7 +260,6 @@ public class Window extends Table {
 	protected void drawStageBackground (Batch batch, float parentAlpha, float x, float y, float width, float height) {
 		Color color = getColor();
 		batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
-		style.stageBackground.draw(batch, x, y, width, height);
 	}
 
 	protected void drawBackground (Batch batch, float parentAlpha, float x, float y) {
--- gdx/src/com/badlogic/gdx/utils/OrderedSet.java
+++ gdx/src/com/badlogic/gdx/utils/OrderedSet.java
@@ -144,21 +144,7 @@ public class OrderedSet<T> extends ObjectSet<T> {
 	}
 
 	public OrderedSetIterator<T> iterator () {
-		if (Collections.allocateIterators) return new OrderedSetIterator(this);
-		if (iterator1 == null) {
-			iterator1 = new OrderedSetIterator(this);
-			iterator2 = new OrderedSetIterator(this);
-		}
-		if (!iterator1.valid) {
-			iterator1.reset();
-			iterator1.valid = true;
-			iterator2.valid = false;
-			return iterator1;
-		}
-		iterator2.reset();
-		iterator2.valid = true;
-		iterator1.valid = false;
-		return iterator2;
+		return new OrderedSetIterator(this);
 	}
 
 	public String toString () {
