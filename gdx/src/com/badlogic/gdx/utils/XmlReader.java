aec750a05d4f5f9a2ff1ea0b7d6cf1c3f1f9d934
GitHub Action <action@github.com>
2023-08-21 09:08:25 +0000

    Apply formatter

--- gdx/src/com/badlogic/gdx/utils/XmlReader.java
+++ gdx/src/com/badlogic/gdx/utils/XmlReader.java
@@ -448,7 +448,7 @@ public class XmlReader {
 	}
 
 	protected void text (String text) {
-		if(current != null){
+		if (current != null) {
 			String existing = current.getText();
 			current.setText(existing != null ? existing + text : text);
 		}
