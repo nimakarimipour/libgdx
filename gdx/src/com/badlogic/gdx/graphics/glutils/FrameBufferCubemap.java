56e7dc58a053d5ba8d2d1df03e1d1d403da27c92
GitHub Action <action@github.com>
2023-08-21 08:48:07 +0000

    Apply formatter

--- gdx/src/com/badlogic/gdx/graphics/glutils/FrameBufferCubemap.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/FrameBufferCubemap.java
@@ -157,7 +157,7 @@ public class FrameBufferCubemap extends GLFrameBuffer<Cubemap> {
 	/** Bind the side, making it active to render on. Should be called in between a call to {@link #begin()} and {@link #end()}.
 	 * @param side The side to bind */
 	protected void bindSide (final Cubemap.CubemapSide side) {
-		if(side == null) {
+		if (side == null) {
 			throw new GdxRuntimeException("Cannot bind cubemap side to null.");
 		}
 		Gdx.gl20.glFramebufferTexture2D(GL20.GL_FRAMEBUFFER, GL20.GL_COLOR_ATTACHMENT0, side.glEnum,
