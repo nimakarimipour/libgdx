6ab47485dce4f6d8e0b339af290f3e4bbf1272e9
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:40:06 -0700

    Revert "fix: 222 - DEREFERENCE_NULLABLE - return new NetJavaSocketImpl(server.accept(), hints); - dereferenced expression server is @Nullable"
    This reverts commit 021b3dcb6c6ba2b1338e78a4332bcaf451239579.

--- gdx/src/com/badlogic/gdx/net/NetJavaServerSocketImpl.java
+++ gdx/src/com/badlogic/gdx/net/NetJavaServerSocketImpl.java
@@ -80,9 +80,6 @@ public class NetJavaServerSocketImpl implements ServerSocket {
 
   @Override
   public Socket accept(SocketHints hints) {
-    if (server == null) {
-      throw new GdxRuntimeException("Server socket is not initialized.");
-    }
     try {
       return new NetJavaSocketImpl(server.accept(), hints);
     } catch (Exception e) {
