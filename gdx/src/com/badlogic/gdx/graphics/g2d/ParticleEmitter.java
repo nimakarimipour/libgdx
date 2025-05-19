5785d17481689c770c864c9c4ccaa3490dfcb606
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:17:33 -0700

    Revert "fix: 82 - DEREFERENCE_NULLABLE - particle.setOrigin(sprite.getOriginX(), sprite.getOriginY()); - dereferenced expression sprite is @Nullable"
    This reverts commit 5c89c5f86fe230459666f2d325bc6efcdc4a73ac.

--- gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
@@ -687,7 +687,7 @@ public class ParticleEmitter {
 
   public void setSprites(Array<Sprite> sprites) {
     this.sprites = sprites;
-    if (sprites == null || sprites.size == 0) return;
+    if (sprites.size == 0) return;
     for (int i = 0, n = particles.length; i < n; i++) {
       Particle particle = particles[i];
       if (particle == null) break;
@@ -705,10 +705,8 @@ public class ParticleEmitter {
           sprite = sprites.get(particle.frame);
           break;
       }
-      if (sprite != null) {
-        particle.setRegion(sprite);
-        particle.setOrigin(sprite.getOriginX(), sprite.getOriginY());
-      }
+      particle.setRegion(sprite);
+      particle.setOrigin(sprite.getOriginX(), sprite.getOriginY());
     }
   }
 
