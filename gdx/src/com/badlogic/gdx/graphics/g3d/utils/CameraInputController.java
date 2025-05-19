e2b0c0603b21b41d890e150d27737cfb8647bcec
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:37:10 -0700

    Revert "fix: 192 - DEREFERENCE_NULLABLE - return controller.pinchZoom(amount / ((w > h) ? h : w)); - dereferenced expression controller is @Nullable"
    This reverts commit 0959733ee308208d9260134905531fd5819f8194.

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/CameraInputController.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/CameraInputController.java
@@ -130,9 +130,6 @@ public class CameraInputController extends GestureDetector {
 
     @Override
     public boolean zoom(float initialDistance, float distance) {
-      if (controller == null) {
-        return false;
-      }
       float newZoom = distance - initialDistance;
       float amount = newZoom - previousZoom;
       previousZoom = newZoom;
