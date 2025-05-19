c99583ee22de58d395d41065367f1fe657ed736f
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 16:12:24 -0700

    fix: 451 - ASSIGN_FIELD_NULLABLE - this.program = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleShader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleShader.java
@@ -303,7 +303,6 @@ public class ParticleShader extends BaseShader {
   @Override
   public void init() {
     final ShaderProgram program = this.program;
-    this.program = null;
     init(program, renderable);
     renderable = null;
   }
