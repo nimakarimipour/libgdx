58a3da0ecdd4b03185cc8904fa1770d3e762d952
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:09:00 -0700

    Revert "fix: 15 - RETURN_NULLABLE - return (T) actor; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit b1f4736e839005ab415e6bf6314a0865557951e0.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -78,7 +78,7 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   /** Returns the actor for this cell, or null. */
-  public @Nullable T getActor() {
+  public @Null T getActor() {
     return (T) actor;
   }
 
