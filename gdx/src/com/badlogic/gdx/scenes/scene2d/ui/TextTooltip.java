8815418cd13c1ca5fb01ea4c60e4d9ce817f1d5a
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:26:57 -0700

    fix: 116 - PASS_NULLABLE - label.setStyle(style.label); - passing @Nullable parameter 'style.label' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java
@@ -62,6 +62,7 @@ public class TextTooltip extends Tooltip<Label> {
 
   public void setStyle(TextTooltipStyle style) {
     if (style == null) throw new NullPointerException("style cannot be null");
+    if (style.label == null) throw new NullPointerException("style.label cannot be null");
     container.setBackground(style.background);
     container.maxWidth(style.wrapWidth);
 
