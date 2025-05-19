b036f85fb15cc9d9171891a017295cce94090ae1
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:29:13 -0700

    Revert "fix: 134 - DEREFERENCE_NULLABLE - if (current.prev == null) { - dereferenced expression current is @Nullable"
    This reverts commit 06c54c1f2ba39660afa5d22d6e202c47cefb3a50.

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -1469,9 +1469,6 @@ public class JsonValue implements Iterable<JsonValue> {
     }
 
     public void remove() {
-      if (current == null) {
-        throw new IllegalStateException("No current element to remove");
-      }
       if (current.prev == null) {
         child = current.next;
         if (child != null) child.prev = null;
@@ -1480,7 +1477,6 @@ public class JsonValue implements Iterable<JsonValue> {
         if (current.next != null) current.next.prev = current.prev;
       }
       size--;
-      current = null; // Reset current after removal
     }
 
     public Iterator<JsonValue> iterator() {
