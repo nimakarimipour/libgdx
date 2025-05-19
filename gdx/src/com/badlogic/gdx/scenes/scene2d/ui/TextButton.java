d26a6d076afa26699630e3253497df34de06310d
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 16:10:04 -0700

    Revert "fix: 433 - METHOD_NO_INIT - public TextButton(@Null String text, TextButtonStyle style) { - initializer method does not guarantee @NonNull field style (line 35) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 15a37f00b406b97417dd37148cb6131c94e13116.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextButton.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextButton.java
@@ -46,7 +46,6 @@ public class TextButton extends Button {
 
   public TextButton(@Null String text, TextButtonStyle style) {
     super();
-    if (style == null) throw new NullPointerException("style cannot be null");
     setStyle(style);
     label = newLabel(text, new LabelStyle(style.font, style.fontColor));
     label.setAlignment(Align.center);
