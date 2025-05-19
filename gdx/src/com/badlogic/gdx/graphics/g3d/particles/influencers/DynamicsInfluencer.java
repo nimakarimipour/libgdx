159c6aea5f14489f446a811642311be6d7be326f
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:09:17 -0700

    Revert "fix: 17 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull fields positionChannel (line 36), previousPositionChannel (line 36), rotationChannel (line 36) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 4e22799e5261101de8a9c7b4a8698f847482b946.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsInfluencer.java
@@ -62,16 +62,16 @@ public class DynamicsInfluencer extends Influencer {
       velocities.items[k].allocateChannels();
     }
 
+    // Hack, shouldn't be done but after all the modifiers allocated their channels
+    // it's possible to check if we need to allocate previous position channel
     accellerationChannel = controller.particles.getChannel(ParticleChannels.Acceleration);
     hasAcceleration = accellerationChannel != null;
     if (hasAcceleration) {
       positionChannel = controller.particles.addChannel(ParticleChannels.Position);
       previousPositionChannel = controller.particles.addChannel(ParticleChannels.PreviousPosition);
-    } else {
-      positionChannel = controller.particles.addChannel(ParticleChannels.Position);
-      previousPositionChannel = controller.particles.addChannel(ParticleChannels.PreviousPosition);
     }
 
+    // Angular velocity check
     angularVelocityChannel = controller.particles.getChannel(ParticleChannels.AngularVelocity2D);
     has2dAngularVelocity = angularVelocityChannel != null;
     if (has2dAngularVelocity) {
@@ -80,11 +80,8 @@ public class DynamicsInfluencer extends Influencer {
     } else {
       angularVelocityChannel = controller.particles.getChannel(ParticleChannels.AngularVelocity3D);
       has3dAngularVelocity = angularVelocityChannel != null;
-      if (has3dAngularVelocity) {
+      if (has3dAngularVelocity)
         rotationChannel = controller.particles.addChannel(ParticleChannels.Rotation3D);
-      } else {
-        rotationChannel = controller.particles.addChannel(ParticleChannels.Rotation2D);
-      }
     }
   }
 
