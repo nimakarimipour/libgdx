726d264587c4122615423ca24dc467e2aeb321c1
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:54:30 -0700

    fix: 331 - ASSIGN_FIELD_NULLABLE - atlasRegion.name = region.name; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
@@ -114,7 +114,12 @@ public class TextureAtlas implements Disposable {
               region.rotate ? region.height : region.width, //
               region.rotate ? region.width : region.height);
       atlasRegion.index = region.index;
-      atlasRegion.name = region.name;
+      if (region.name != null) {
+        atlasRegion.name = region.name;
+      } else {
+        // Provide a default name or handle the situation where region.name is null
+        atlasRegion.name = "defaultName";
+      }
       atlasRegion.offsetX = region.offsetX;
       atlasRegion.offsetY = region.offsetY;
       atlasRegion.originalHeight = region.originalHeight;
