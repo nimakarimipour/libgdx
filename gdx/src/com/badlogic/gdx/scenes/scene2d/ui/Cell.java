a5f2256abed3e46a00b207c0580e17b4e97e650c
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 16:06:12 -0700

    fix: 402 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull field table (line 45) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -1017,7 +1017,10 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   @Initializer
-  void set(@Nullable Cell cell) {
+  void set(Cell cell) {
+    if (cell == null) {
+      throw new IllegalArgumentException("cell cannot be null");
+    }
     minWidth = cell.minWidth;
     minHeight = cell.minHeight;
     prefWidth = cell.prefWidth;
@@ -1040,6 +1043,7 @@ public class Cell<T extends Actor> implements Poolable {
     colspan = cell.colspan;
     uniformX = cell.uniformX;
     uniformY = cell.uniformY;
+    table = cell.table;
   }
 
   void merge(@Nullable @Null Cell cell) {
