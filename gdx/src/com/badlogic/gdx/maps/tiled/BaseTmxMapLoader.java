dbc821e2e5636405fab35d5a3e743662c843681a
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:41:37 -0700

    Revert "fix: 235 - METHOD_NO_INIT - public BaseTmxMapLoader(FileHandleResolver resolver) { - initializer method does not guarantee @NonNull fields root (line 66), map (line 75) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 377e0f412b89be680cff60a07380c1fbe035373f.

--- gdx/src/com/badlogic/gdx/maps/tiled/BaseTmxMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/BaseTmxMapLoader.java
@@ -76,8 +76,6 @@ public abstract class BaseTmxMapLoader<P extends BaseTmxMapLoader.Parameters>
 
   public BaseTmxMapLoader(FileHandleResolver resolver) {
     super(resolver);
-    this.root = new Element("root", null);
-    this.map = new TiledMap();
   }
 
   @Override
