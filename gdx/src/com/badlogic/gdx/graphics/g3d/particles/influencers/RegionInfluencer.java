ec9f4f1e0ea23b05f2e074312aa2d372c31eff83
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:28:33 -0700

    fix: 130 - METHOD_NO_INIT - public RegionInfluencer(int regionsCount) { - initializer method does not guarantee @NonNull field regionChannel (line 216) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/RegionInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/RegionInfluencer.java
@@ -218,6 +218,8 @@ public abstract class RegionInfluencer extends Influencer {
 
   public RegionInfluencer(int regionsCount) {
     this.regions = new Array<AspectTextureRegion>(false, regionsCount, AspectTextureRegion.class);
+    this.regionChannel =
+        new FloatChannel(); // or an appropriate initialization based on your code context
   }
 
   public RegionInfluencer() {
