a9d511ad6e1d8e77d9aa24c4ff06feb21550519b
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:41:43 -0700

    fix: 236 - DEREFERENCE_NULLABLE - setMinWidth(sprite.getWidth()); - dereferenced expression sprite is @Nullable

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java
@@ -80,10 +80,12 @@ public class SpriteDrawable extends BaseDrawable implements TransformDrawable {
     sprite.setPackedColor(oldColor);
   }
 
-  public void setSprite(@Nullable Sprite sprite) {
-    this.sprite = sprite;
-    setMinWidth(sprite.getWidth());
-    setMinHeight(sprite.getHeight());
+  public void setSprite(Sprite sprite) {
+    if (sprite != null) {
+      this.sprite = sprite;
+      setMinWidth(sprite.getWidth());
+      setMinHeight(sprite.getHeight());
+    }
   }
 
   @Nullable
