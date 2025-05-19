4bce1d95654e1e9c8d4de971965727ec4d366bf7
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:32:24 -0700

    Revert "fix: 155 - PASS_NULLABLE - return maxWidth.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required"
    This reverts commit 11f67a46954917897c4ef2475cd227aa12c4d268.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -796,9 +796,6 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getMaxWidth() {
-    if (actor == null) {
-      throw new IllegalArgumentException("Actor cannot be null");
-    }
     return maxWidth.get(actor);
   }
 
