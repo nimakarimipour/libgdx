d387bb0def3957bb4b240f4def4307ac507e5093
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:12:45 -0700

    fix: 42 - METHOD_NO_INIT - public GLFrameBufferBuilder(int width, int height) { - initializer method does not guarantee @NonNull fields stencilRenderBufferSpec (line 525), depthRenderBufferSpec (line 526), packedStencilDepthRenderBufferSpec (line 527) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/glutils/GLFrameBuffer.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/GLFrameBuffer.java
@@ -533,6 +533,9 @@ public abstract class GLFrameBuffer<T extends GLTexture> implements Disposable {
     public GLFrameBufferBuilder(int width, int height) {
       this.width = width;
       this.height = height;
+      this.stencilRenderBufferSpec = new FrameBufferRenderBufferAttachmentSpec(0);
+      this.depthRenderBufferSpec = new FrameBufferRenderBufferAttachmentSpec(0);
+      this.packedStencilDepthRenderBufferSpec = new FrameBufferRenderBufferAttachmentSpec(0);
     }
 
     public GLFrameBufferBuilder<U> addColorTextureAttachment(
