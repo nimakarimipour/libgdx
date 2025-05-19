7f91df307566be5d6000d3dc34d5ab64eff17079
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:30:01 -0700

    Revert "fix: 139 - ASSIGN_FIELD_NULLABLE - padRight = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit b614047c85c54e27a1cbf07075a67400b9ad16da.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -993,7 +993,7 @@ public class Cell<T extends Actor> implements Poolable {
     padTop = null;
     padLeft = null;
     padBottom = null;
-    padRight = 0; // Set to a non-null default value instead of null
+    padRight = null;
     fillX = null;
     fillY = null;
     align = null;
