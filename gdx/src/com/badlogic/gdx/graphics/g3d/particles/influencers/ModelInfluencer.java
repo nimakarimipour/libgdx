1310c11e53fd53feaa3474440aee04371f61273d
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:14:32 -0700

    fix: 57 - METHOD_NO_INIT - public ModelInfluencer() { - initializer method does not guarantee @NonNull field modelChannel (line 122) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ModelInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ModelInfluencer.java
@@ -123,6 +123,7 @@ public abstract class ModelInfluencer extends Influencer {
 
   public ModelInfluencer() {
     this.models = new Array<Model>(true, 1, Model.class);
+    this.modelChannel = new ObjectChannel<>(); // Ensure modelChannel is initialized
   }
 
   public ModelInfluencer(Model... models) {
