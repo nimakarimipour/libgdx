a4f579bd2ae82640b606fb885258f7ae124e4f80
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:36:08 -0700

    Revert "fix: 184 - DEREFERENCE_NULLABLE - effectData.resource.load(manager, effectData); - dereferenced expression effectData is @Nullable"
    This reverts commit 984313b89f374c9fa4ca87128c3fe1053b64e5dd.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleEffectLoader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleEffectLoader.java
@@ -126,7 +126,7 @@ public class ParticleEffectLoader
       AssetManager manager,
       String fileName,
       FileHandle file,
-      ParticleEffectLoadParameter parameter) {
+      @Nullable ParticleEffectLoadParameter parameter) {
     ResourceData<ParticleEffect> effectData = null;
     synchronized (items) {
       for (int i = 0; i < items.size; ++i) {
@@ -137,10 +137,6 @@ public class ParticleEffectLoader
           break;
         }
       }
-    }
-
-    if (effectData == null) {
-      throw new IllegalStateException("Effect data not found for file: " + fileName);
     }
 
     effectData.resource.load(manager, effectData);
