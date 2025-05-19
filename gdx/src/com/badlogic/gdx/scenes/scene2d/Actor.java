a259b3b64c408fa33593dcc2349f02f01b23507d
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:59:20 -0700

    fix: 365 - DEREFERENCE_NULLABLE - action.setActor(this); - dereferenced expression action is @Nullable

--- gdx/src/com/badlogic/gdx/scenes/scene2d/Actor.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/Actor.java
@@ -303,7 +303,8 @@ public class Actor {
     return captureListeners;
   }
 
-  public void addAction(@Nullable Action action) {
+  public void addAction(Action action) {
+    if (action == null) return;
     action.setActor(this);
     actions.add(action);
 
