543beee134b92ecc527f1054597b1b5cf40f26c7
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:43:21 -0700

    Revert "fix: 249 - DEREFERENCE_NULLABLE - Gdx.net.sendHttpRequest( - dereferenced expression Gdx.net is @Nullable"
    This reverts commit d700eb15c6f09ae19dab997a50e644cbc2993c33.

--- gdx/src/com/badlogic/gdx/graphics/Pixmap.java
+++ gdx/src/com/badlogic/gdx/graphics/Pixmap.java
@@ -255,10 +255,6 @@ public class Pixmap implements Disposable {
    */
   public static void downloadFromUrl(
       String url, final DownloadPixmapResponseListener responseListener) {
-    if (Gdx.net == null) {
-      responseListener.downloadFailed(new NullPointerException("Gdx.net is null"));
-      return;
-    }
     Net.HttpRequest request = new Net.HttpRequest(Net.HttpMethods.GET);
     request.setUrl(url);
     Gdx.net.sendHttpRequest(
