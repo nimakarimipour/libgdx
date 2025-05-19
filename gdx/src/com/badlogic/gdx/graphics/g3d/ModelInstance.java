3fce4a64d9e2997f1d01b26f4c09a14a55fc4564
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:59:34 -0700

    fix: 367 - PASS_NULLABLE - this(model, transform, (String[]) null); - passing @Nullable parameter '(String[]) null' where @NonNull is required

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -283,7 +283,7 @@ public class ModelInstance implements RenderableProvider {
 
   /** Constructs a new ModelInstance with the specified transform. */
   public ModelInstance(final Model model, Matrix4 transform) {
-    this(model, transform, (String[]) null);
+    this(model, transform, new String[0]);
   }
 
   /** Constructs a new ModelInstance which is an copy of the specified ModelInstance. */
