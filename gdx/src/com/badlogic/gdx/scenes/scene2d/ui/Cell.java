80524ec1cec513b7054a17f5b167de234009ad01
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:29:37 -0700

    Revert "fix: 137 - ASSIGN_FIELD_NULLABLE - maxWidth = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 07128377e74b55c2c22328aee825345098885d4f.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -984,7 +984,7 @@ public class Cell<T extends Actor> implements Poolable {
     minHeight = null;
     prefWidth = null;
     prefHeight = null;
-    maxWidth = 0f;
+    maxWidth = null;
     maxHeight = null;
     spaceTop = null;
     spaceLeft = null;
