9f8d678f491ec5eb375e51ec07e1e18feb2561eb
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:40:28 -0700

    Revert "fix: 225 - DEREFERENCE_NULLABLE - for (int i = 0; i < n; i++) if ((node = nodes.get(i)).id.equalsIgnoreCase(id)) return node; - dereferenced expression (node = nodes.get(i)).id is @Nullable"
    This reverts commit caa66854daf0234e800eed71d9b0699079bebb04.

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/Node.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/Node.java
@@ -389,26 +389,19 @@ public class Node {
    *     specified node.
    * @return The node with the specified id, or null if not found.
    */
+  @Nullable
   public static Node getNode(
-      final Array<Node> nodes, final String id, boolean recursive, boolean ignoreCase) {
+      final Array<Node> nodes, @Nullable final String id, boolean recursive, boolean ignoreCase) {
     final int n = nodes.size;
     Node node;
     if (ignoreCase) {
-      for (int i = 0; i < n; i++) {
-        node = nodes.get(i);
-        if (node != null && node.id != null && node.id.equalsIgnoreCase(id)) return node;
-      }
+      for (int i = 0; i < n; i++) if ((node = nodes.get(i)).id.equalsIgnoreCase(id)) return node;
     } else {
-      for (int i = 0; i < n; i++) {
-        node = nodes.get(i);
-        if (node != null && node.id != null && node.id.equals(id)) return node;
-      }
+      for (int i = 0; i < n; i++) if ((node = nodes.get(i)).id.equals(id)) return node;
     }
     if (recursive) {
-      for (int i = 0; i < n; i++) {
-        node = getNode(nodes.get(i).children, id, true, ignoreCase);
-        if (node != null) return node;
-      }
+      for (int i = 0; i < n; i++)
+        if ((node = getNode(nodes.get(i).children, id, true, ignoreCase)) != null) return node;
     }
     return null;
   }
