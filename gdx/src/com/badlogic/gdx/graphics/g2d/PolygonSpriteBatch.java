60706f81a92d257e93c5e491081fa1ef4559c193
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:49:55 -0700

    fix: 300 - DEREFERENCE_NULLABLE - invTexWidth = 1.0f / texture.getWidth(); - dereferenced expression texture is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g2d/PolygonSpriteBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/PolygonSpriteBatch.java
@@ -1435,7 +1435,10 @@ public class PolygonSpriteBatch implements PolygonBatch {
     }
   }
 
-  private void switchTexture(@Nullable Texture texture) {
+  private void switchTexture(Texture texture) {
+    if (texture == null) {
+      throw new IllegalArgumentException("Texture cannot be null");
+    }
     flush();
     lastTexture = texture;
     invTexWidth = 1.0f / texture.getWidth();
