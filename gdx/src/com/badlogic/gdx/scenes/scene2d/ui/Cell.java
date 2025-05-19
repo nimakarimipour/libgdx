84d90a3d7e8cfa18e231c1952c5a4cc442a2505b
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:41:23 -0700

    Revert "fix: 232 - PASS_NULLABLE - return padTop.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required"
    This reverts commit f475530d3591c84f74b970784fbd2209508324f3.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -862,9 +862,6 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getPadTop() {
-    if (actor == null) {
-      throw new IllegalArgumentException("actor cannot be null");
-    }
     return padTop.get(actor);
   }
 
