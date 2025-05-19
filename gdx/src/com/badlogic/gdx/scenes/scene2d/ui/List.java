6030d38b61a4a55a6e4ffcd771f41ebc00fd4e80
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 16:05:37 -0700

    fix: 396 - METHOD_NO_INIT - public List(ListStyle style) { - initializer method does not guarantee @NonNull field style (line 53) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/List.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/List.java
@@ -70,6 +70,11 @@ public class List<T> extends Widget implements Cullable {
   }
 
   public List(ListStyle style) {
+    if (style == null) {
+      throw new IllegalArgumentException("style cannot be null.");
+    }
+    this.style = style;
+
     selection.setActor(this);
     selection.setRequired(true);
 
@@ -80,9 +85,9 @@ public class List<T> extends Widget implements Cullable {
         keyListener =
             new InputListener() {
               long typeTimeout;
-              @Nullable String prefix;
+              String prefix;
 
-              public boolean keyDown(@Nullable InputEvent event, int keycode) {
+              public boolean keyDown(InputEvent event, int keycode) {
                 if (items.isEmpty()) return false;
                 int index;
                 switch (keycode) {
@@ -160,8 +165,7 @@ public class List<T> extends Widget implements Cullable {
             return false;
           }
 
-          public void exit(
-              InputEvent event, float x, float y, int pointer, @Nullable Actor toActor) {
+          public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
             if (pointer == 0) pressedIndex = -1;
             if (pointer == -1) overIndex = -1;
           }
