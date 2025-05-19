abbb2626ecd0ec3040d90addb79ee583ba9f8eb7
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:35:42 -0700

    fix: 182 - PASS_NULLABLE - return (T) constructor.newInstance((Object[]) null); - passing @Nullable parameter '(Object[]) null' where @NonNull is required

--- gdx/src/com/badlogic/gdx/utils/ReflectionPool.java
+++ gdx/src/com/badlogic/gdx/utils/ReflectionPool.java
@@ -64,7 +64,7 @@ public class ReflectionPool<T> extends Pool<T> {
 
   protected T newObject() {
     try {
-      return (T) constructor.newInstance((Object[]) null);
+      return (T) constructor.newInstance();
     } catch (Exception ex) {
       throw new GdxRuntimeException(
           "Unable to create new instance: " + constructor.getDeclaringClass().getName(), ex);
