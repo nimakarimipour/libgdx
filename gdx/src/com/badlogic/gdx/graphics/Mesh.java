0c3e331141427736eaac489ada4dd52930fa2957
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:41:15 -0700

    Revert "fix: 230 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 040e483e1aef02a4ef7c2769d75b27648633755f.

--- gdx/src/com/badlogic/gdx/graphics/Mesh.java
+++ gdx/src/com/badlogic/gdx/graphics/Mesh.java
@@ -801,12 +801,9 @@ public class Mesh implements Disposable {
   public VertexAttribute getVertexAttribute(int usage) {
     VertexAttributes attributes = vertices.getAttributes();
     int len = attributes.size();
-    for (int i = 0; i < len; i++) {
-      if (attributes.get(i).usage == usage) {
-        return attributes.get(i);
-      }
-    }
-    throw new IllegalArgumentException("No VertexAttribute found for the given usage: " + usage);
+    for (int i = 0; i < len; i++) if (attributes.get(i).usage == usage) return attributes.get(i);
+
+    return null;
   }
 
   /**
