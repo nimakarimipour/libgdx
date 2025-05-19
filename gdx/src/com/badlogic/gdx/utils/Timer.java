423e83f65c33d27e93a7e548aa0d03e6a5854921
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:11:05 -0700

    fix: 29 - DEREFERENCE_NULLABLE - if (thread.pauseTimeMillis > 0) executeTimeMillis -= timeMillis - thread.pauseTimeMillis; - dereferenced expression thread is @Nullable

--- gdx/src/com/badlogic/gdx/utils/Timer.java
+++ gdx/src/com/badlogic/gdx/utils/Timer.java
@@ -99,7 +99,8 @@ public class Timer {
           task.timer = this;
           long timeMillis = System.nanoTime() / 1000000;
           long executeTimeMillis = timeMillis + (long) (delaySeconds * 1000);
-          if (thread.pauseTimeMillis > 0) executeTimeMillis -= timeMillis - thread.pauseTimeMillis;
+          if (thread != null && thread.pauseTimeMillis > 0)
+            executeTimeMillis -= timeMillis - thread.pauseTimeMillis;
           task.executeTimeMillis = executeTimeMillis;
           task.intervalMillis = (long) (intervalSeconds * 1000);
           task.repeatCount = repeatCount;
