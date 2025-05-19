7a8a6880429d2991708eaf80ba9b0fe3145b8063
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 16:11:48 -0700

    Revert "fix: 446 - PASS_NULLABLE - return addChannel(channelDescriptor, null); - passing @Nullable parameter 'channelDescriptor' where @NonNull is required"
    This reverts commit 98d1e06975bb069a2ced38757cd21c3e04abb4a3.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParallelArray.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParallelArray.java
@@ -199,10 +199,7 @@ public class ParallelArray {
    * Adds and returns a channel described by the channel descriptor parameter. If a channel with the
    * same id already exists, no allocation is performed and that channel is returned.
    */
-  public <T extends Channel> T addChannel(ChannelDescriptor channelDescriptor) {
-    if (channelDescriptor == null) {
-      throw new IllegalArgumentException("channelDescriptor cannot be null");
-    }
+  public <T extends Channel> T addChannel(@Nullable ChannelDescriptor channelDescriptor) {
     return addChannel(channelDescriptor, null);
   }
 
