928bfa37e8d9196462dca0c47c3c2be5f0ab0a71
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:56:15 -0700

    Revert "fix: 341 - PASS_NULLABLE - this(model, (String[]) null); - passing @Nullable parameter '(String[]) null' where @NonNull is required"
    This reverts commit f3ad5d075c77d4065b2fa64d51a8e8673a7fe154.

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -73,7 +73,7 @@ public class ModelInstance implements RenderableProvider {
    * @param model The {@link Model} to create an instance of.
    */
   public ModelInstance(final Model model) {
-    this(model, new String[0]);
+    this(model, (String[]) null);
   }
 
   /**
