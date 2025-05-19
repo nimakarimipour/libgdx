d5f545043dd8cc05bb7e15a9c16c5796ec9a8df2
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:47:41 -0700

    fix: 280 - DEREFERENCE_NULLABLE - texture = region.texture; - dereferenced expression region is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java
@@ -123,7 +123,10 @@ public class TextureRegion {
   }
 
   /** Sets the texture and coordinates to the specified region. */
-  public void setRegion(@Nullable TextureRegion region) {
+  public void setRegion(TextureRegion region) {
+    if (region == null || region.texture == null) {
+      throw new IllegalArgumentException("Region or its texture cannot be null");
+    }
     texture = region.texture;
     setRegion(region.u, region.v, region.u2, region.v2);
   }
