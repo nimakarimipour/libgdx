e714ffff9c6d7e66ac76daf81a526848398d269e
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:15:39 -0700

    fix: 67 - DEREFERENCE_NULLABLE - this.instances.updateInstanceData(targetOffset, source, sourceOffset, count); - dereferenced expression this.instances is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/Mesh.java
+++ gdx/src/com/badlogic/gdx/graphics/Mesh.java
@@ -359,7 +359,9 @@ public class Mesh implements Disposable {
    */
   public Mesh updateInstanceData(
       int targetOffset, FloatBuffer source, int sourceOffset, int count) {
-    this.instances.updateInstanceData(targetOffset, source, sourceOffset, count);
+    if (this.instances != null) {
+      this.instances.updateInstanceData(targetOffset, source, sourceOffset, count);
+    }
     return this;
   }
 
