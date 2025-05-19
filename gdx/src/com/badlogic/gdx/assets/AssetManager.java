351d20fd6a9b7378ed88c74922bee06b422edf69
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:07:13 -0700

    fix: 5 - DEREFERENCE_NULLABLE - return get(assetDescriptor.fileName, assetDescriptor.type, true); - dereferenced expression assetDescriptor is @Nullable

--- gdx/src/com/badlogic/gdx/assets/AssetManager.java
+++ gdx/src/com/badlogic/gdx/assets/AssetManager.java
@@ -204,7 +204,10 @@ public class AssetManager implements Disposable {
    * @return the asset
    * @throws GdxRuntimeException if the asset is not loaded
    */
-  public synchronized <T> T get(@Nullable AssetDescriptor<T> assetDescriptor) {
+  public synchronized <T> T get(AssetDescriptor<T> assetDescriptor) {
+    if (assetDescriptor == null) {
+      throw new NullPointerException("AssetDescriptor cannot be null");
+    }
     return get(assetDescriptor.fileName, assetDescriptor.type, true);
   }
 
