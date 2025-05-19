ac84cebb6b5d5a7c555273be89972f18f63bff3a
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:11:16 -0700

    Revert "fix: 30 - DEREFERENCE_NULLABLE - final int idx = unitsLRU[i]; - dereferenced expression unitsLRU is @Nullable"
    This reverts commit 4d3eac8c0a6f27938adf477733c6b8485ef43a94.

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultTextureBinder.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultTextureBinder.java
@@ -160,34 +160,30 @@ public final class DefaultTextureBinder implements TextureBinder {
   }
 
   private final int bindTextureLRU(final GLTexture texture) {
-      if (unitsLRU == null) {
-        // Handle the case when unitsLRU is null
-        throw new IllegalStateException("unitsLRU is not initialized");
-      }
-  
-      int i;
-      for (i = 0; i < count; i++) {
-        final int idx = unitsLRU[i];
-        if (textures[idx] == texture) {
-          reused = true;
-          break;
-        }
-        if (textures[idx] == null) {
-          break;
-        }
-      }
-      if (i >= count) i = count - 1;
+    int i;
+    for (i = 0; i < count; i++) {
       final int idx = unitsLRU[i];
-      while (i > 0) {
-        unitsLRU[i] = unitsLRU[i - 1];
-        i--;
+      if (textures[idx] == texture) {
+        reused = true;
+        break;
       }
-      unitsLRU[0] = idx;
-      if (!reused) {
-        textures[idx] = texture;
-        texture.bind(offset + idx);
+      if (textures[idx] == null) {
+        break;
       }
-      return idx;
+    }
+    if (i >= count) i = count - 1;
+    final int idx = unitsLRU[i];
+    while (i > 0) {
+      unitsLRU[i] = unitsLRU[i - 1];
+      i--;
+    }
+    unitsLRU[0] = idx;
+    if (!reused) {
+      textures[idx] = texture;
+      texture.bind(offset + idx);
+    }
+    return idx;
+  }
 
   @Override
   public final int getBindCount() {
