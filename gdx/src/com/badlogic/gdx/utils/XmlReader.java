7fb6f7c3476a8e208c34a52fa75684d9534a4561
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:57:30 -0700

    Revert "fix: 352 - RETURN_NULLABLE - return text; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit ee6c50dea27f823ded51554e9665841f685b721a.

--- gdx/src/com/badlogic/gdx/utils/XmlReader.java
+++ gdx/src/com/badlogic/gdx/utils/XmlReader.java
@@ -552,7 +552,7 @@ public class XmlReader {
     }
 
     public String getText() {
-      return text != null ? text : "";
+      return text;
     }
 
     public void setText(@Nullable String text) {
