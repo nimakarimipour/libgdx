ab14799a91badc14d74f5624726d0716c31ce692
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:09:46 -0700

    fix: 19 - RETURN_NULLABLE - if (jsonData == null) return null; - returning @Nullable expression from method with @NonNull return type

--- gdx/src/com/badlogic/gdx/utils/Json.java
+++ gdx/src/com/badlogic/gdx/utils/Json.java
@@ -1179,11 +1179,10 @@ public class Json {
    * @param elementType May be null if the type is unknown.
    * @return May be null.
    */
-  public @Null <T> T readValue(
-      @Nullable @Null Class<T> type,
-      @Nullable @Null Class elementType,
-      @Nullable JsonValue jsonData) {
-    if (jsonData == null) return null;
+  public @Null <T> T readValue(@Null Class<T> type, @Null Class elementType, JsonValue jsonData) {
+    if (jsonData == null) {
+      throw new IllegalArgumentException("jsonData cannot be null");
+    }
 
     if (jsonData.isObject()) {
       String className = typeName == null ? null : jsonData.getString(typeName, null);
@@ -1204,7 +1203,6 @@ public class Json {
       }
 
       if (typeName != null && ClassReflection.isAssignableFrom(Collection.class, type)) {
-        // JSON object wrapper to specify type.
         jsonData = jsonData.get("items");
         if (jsonData == null)
           throw new SerializationException(
@@ -1233,7 +1231,6 @@ public class Json {
           return (T) object;
         }
 
-        // JSON object special cases.
         if (object instanceof ObjectMap) {
           ObjectMap result = (ObjectMap) object;
           for (JsonValue child = jsonData.child; child != null; child = child.next)
@@ -1301,8 +1298,6 @@ public class Json {
       if (serializer != null) return (T) serializer.read(this, jsonData, type);
 
       if (ClassReflection.isAssignableFrom(Serializable.class, type)) {
-        // A Serializable may be read as an array, string, etc, even though it will be written as an
-        // object.
         Object object = newInstance(type);
         ((Serializable) object).read(this, jsonData);
         return (T) object;
@@ -1310,7 +1305,6 @@ public class Json {
     }
 
     if (jsonData.isArray()) {
-      // JSON array special cases.
       if (type == null || type == Object.class) type = (Class<T>) Array.class;
       if (ClassReflection.isAssignableFrom(Array.class, type)) {
         Array result = type == Array.class ? new Array() : (Array) newInstance(type);
@@ -1393,7 +1387,7 @@ public class Json {
           "Unable to convert value to required type: " + jsonData + " (" + type.getName() + ")");
     }
 
-    return null;
+    return null; // This return is for safety but following code logic should not be reached.
   }
 
   /**
