edaccdceec2498aa750dfb9958676ef4ae0380c3
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:17:18 -0700

    fix: 80 - RETURN_NULLABLE - return i < 0 ? null : valueTable[i]; - returning @Nullable expression from method with @NonNull return type

--- gdx/src/com/badlogic/gdx/utils/ObjectMap.java
+++ gdx/src/com/badlogic/gdx/utils/ObjectMap.java
@@ -201,7 +201,7 @@ public class ObjectMap<K, V> implements Iterable<ObjectMap.Entry<K, V>> {
   }
 
   /** Returns the value for the specified key, or null if the key is not in the map. */
-  public @Null <T extends K> V get(T key) {
+  public @Null V get(K key) {
     int i = locateKey(key);
     return i < 0 ? null : valueTable[i];
   }
