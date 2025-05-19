08b3f6b5c51817bd45671a7363b1e6094a5e03d5
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:54:21 -0700

    fix: 330 - RETURN_NULLABLE - if (!dirty) return worldVertices; - returning @Nullable expression from method with @NonNull return type

--- gdx/src/com/badlogic/gdx/math/Polyline.java
+++ gdx/src/com/badlogic/gdx/math/Polyline.java
@@ -51,7 +51,7 @@ public class Polyline implements Shape2D {
 
   /** Returns vertices scaled, rotated, and offset by the polygon position. */
   public float[] getTransformedVertices() {
-    if (!dirty) return worldVertices;
+    if (!dirty) return worldVertices != null ? worldVertices : new float[0];
     dirty = false;
 
     final float[] localVertices = this.localVertices;
