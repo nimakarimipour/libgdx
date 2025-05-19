46a227fd85e8d263238cbd59a571997e0a102cf7
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:37:39 -0700

    Revert "fix: 196 - DEREFERENCE_NULLABLE - plugs.get(group).beforeGroup(contents); - dereferenced expression plugs.get(group) is @Nullable"
    This reverts commit 85cf7689fee20b3e6c7a1f49890f7e2523e5d1d4.

--- gdx/src/com/badlogic/gdx/graphics/g3d/decals/PluggableGroupStrategy.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/decals/PluggableGroupStrategy.java
@@ -29,10 +29,7 @@ public abstract class PluggableGroupStrategy implements GroupStrategy {
 
   @Override
   public void beforeGroup(int group, Array<Decal> contents) {
-    GroupPlug plug = plugs.get(group);
-    if (plug != null) {
-      plug.beforeGroup(contents);
-    }
+    plugs.get(group).beforeGroup(contents);
   }
 
   @Override
