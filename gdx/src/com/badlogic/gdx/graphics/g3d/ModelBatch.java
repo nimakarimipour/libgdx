c85c06a0557635b6d9d1fb9716782d4af0bb18ca
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:37:03 -0700

    Revert "fix: 191 - ASSIGN_FIELD_NULLABLE - renderable.material = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 0a1b91e093831ff2e8fd609f61c0cf3a7318ac33.

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelBatch.java
@@ -57,7 +57,7 @@ public class ModelBatch implements Disposable {
     public Renderable obtain() {
       Renderable renderable = super.obtain();
       renderable.environment = null;
-      renderable.material = new Material(); // Provide a non-null default instance
+      renderable.material = null;
       renderable.meshPart.set("", null, 0, 0, 0);
       renderable.shader = null;
       renderable.userData = null;
