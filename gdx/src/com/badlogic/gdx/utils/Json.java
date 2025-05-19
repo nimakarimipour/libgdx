0acc6bd3577b9943f1d3f592bce9d11004ae3024
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:47:57 -0700

    fix: 283 - ASSIGN_FIELD_NULLABLE - this.writer = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/utils/Json.java
+++ gdx/src/com/badlogic/gdx/utils/Json.java
@@ -335,16 +335,13 @@ public class Json {
    * @param elementType May be null if the type is unknown.
    */
   public void toJson(
-      @Null Object object,
-      @Nullable @Null Class knownType,
-      @Nullable @Null Class elementType,
-      Writer writer) {
+      @Null Object object, @Null Class knownType, @Null Class elementType, Writer writer) {
     setWriter(writer);
     try {
       writeValue(object, knownType, elementType);
     } finally {
       StreamUtils.closeQuietly(this.writer);
-      this.writer = null;
+      this.writer = new JsonWriter(new StringWriter());
     }
   }
 
