d6419c62ab543d155cbdea9fcaaac0abc4669b6d
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 16:09:23 -0700

    Revert "fix: 429 - METHOD_NO_INIT - public TextButtonStyle() {} - initializer method does not guarantee @NonNull field font (line 147) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit f59bbe98e50ea9051b61b25e32ec3a47ec857f24.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextButton.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextButton.java
@@ -153,9 +153,7 @@ public class TextButton extends Button {
         checkedOverFontColor,
         checkedFocusedFontColor;
 
-    public TextButtonStyle() {
-      this.font = new BitmapFont(); // Assumes a default font is acceptable
-    }
+    public TextButtonStyle() {}
 
     public TextButtonStyle(
         @Null Drawable up, @Null Drawable down, @Null Drawable checked, @Null BitmapFont font) {
