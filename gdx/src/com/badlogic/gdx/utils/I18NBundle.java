71a647e719d9e3e9ec660ef4dff740671c7de08c
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:46:07 -0700

    Revert "fix: 267 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull field properties (line 94) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 266fe15e3875e7e237fe5608457174962f5af1ff.

--- gdx/src/com/badlogic/gdx/utils/I18NBundle.java
+++ gdx/src/com/badlogic/gdx/utils/I18NBundle.java
@@ -487,9 +487,6 @@ public class I18NBundle {
    */
   @Initializer
   private void setLocale(Locale locale) {
-    if (locale == null) {
-      throw new IllegalArgumentException("Locale cannot be null");
-    }
     this.locale = locale;
     this.formatter = new TextFormatter(locale, !simpleFormatter);
   }
