cb787f06bef89a1b761b73bc99237a735f401476
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:27:14 -0700

    Revert "fix: 118 - DEREFERENCE_NULLABLE - String existing = current.getText(); - dereferenced expression current is @Nullable"
    This reverts commit 7fc71793d052d5e69b1c1072c1ae89d1cb1ef65c.

--- gdx/src/com/badlogic/gdx/utils/XmlReader.java
+++ gdx/src/com/badlogic/gdx/utils/XmlReader.java
@@ -472,11 +472,9 @@ public class XmlReader {
     return null;
   }
 
-  protected void text(String text) {
-    if (current != null) {
-      String existing = current.getText();
-      current.setText(existing != null ? existing + text : text);
-    }
+  protected void text(@Nullable String text) {
+    String existing = current.getText();
+    current.setText(existing != null ? existing + text : text);
   }
 
   protected void close() {
