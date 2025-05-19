4bcd9547571ad276f120bc61a3306b9a3f1225e9
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 15:44:45 -0700

    fix: 262 - ASSIGN_FIELD_NULLABLE - _bufferBase = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/utils/compression/lz/InWindow.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lz/InWindow.java
@@ -54,7 +54,7 @@ public class InWindow {
   }
 
   void Free() {
-    _bufferBase = null;
+    _bufferBase = new byte[0];
   }
 
   public void Create(int keepSizeBefore, int keepSizeAfter, int keepSizeReserv) {
