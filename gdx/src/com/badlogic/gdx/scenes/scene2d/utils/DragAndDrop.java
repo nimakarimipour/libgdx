67d43ca4a4462bb267e3ac65b02549b5674e1603
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-03 12:00:18 -0700

    Revert "fix: 53 - DEREFERENCE_NULLABLE - target.actor.stageToLocalCoordinates(tmpVector.set(stageX, stageY)); - dereferenced expression target is @Nullable"
    This reverts commit e5ba77e9b883f09297297e28773e2752d74bf311.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/DragAndDrop.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/DragAndDrop.java
@@ -161,18 +161,14 @@ public class DragAndDrop {
             else if (!isValidTarget && target != null) {
               float stageX = event.getStageX() + touchOffsetX,
                   stageY = event.getStageY() + touchOffsetY;
-              NullabilityUtil.castToNonnull(target, "checked in condition")
-                  .actor
-                  .stageToLocalCoordinates(tmpVector.set(stageX, stageY));
+              target.actor.stageToLocalCoordinates(tmpVector.set(stageX, stageY));
               isValidTarget = target.drag(source, payload, tmpVector.x, tmpVector.y, pointer);
             }
             if (dragActor != null && removeDragActor) dragActor.remove();
             if (isValidTarget) {
               float stageX = event.getStageX() + touchOffsetX,
                   stageY = event.getStageY() + touchOffsetY;
-              NullabilityUtil.castToNonnull(target, "checked in condition")
-                  .actor
-                  .stageToLocalCoordinates(tmpVector.set(stageX, stageY));
+              target.actor.stageToLocalCoordinates(tmpVector.set(stageX, stageY));
               target.drop(source, payload, tmpVector.x, tmpVector.y, pointer);
             }
             source.dragStop(event, x, y, pointer, payload, isValidTarget ? target : null);
