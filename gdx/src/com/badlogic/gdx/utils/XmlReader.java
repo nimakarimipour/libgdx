de78e1fa8e5216e801b4dfc08b5c3d109f9f7456
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-03 11:17:26 -0700

    Revert "fix: 2 - RETURN_NULLABLE - if (children == null) return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit e4e8b2079c22a5ea677fcc2db75a78f7a46b2be7.

--- gdx/src/com/badlogic/gdx/utils/XmlReader.java
+++ gdx/src/com/badlogic/gdx/utils/XmlReader.java
@@ -621,7 +621,6 @@ public class XmlReader {
      * @param name the name of the child {@link Element}
      * @return the first child having the given name or null, does not recurse
      */
-    @SuppressWarnings("NullAway")
     public @Null Element getChildByName(String name) {
       if (children == null) return null;
       for (int i = 0; i < children.size; i++) {
