a1e3c7ff105e10b63589e361ec5dbec9efb40333
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-03 11:51:57 -0700

    fix: 22 - ASSIGN_FIELD_NULLABLE - httpRequest = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/net/HttpRequestBuilder.java
+++ gdx/src/com/badlogic/gdx/net/HttpRequestBuilder.java
@@ -22,6 +22,7 @@ import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.Pools;
 import java.io.InputStream;
 import java.util.Map;
+import javax.annotation.Nullable;
 
 /**
  * A builder for {@link HttpRequest}s.
@@ -49,7 +50,7 @@ public class HttpRequestBuilder {
   /** Will be used for the object serialization in case {@link #jsonContent(Object)} is called. */
   public static Json json = new Json();
 
-  private HttpRequest httpRequest;
+  @Nullable private HttpRequest httpRequest;
 
   /** Initializes the builder and sets it up to build a new {@link HttpRequest} . */
   public HttpRequestBuilder newRequest() {
@@ -145,9 +146,11 @@ public class HttpRequestBuilder {
    */
   public HttpRequestBuilder formEncodedContent(Map<String, String> content) {
     validate();
-    httpRequest.setHeader(HttpRequestHeader.ContentType, "application/x-www-form-urlencoded");
-    String formEncodedContent = HttpParametersUtils.convertHttpParameters(content);
-    httpRequest.setContent(formEncodedContent);
+    if (httpRequest != null) { // Added check to ensure httpRequest is not null
+      httpRequest.setHeader(HttpRequestHeader.ContentType, "application/x-www-form-urlencoded");
+      String formEncodedContent = HttpParametersUtils.convertHttpParameters(content);
+      httpRequest.setContent(formEncodedContent);
+    }
     return this;
   }
 
@@ -176,6 +179,7 @@ public class HttpRequestBuilder {
    * Returns the {@link HttpRequest} that has been setup by this builder so far. After using the
    * request, it should be returned to the pool via {@code Pools.free(request)}.
    */
+  @Nullable
   public HttpRequest build() {
     validate();
     HttpRequest request = httpRequest;
