59d0f33195c96144fa2ba5ad1f871b22d89b1955
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-03 11:58:31 -0700

    fix: 41 - DEREFERENCE_NULLABLE - Stream.flush(); - dereferenced expression Stream is @Nullable

--- gdx/src/com/badlogic/gdx/utils/compression/rangecoder/Encoder.java
+++ gdx/src/com/badlogic/gdx/utils/compression/rangecoder/Encoder.java
@@ -56,10 +56,16 @@ public class Encoder {
   }
 
   public void FlushStream() throws IOException {
-    Stream.flush();
+    if (Stream != null) {
+      Stream.flush();
+    }
   }
 
   public void ShiftLow() throws IOException {
+    if (Stream == null) {
+      throw new IOException("Stream is not set");
+    }
+
     int LowHi = (int) (Low >>> 32);
     if (LowHi != 0 || Low < 0xFF000000L) {
       _position += _cacheSize;
