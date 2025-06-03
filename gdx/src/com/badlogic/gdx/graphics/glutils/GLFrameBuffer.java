6f41236d09846c716e63632ccb8ec451805c2df1
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-03 11:58:37 -0700

    fix: 42 - METHOD_NO_INIT - public GLFrameBufferBuilder(int width, int height) { - initializer method does not guarantee @NonNull fields stencilRenderBufferSpec (line 525), depthRenderBufferSpec (line 526), packedStencilDepthRenderBufferSpec (line 527) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/glutils/GLFrameBuffer.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/GLFrameBuffer.java
@@ -33,6 +33,7 @@ import java.nio.ByteOrder;
 import java.nio.IntBuffer;
 import java.util.HashMap;
 import java.util.Map;
+import javax.annotation.Nullable;
 
 /**
  * Encapsulates OpenGL ES 2.0 frame buffer objects. This is a simple helper class which should cover
@@ -157,7 +158,8 @@ public abstract class GLFrameBuffer<T extends GLTexture> implements Disposable {
           height);
     }
 
-    if (bufferBuilder.hasPackedStencilDepthRenderBuffer) {
+    if (bufferBuilder.hasPackedStencilDepthRenderBuffer
+        && bufferBuilder.packedStencilDepthRenderBufferSpec != null) {
       depthStencilPackedBufferHandle = gl.glGenRenderbuffer();
       gl.glBindRenderbuffer(GL20.GL_RENDERBUFFER, depthStencilPackedBufferHandle);
       gl.glRenderbufferStorage(
@@ -522,9 +524,9 @@ public abstract class GLFrameBuffer<T extends GLTexture> implements Disposable {
     protected Array<FrameBufferTextureAttachmentSpec> textureAttachmentSpecs =
         new Array<FrameBufferTextureAttachmentSpec>();
 
-    protected FrameBufferRenderBufferAttachmentSpec stencilRenderBufferSpec;
-    protected FrameBufferRenderBufferAttachmentSpec depthRenderBufferSpec;
-    protected FrameBufferRenderBufferAttachmentSpec packedStencilDepthRenderBufferSpec;
+    @Nullable protected FrameBufferRenderBufferAttachmentSpec stencilRenderBufferSpec;
+    @Nullable protected FrameBufferRenderBufferAttachmentSpec depthRenderBufferSpec;
+    @Nullable protected FrameBufferRenderBufferAttachmentSpec packedStencilDepthRenderBufferSpec;
 
     protected boolean hasStencilRenderBuffer;
     protected boolean hasDepthRenderBuffer;
