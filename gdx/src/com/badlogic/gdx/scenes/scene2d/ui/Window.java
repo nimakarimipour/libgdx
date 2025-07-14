fb3fc8164c9b72ea29b1fdc191ae5cc5019cdedb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 15:50:22 -0700

    Revert "fix: 18 - METHOD_NO_INIT - public WindowStyle() {} - initializer method does not guarantee @NonNull field titleFont (line 390) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 3597ad9dd98312807afb995529067be3a0efba40.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
@@ -391,10 +391,7 @@ public class Window extends Table {
     public @Null Color titleFontColor = new Color(1, 1, 1, 1);
     @Nullable public @Null Drawable stageBackground;
 
-    public WindowStyle() {
-      this.titleFont =
-          new BitmapFont(); // Or another default initialization relevant to your application
-    }
+    public WindowStyle() {}
 
     public WindowStyle(BitmapFont titleFont, Color titleFontColor, @Null Drawable background) {
       this.titleFont = titleFont;
