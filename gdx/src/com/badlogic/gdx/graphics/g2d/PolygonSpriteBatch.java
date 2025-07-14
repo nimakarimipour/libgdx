7bd37b5f39943b9d234fece208dfb89164e3fb1b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:38:08 -0700

    Revert "fix: 299 - DEREFERENCE_NULLABLE - invTexWidth = 1.0f / texture.getWidth(); - dereferenced expression texture is @Nullable"
    This reverts commit d199c054c2749db98caec20ca439a3908f79157f.

--- gdx/src/com/badlogic/gdx/graphics/g2d/PolygonSpriteBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/PolygonSpriteBatch.java
@@ -1436,9 +1436,6 @@ public class PolygonSpriteBatch implements PolygonBatch {
   }
 
   private void switchTexture(@Nullable Texture texture) {
-    if (texture == null) {
-      throw new IllegalArgumentException("Texture cannot be null");
-    }
     flush();
     lastTexture = texture;
     invTexWidth = 1.0f / texture.getWidth();
