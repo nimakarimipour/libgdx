3ae0c317e9f99f182f9c3c4394d3babb555af459
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:16:22 -0700

    fix: 170 - FIELD_NO_INIT - public String id; - @NonNull field id not initialized

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/Animation.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/Animation.java
@@ -29,7 +29,7 @@ import com.badlogic.gdx.utils.Array;
  */
 public class Animation {
   /** the unique id of the animation * */
-  public String id;
+  public String id = "";
 
   /** the duration in seconds * */
   public float duration;
