f87a166d1f6fd1fffc03715e61e90674701a50ba
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:10:19 -0700

    fix: 135 - ASSIGN_FIELD_NULLABLE - colspan = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -999,7 +999,7 @@ public class Cell<T extends Actor> implements Poolable {
     align = null;
     expandX = null;
     expandY = null;
-    colspan = null;
+    colspan = 1; // Default value for colspan as an example
     uniformX = null;
     uniformY = null;
   }
