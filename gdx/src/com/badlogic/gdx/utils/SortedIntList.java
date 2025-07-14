04a91be853c251d7b3899c6159a0ef61c01e944b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:41:48 -0700

    Revert "fix: 321 - DEREFERENCE_NULLABLE - previousPosition.p.n = position; - dereferenced expression previousPosition.p is @Nullable"
    This reverts commit a290361cefbd39664cc543856ebfedbcc4f9f2a0.

--- gdx/src/com/badlogic/gdx/utils/SortedIntList.java
+++ gdx/src/com/badlogic/gdx/utils/SortedIntList.java
@@ -159,10 +159,15 @@ public class SortedIntList<E> implements Iterable<SortedIntList.Node<E>> {
 
     @Override
     public void remove() {
+      // the contract specifies to remove the last returned element, if nothing was returned yet
+      // assumably do nothing
       if (previousPosition != null) {
+        // if we are at the second element set it as the first element
         if (previousPosition == first) {
           first = position;
-        } else if (previousPosition.p != null) {
+        }
+        // else remove last returned element by changing the chain
+        else {
           previousPosition.p.n = position;
           if (position != null) {
             position.p = previousPosition.p;
