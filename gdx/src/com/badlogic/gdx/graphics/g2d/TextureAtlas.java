e8c6f07107c64136a45dfd7a8d899047e0a934c3
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 15:47:48 -0700

    fix: 3 - METHOD_NO_INIT - public AtlasRegion(TextureRegion region) { - initializer method does not guarantee @NonNull fields name (line 622), values (line 670) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
@@ -699,6 +699,8 @@ public class TextureAtlas implements Disposable {
       packedHeight = region.getRegionHeight();
       originalWidth = packedWidth;
       originalHeight = packedHeight;
+      name = ""; // Initialize the name field
+      values = new float[0]; // Initialize the values field
     }
 
     @Override
