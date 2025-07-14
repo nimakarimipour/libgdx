e5153e0b505d647c290e077fb7523f97ef148df7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 15:53:15 -0700

    Revert "fix: 35 - RETURN_NULLABLE - if (jsonValue == null) return defaultValue; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 0c648168034a9ce9411511f7052f425274c69155.

--- gdx/src/com/badlogic/gdx/utils/Json.java
+++ gdx/src/com/badlogic/gdx/utils/Json.java
@@ -1124,14 +1124,9 @@ public class Json {
    * @return May be null.
    */
   public @Null <T> T readValue(
-      String name, @Null Class<T> type, @Nullable @Null T defaultValue, JsonValue jsonMap) {
+      String name, @Null Class<T> type, @Nullable T defaultValue, JsonValue jsonMap) {
     JsonValue jsonValue = jsonMap.get(name);
-    if (jsonValue == null) {
-      if (defaultValue == null) {
-        throw new IllegalArgumentException("defaultValue cannot be null");
-      }
-      return defaultValue;
-    }
+    if (jsonValue == null) return defaultValue;
     return readValue(type, null, jsonValue);
   }
 
