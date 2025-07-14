0d063971eb978ba4b4af58856182ef13144ca37e
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:25:46 -0700

    Revert "fix: 226 - METHOD_NO_INIT - public TextureRegion(TextureRegion region, int x, int y, int width, int height) { - initializer method does not guarantee @NonNull field texture (line 30) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit cece41b6aa6a3860f77d8cbd17e562518469d80c.

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureRegion.java
@@ -78,12 +78,7 @@ public class TextureRegion {
    * @param height The height of the texture region. May be negative to flip the sprite when drawn.
    */
   public TextureRegion(TextureRegion region, int x, int y, int width, int height) {
-    if (region.texture != null) {
-      this.texture = region.texture;
-      setRegion(region, x, y, width, height);
-    } else {
-      throw new IllegalArgumentException("Provided region must have a non-null texture");
-    }
+    setRegion(region, x, y, width, height);
   }
 
   /** Sets the texture and sets the coordinates to the size of the specified texture. */
