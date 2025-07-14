c06eea5b56860e3129da9587723d1d734b05400a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:49:32 -0700

    Revert "fix: 368 - DEREFERENCE_NULLABLE - lastTexture.bind(); - dereferenced expression lastTexture is @Nullable"
    This reverts commit c1822813a9d98aa30bb6835c8726ea94da63ecac.

--- gdx/src/com/badlogic/gdx/graphics/g2d/PolygonSpriteBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/PolygonSpriteBatch.java
@@ -1323,9 +1323,7 @@ public class PolygonSpriteBatch implements PolygonBatch {
     int trianglesInBatch = triangleIndex;
     if (trianglesInBatch > maxTrianglesInBatch) maxTrianglesInBatch = trianglesInBatch;
 
-    if (lastTexture != null) {
-      lastTexture.bind();
-    }
+    lastTexture.bind();
     Mesh mesh = this.mesh;
     mesh.setVertices(vertices, 0, vertexIndex);
     mesh.setIndices(triangles, 0, trianglesInBatch);
