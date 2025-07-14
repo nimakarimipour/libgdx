8543295c699e6157d92265980f5867f23348c8eb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:42:29 -0700

    Revert "fix: 325 - RETURN_NULLABLE - return root; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit af458a303de788a2118b9e33bcc129c7f54ba805.

--- gdx/src/com/badlogic/gdx/utils/JsonReader.java
+++ gdx/src/com/badlogic/gdx/utils/JsonReader.java
@@ -574,7 +574,7 @@ public class JsonReader implements BaseJsonReader {
     }
     if (parseRuntimeEx != null)
       throw new SerializationException("Error parsing JSON: " + new String(data), parseRuntimeEx);
-    return root != null ? root : new JsonValue(); // Expects a non-null JsonValue.
+    return root;
   }
 
   // line 421 "JsonReader.java"
