566bba92dc8d34e89f148b60759228a2f5e97716
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:46:03 -0700

    Revert "fix: 346 - DEREFERENCE_NULLABLE - c = i + controller.particles.size * rotationalVelocity2dChannel.strideSize; - dereferenced expression rotationalVelocity2dChannel is @Nullable"
    This reverts commit 6df991beeeb1b18e56da04f5ee483b3c70d1b990.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
@@ -227,13 +227,6 @@ public abstract class DynamicsModifier extends Influencer {
 
     @Override
     public void update() {
-      if (rotationalVelocity2dChannel == null) {
-        // Handle the case where rotationalVelocity2dChannel is not initialized.
-        // This might include returning early, throwing an exception, or any other logic appropriate
-        // for your application.
-        return;
-      }
-
       for (int i = 0,
               l = ParticleChannels.LifePercentOffset,
               s = 0,
