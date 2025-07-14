214f9af2e010acde0fa193e773c64ef8d1c251f2
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:19:25 -0700

    Revert "fix: 188 - DEREFERENCE_NULLABLE - this.transform.mul(node.getParent().globalTransform); - dereferenced expression node.getParent() is @Nullable"
    This reverts commit a422c0c70269abbcf69635660279e9cd0c4c179f.

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -212,9 +212,8 @@ public class ModelInstance implements RenderableProvider {
       copy.translation.set(0, 0, 0);
       copy.rotation.idt();
       copy.scale.set(1, 1, 1);
-    } else if (parentTransform && copy.hasParent() && node.getParent() != null) {
+    } else if (parentTransform && copy.hasParent())
       this.transform.mul(node.getParent().globalTransform);
-    }
     invalidate();
     copyAnimations(model.animations, shareKeyframes);
     calculateTransforms();
