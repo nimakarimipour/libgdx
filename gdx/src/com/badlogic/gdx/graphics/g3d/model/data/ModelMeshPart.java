b91ad0d2aae4fe1d0630a18a3871ea31c2121a95
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:29:06 -0700

    fix: 246 - FIELD_NO_INIT - public short[] indices; - @NonNull field indices not initialized

--- gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelMeshPart.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelMeshPart.java
@@ -20,6 +20,6 @@ import javax.annotation.Nullable;
 
 public class ModelMeshPart {
   @Nullable public String id;
-  public short[] indices;
+  public short[] indices = new short[0];
   public int primitiveType;
 }
