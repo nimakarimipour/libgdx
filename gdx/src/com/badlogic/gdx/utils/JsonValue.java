1e2b1e52830f93fddc985313fab241f58d319af1
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:59:26 -0700

    Revert "fix: 407 - ASSIGN_FIELD_NULLABLE - this.stringValue = stringValue; - assigning @Nullable expression to @NonNull field"
    This reverts commit 32f6511fd26bc53e3bfdc180f46219b2f17e8b4b.

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -1175,7 +1175,7 @@ public class JsonValue implements Iterable<JsonValue> {
   public void set(double value, @Nullable @Null String stringValue) {
     doubleValue = value;
     longValue = (long) value;
-    this.stringValue = stringValue != null ? stringValue : "";
+    this.stringValue = stringValue;
     type = ValueType.doubleValue;
   }
 
