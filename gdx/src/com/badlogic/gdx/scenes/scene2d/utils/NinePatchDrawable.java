0bb8cc27099ebaaf701941e25f2bfb0e52593016
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 15:50:12 -0700

    Revert "fix: 17 - METHOD_NO_INIT - public NinePatchDrawable(NinePatch patch) { - initializer method does not guarantee @NonNull field patch (line 37) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 062d29359f019ab844a1690dd48a021cea73389d.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/NinePatchDrawable.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/NinePatchDrawable.java
@@ -43,9 +43,6 @@ public class NinePatchDrawable extends BaseDrawable implements TransformDrawable
   public NinePatchDrawable() {}
 
   public NinePatchDrawable(NinePatch patch) {
-    if (patch == null) {
-      throw new NullPointerException("NinePatch cannot be null.");
-    }
     setPatch(patch);
   }
 
