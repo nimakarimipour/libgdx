901feadbdc4b66291dbdefad6ebc81fc712b395a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:25:54 -0700

    fix: 227 - DEREFERENCE_NULLABLE - float start = rangeStart.actor.getY(), end = node.actor.getY(); - dereferenced expression rangeStart is @Nullable

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java
@@ -106,16 +106,18 @@ public class Tree<N extends Node, V> extends WidgetGroup {
                   // Select range (shift).
                   if (rangeStart == null) rangeStart = node;
                   N rangeStart = Tree.this.rangeStart;
-                  if (!UIUtils.ctrl()) selection.clear();
-                  float start = rangeStart.actor.getY(), end = node.actor.getY();
-                  if (start > end) selectNodes(rootNodes, end, start);
-                  else {
-                    selectNodes(rootNodes, start, end);
-                    selection.items().orderedItems().reverse();
-                  }
+                  if (rangeStart != null) {
+                    if (!UIUtils.ctrl()) selection.clear();
+                    float start = rangeStart.actor.getY(), end = node.actor.getY();
+                    if (start > end) selectNodes(rootNodes, end, start);
+                    else {
+                      selectNodes(rootNodes, start, end);
+                      selection.items().orderedItems().reverse();
+                    }
 
-                  selection.fireChangeEvent();
-                  Tree.this.rangeStart = rangeStart;
+                    selection.fireChangeEvent();
+                    Tree.this.rangeStart = rangeStart;
+                  }
                   return;
                 }
                 if (node.children.size > 0 && (!selection.getMultiple() || !UIUtils.ctrl())) {
