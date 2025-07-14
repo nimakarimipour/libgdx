0901d40f9f6ad6e99d313ef0ab3da1172e2d1ea2
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:31:10 -0700

    Revert "fix: 258 - FIELD_NO_INIT - private Camera camera; - @NonNull field camera not initialized"
    This reverts commit 7f9d5667ba7aae3df3f7a0232474a6aeabb69ea1.

--- gdx/src/com/badlogic/gdx/utils/viewport/Viewport.java
+++ gdx/src/com/badlogic/gdx/utils/viewport/Viewport.java
@@ -35,8 +35,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ScissorStack;
  * @author Nathan Sweet
  */
 public abstract class Viewport {
-  private Camera camera =
-      new Camera(); // Make sure to replace 'Camera()' with the actual implementation.
+  private Camera camera;
   private float worldWidth, worldHeight;
   private int screenX, screenY, screenWidth, screenHeight;
 
