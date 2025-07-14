9b01f7f5704d8b5b761f597d6f1a326fbc354c53
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:58:12 -0700

    Revert "fix: 400 - DEREFERENCE_NULLABLE - int numReadBytes = _stream.read(_bufferBase, _bufferOffset + _streamPos, size); - dereferenced expression _stream is @Nullable"
    This reverts commit d73083e4a874b599f4b8e2f17c41464645120026.

--- gdx/src/com/badlogic/gdx/utils/compression/lz/InWindow.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lz/InWindow.java
@@ -38,22 +38,18 @@ public class InWindow {
     while (true) {
       int size = (0 - _bufferOffset) + _blockSize - _streamPos;
       if (size == 0) return;
-      if (_stream != null) {
-        int numReadBytes = _stream.read(_bufferBase, _bufferOffset + _streamPos, size);
-        if (numReadBytes == -1) {
-          _posLimit = _streamPos;
-          int pointerToPostion = _bufferOffset + _posLimit;
-          if (pointerToPostion > _pointerToLastSafePosition)
-            _posLimit = _pointerToLastSafePosition - _bufferOffset;
+      int numReadBytes = _stream.read(_bufferBase, _bufferOffset + _streamPos, size);
+      if (numReadBytes == -1) {
+        _posLimit = _streamPos;
+        int pointerToPostion = _bufferOffset + _posLimit;
+        if (pointerToPostion > _pointerToLastSafePosition)
+          _posLimit = _pointerToLastSafePosition - _bufferOffset;
 
-          _streamEndWasReached = true;
-          return;
-        }
-        _streamPos += numReadBytes;
-        if (_streamPos >= _pos + _keepSizeAfter) _posLimit = _streamPos - _keepSizeAfter;
-      } else {
-        throw new IOException("Stream is null");
+        _streamEndWasReached = true;
+        return;
       }
+      _streamPos += numReadBytes;
+      if (_streamPos >= _pos + _keepSizeAfter) _posLimit = _streamPos - _keepSizeAfter;
     }
   }
 
