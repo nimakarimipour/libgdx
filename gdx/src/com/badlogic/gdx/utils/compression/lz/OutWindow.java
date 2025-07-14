dcb7a052f9d127f114cfb9230317299a7bfbeec7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:18:04 -0700

    fix: 180 - DEREFERENCE_NULLABLE - _stream.write(_buffer, _streamPos, size); - dereferenced expression _stream is @Nullable

--- gdx/src/com/badlogic/gdx/utils/compression/lz/OutWindow.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lz/OutWindow.java
@@ -37,6 +37,9 @@ public class OutWindow {
   }
 
   public void Flush() throws IOException {
+    if (_stream == null) {
+      throw new IOException("Stream is null");
+    }
     int size = _pos - _streamPos;
     if (size == 0) return;
     _stream.write(_buffer, _streamPos, size);
