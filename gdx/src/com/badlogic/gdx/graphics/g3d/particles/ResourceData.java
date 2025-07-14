dc91945ebcae2e5aa0f9f2a91270cf46a27ad0f1
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 15:48:21 -0700

    Revert "fix: 6 - ASSIGN_FIELD_NULLABLE - this.filename = filename; - assigning @Nullable expression to @NonNull field"
    This reverts commit e800cf5b7e207e19043da41443a08f2887b214f6.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/ResourceData.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/ResourceData.java
@@ -125,9 +125,6 @@ public class ResourceData<T> implements Json.Serializable {
     public AssetData() {}
 
     public AssetData(@Nullable String filename, Class<T> type) {
-      if (filename == null) {
-        throw new IllegalArgumentException("filename cannot be null");
-      }
       this.filename = filename;
       this.type = type;
     }
