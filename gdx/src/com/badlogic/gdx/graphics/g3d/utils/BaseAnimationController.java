8e1b2bccadcf54695cd3f20a355cea0c94f772ac
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:36:17 -0700

    Revert "fix: 288 - PASS_NULLABLE - applyNodeAnimationBlending(nodeAnim, out, pool, alpha, time); - passing @Nullable parameter 'pool' where @NonNull is required"
    This reverts commit dfbb0c852606003a73d1f4a1cf2e016f7d126cba.

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/BaseAnimationController.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/BaseAnimationController.java
@@ -319,9 +319,6 @@ public class BaseAnimationController {
       for (final NodeAnimation nodeAnim : animation.nodeAnimations)
         applyNodeAnimationDirectly(nodeAnim, time);
     } else {
-      if (pool == null) {
-        throw new IllegalArgumentException("Parameter 'pool' cannot be null");
-      }
       for (final Node node : out.keys()) node.isAnimated = false;
       for (final NodeAnimation nodeAnim : animation.nodeAnimations)
         applyNodeAnimationBlending(nodeAnim, out, pool, alpha, time);
