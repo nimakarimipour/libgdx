bb56696cc6aaa46d694e1fc1e796259d86631b00
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:01:37 -0700

    Revert "fix: 84 - DEREFERENCE_NULLABLE - this.r = color.r; - dereferenced expression color is @Nullable"
    This reverts commit a78583dde0ece3a821adece6ae0f35d7d76e1222.

--- gdx/src/com/badlogic/gdx/graphics/Color.java
+++ gdx/src/com/badlogic/gdx/graphics/Color.java
@@ -115,9 +115,6 @@ public class Color {
    * @param color the Color
    */
   public Color set(@Nullable Color color) {
-    if (color == null) {
-      throw new IllegalArgumentException("color cannot be null");
-    }
     this.r = color.r;
     this.g = color.g;
     this.b = color.b;
