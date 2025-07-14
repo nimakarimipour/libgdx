7f393c65aea20700a71a73ededb1c0817fbbecbb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:39:28 -0700

    Revert "fix: 307 - PASS_NULLABLE - return spaceTop.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required"
    This reverts commit 17367cd15d42e781ee1114bb58593426e90bb166.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -818,9 +818,6 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getSpaceTop() {
-    if (actor == null || spaceTop == null) {
-      throw new NullPointerException("actor and spaceTop must not be null");
-    }
     return spaceTop.get(actor);
   }
 
