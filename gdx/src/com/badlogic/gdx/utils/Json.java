9ac3f06227fa4c7b44d758fc4b08ec42b6efd9bc
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:35:25 -0700

    Revert "fix: 283 - ASSIGN_FIELD_NULLABLE - this.writer = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit ffc77268aaf11604ff3f599864158953c383cc53.

--- gdx/src/com/badlogic/gdx/utils/Json.java
+++ gdx/src/com/badlogic/gdx/utils/Json.java
@@ -344,7 +344,7 @@ public class Json {
       writeValue(object, knownType, elementType);
     } finally {
       StreamUtils.closeQuietly(this.writer);
-      this.writer = new JsonWriter(new StringWriter());
+      this.writer = null;
     }
   }
 
