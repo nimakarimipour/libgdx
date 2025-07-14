b6bc134a1e4dbfabf5400e11ed72ae0d40757244
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:32:19 -0700

    fix: 265 - DEREFERENCE_NULLABLE - if (current.size > 0) lastChild.pop(); - dereferenced expression current is @Nullable

--- gdx/src/com/badlogic/gdx/utils/JsonReader.java
+++ gdx/src/com/badlogic/gdx/utils/JsonReader.java
@@ -739,7 +739,7 @@ public class JsonReader implements BaseJsonReader {
 
   protected void pop() {
     root = elements.pop();
-    if (current.size > 0) lastChild.pop();
+    if (current != null && current.size > 0) lastChild.pop();
     current = elements.size > 0 ? elements.peek() : null;
   }
 
