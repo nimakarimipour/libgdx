802600adaf1e0bdc1fb40284f4ee9cb3af9133bd
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:17:45 -0700

    Revert "fix: 178 - ASSIGN_FIELD_NULLABLE - textureDescription.texture = texture; - assigning @Nullable expression to @NonNull field"
    This reverts commit dfc1efe342a3603e637886ee7b4641566597ce97.

--- gdx/src/com/badlogic/gdx/graphics/g3d/attributes/TextureAttribute.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/attributes/TextureAttribute.java
@@ -157,9 +157,7 @@ public class TextureAttribute extends Attribute {
 
   public TextureAttribute(final long type, @Nullable final Texture texture) {
     this(type);
-    if (texture != null) {
-      textureDescription.texture = texture;
-    }
+    textureDescription.texture = texture;
   }
 
   public TextureAttribute(final long type, final TextureRegion region) {
