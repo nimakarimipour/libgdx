11290da3d1dc4a6eab7ff81bd5dbced33f629d4d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:24:05 -0700

    fix: 216 - FIELD_NO_INIT - public FloatChannel positionChannel; - @NonNull field positionChannel not initialized

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerRenderData.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerRenderData.java
@@ -26,5 +26,5 @@ import com.badlogic.gdx.graphics.g3d.particles.ParticleController;
  */
 public abstract class ParticleControllerRenderData {
   public ParticleController controller;
-  public FloatChannel positionChannel;
+  public FloatChannel positionChannel = new FloatChannel();
 }
