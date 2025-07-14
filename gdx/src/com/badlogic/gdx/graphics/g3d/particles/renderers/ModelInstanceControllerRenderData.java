8a0e8fc52afa03160d49f0a3fda15f775d85b1d9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:48:53 -0700

    Revert "fix: 364 - FIELD_NO_INIT - public ObjectChannel<ModelInstance> modelInstanceChannel; - @NonNull field modelInstanceChannel not initialized"
    This reverts commit ddad4aeb3aa3a423e0f2c3d647e7b0e501a16b27.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ModelInstanceControllerRenderData.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ModelInstanceControllerRenderData.java
@@ -27,6 +27,6 @@ import javax.annotation.Nullable;
  * @author Inferno
  */
 public class ModelInstanceControllerRenderData extends ParticleControllerRenderData {
-  public ObjectChannel<ModelInstance> modelInstanceChannel = new ObjectChannel<>();
+  public ObjectChannel<ModelInstance> modelInstanceChannel;
   @Nullable public FloatChannel colorChannel, scaleChannel, rotationChannel;
 }
