d6e693aa90888dc6b43b124447d206ccb1cb9afd
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:50:03 -0700

    Revert "fix: 371 - METHOD_NO_INIT - public Angular(Angular value) { - initializer method does not guarantee @NonNull field angularChannel (line 144) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 3e0101604fb411c019e7fb8f7ff3836cf0016141.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
@@ -160,9 +160,6 @@ public abstract class DynamicsModifier extends Influencer {
       phiValue = new ScaledNumericValue();
       thetaValue.load(value.thetaValue);
       phiValue.load(value.phiValue);
-      if (value.angularChannel != null) {
-        allocateChannels();
-      }
     }
 
     @Override
