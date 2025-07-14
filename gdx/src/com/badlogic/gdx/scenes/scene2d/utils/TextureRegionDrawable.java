06383610f50c4019baf1bb411a46199a812175da
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:33:20 -0700

    fix: 271 - METHOD_NO_INIT - public TextureRegionDrawable(TextureRegionDrawable drawable) { - initializer method does not guarantee @NonNull field region (line 33) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/TextureRegionDrawable.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/TextureRegionDrawable.java
@@ -45,6 +45,9 @@ public class TextureRegionDrawable extends BaseDrawable implements TransformDraw
 
   public TextureRegionDrawable(TextureRegionDrawable drawable) {
     super(drawable);
+    if (drawable.region == null) {
+      throw new IllegalArgumentException("Drawable region cannot be null");
+    }
     setRegion(drawable.region);
   }
 
