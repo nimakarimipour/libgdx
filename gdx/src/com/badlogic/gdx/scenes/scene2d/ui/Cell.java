c1034a46e2e439d6816f49b0648447394fd3bcf2
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:10:51 -0700

    Revert "fix: 138 - ASSIGN_FIELD_NULLABLE - spaceLeft = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 821fb1e77a09d8728f3095e5df3a30bb5b3ae6d2.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -987,7 +987,7 @@ public class Cell<T extends Actor> implements Poolable {
     maxWidth = null;
     maxHeight = null;
     spaceTop = null;
-    spaceLeft = 0; // Changed to a default non-null value
+    spaceLeft = null;
     spaceBottom = null;
     spaceRight = null;
     padTop = null;
