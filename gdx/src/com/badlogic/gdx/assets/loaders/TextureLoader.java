1f78bd2835a24b73f93db2fe39215923d91a6e3d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:24:55 -0700

    fix: 221 - DEREFERENCE_NULLABLE - if (!info.data.isPrepared()) info.data.prepare(); - dereferenced expression info.data is @Nullable

--- gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java
@@ -75,7 +75,9 @@ public class TextureLoader
       info.data = parameter.textureData;
       info.texture = parameter.texture;
     }
-    if (!info.data.isPrepared()) info.data.prepare();
+    if (info.data != null && !info.data.isPrepared()) {
+      info.data.prepare();
+    }
   }
 
   @Nullable
