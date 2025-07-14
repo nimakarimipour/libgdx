182ad8d61ad336d90b1db6dab0e5029740f946df
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:58:33 -0700

    Revert "fix: 402 - DEREFERENCE_NULLABLE - Stream.write(temp + LowHi); - dereferenced expression Stream is @Nullable"
    This reverts commit 9b73df0b2b743cd4788e963d5291857bf2083b74.

--- gdx/src/com/badlogic/gdx/utils/compression/rangecoder/Encoder.java
+++ gdx/src/com/badlogic/gdx/utils/compression/rangecoder/Encoder.java
@@ -60,22 +60,18 @@ public class Encoder {
   }
 
   public void ShiftLow() throws IOException {
-    if (Stream != null) {
-      int LowHi = (int) (Low >>> 32);
-      if (LowHi != 0 || Low < 0xFF000000L) {
-        _position += _cacheSize;
-        int temp = _cache;
-        do {
-          Stream.write(temp + LowHi);
-          temp = 0xFF;
-        } while (--_cacheSize != 0);
-        _cache = (((int) Low) >>> 24);
-      }
-      _cacheSize++;
-      Low = (Low & 0xFFFFFF) << 8;
-    } else {
-      throw new IOException("Stream is null");
+    int LowHi = (int) (Low >>> 32);
+    if (LowHi != 0 || Low < 0xFF000000L) {
+      _position += _cacheSize;
+      int temp = _cache;
+      do {
+        Stream.write(temp + LowHi);
+        temp = 0xFF;
+      } while (--_cacheSize != 0);
+      _cache = (((int) Low) >>> 24);
     }
+    _cacheSize++;
+    Low = (Low & 0xFFFFFF) << 8;
   }
 
   public void EncodeDirectBits(int v, int numTotalBits) throws IOException {
