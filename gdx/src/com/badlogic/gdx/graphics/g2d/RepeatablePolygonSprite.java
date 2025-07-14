edbd6672ca98a01ac5734eb99fdf725313849b8d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:29:48 -0700

    Revert "fix: 250 - FIELD_NO_INIT - private TextureRegion region; - @NonNull field region not initialized"
    This reverts commit e677d350ce3e4e11bece04a1309fbd1a626fa613.

--- gdx/src/com/badlogic/gdx/graphics/g2d/RepeatablePolygonSprite.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/RepeatablePolygonSprite.java
@@ -29,7 +29,7 @@ import com.badlogic.gdx.utils.ShortArray;
  */
 public class RepeatablePolygonSprite {
 
-  private TextureRegion region = new TextureRegion();
+  private TextureRegion region;
   private float density;
 
   private boolean dirty = true;
