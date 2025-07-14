bc7b90dc839da2e4733c61f584eb04e74b6e74f4
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:56:27 -0700

    Revert "fix: 390 - FIELD_NO_INIT - public static GL20 gl; - @NonNull static field gl not initialized"
    This reverts commit 853a52b7da1ec2cf5eb58a8b98d78602cfff843a.

--- gdx/src/com/badlogic/gdx/Gdx.java
+++ gdx/src/com/badlogic/gdx/Gdx.java
@@ -38,9 +38,7 @@ public class Gdx {
   public static Files files;
   @Nullable public static Net net;
 
-  public static GL20 gl = new GL20() {
-        // Implement necessary methods, or initialize it appropriately
-      };
+  public static GL20 gl;
   public static GL20 gl20;
   public static GL30 gl30;
 }
