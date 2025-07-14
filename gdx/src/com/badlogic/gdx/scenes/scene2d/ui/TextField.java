7089796357c42a27a5bbd184786c085b427263ac
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:40:28 -0700

    Revert "fix: 313 - METHOD_NO_INIT - public TextFieldStyle() {} - initializer method does not guarantee @NonNull field font (line 1219) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 1725a33cb0f416609494acf0d358aac7adce066f.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
@@ -1224,10 +1224,7 @@ public class TextField extends Widget implements Disableable {
     @Nullable public @Null BitmapFont messageFont;
     @Nullable public @Null Color messageFontColor;
 
-    public TextFieldStyle() {
-      this.font = new BitmapFont();
-      this.fontColor = new Color();
-    }
+    public TextFieldStyle() {}
 
     public TextFieldStyle(
         BitmapFont font,
