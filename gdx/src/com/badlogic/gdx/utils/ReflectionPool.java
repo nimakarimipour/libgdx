80f32b9c23d54d1097fb4e5f27b31b4be95d604f
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:34:33 -0700

    Revert "fix: 278 - PASS_NULLABLE - return ClassReflection.getConstructor(type, (Class[]) null); - passing @Nullable parameter '(Class[]) null' where @NonNull is required"
    This reverts commit 458098a2f567f8c9ecfd1dd4bbcd9deea3de68ee.

--- gdx/src/com/badlogic/gdx/utils/ReflectionPool.java
+++ gdx/src/com/badlogic/gdx/utils/ReflectionPool.java
@@ -50,10 +50,10 @@ public class ReflectionPool<T> extends Pool<T> {
   @Nullable
   private @Null Constructor findConstructor(Class<T> type) {
     try {
-      return ClassReflection.getConstructor(type);
+      return ClassReflection.getConstructor(type, (Class[]) null);
     } catch (Exception ex1) {
       try {
-        Constructor constructor = ClassReflection.getDeclaredConstructor(type);
+        Constructor constructor = ClassReflection.getDeclaredConstructor(type, (Class[]) null);
         constructor.setAccessible(true);
         return constructor;
       } catch (ReflectionException ex2) {
