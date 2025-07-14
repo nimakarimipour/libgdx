a8d493b8d576fbe784c67e4452a8df4034cf04ea
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:38:48 -0700

    Revert "fix: 303 - DEREFERENCE_NULLABLE - else if (loadFuture.isDone()) { - dereferenced expression loadFuture is @Nullable"
    This reverts commit 26024b23de05cf541682e514cbc8f28591e826d8.

--- gdx/src/com/badlogic/gdx/assets/AssetLoadingTask.java
+++ gdx/src/com/badlogic/gdx/assets/AssetLoadingTask.java
@@ -146,18 +146,17 @@ class AssetLoadingTask implements AsyncTask<Void> {
               asyncLoader.loadSync(
                   manager, assetDesc.fileName, resolve(loader, assetDesc), assetDesc.params);
       }
-    } else if (loadFuture == null && !asyncDone) {
-      loadFuture = executor.submit(this);
-    } else if (loadFuture != null && loadFuture.isDone()) {
+    } else if (loadFuture == null && !asyncDone) loadFuture = executor.submit(this);
+    else if (asyncDone)
+      asset =
+          asyncLoader.loadSync(
+              manager, assetDesc.fileName, resolve(loader, assetDesc), assetDesc.params);
+    else if (loadFuture.isDone()) {
       try {
         loadFuture.get();
       } catch (Exception e) {
         throw new GdxRuntimeException("Couldn't load asset: " + assetDesc.fileName, e);
       }
-      asset =
-          asyncLoader.loadSync(
-              manager, assetDesc.fileName, resolve(loader, assetDesc), assetDesc.params);
-    } else if (asyncDone) {
       asset =
           asyncLoader.loadSync(
               manager, assetDesc.fileName, resolve(loader, assetDesc), assetDesc.params);
