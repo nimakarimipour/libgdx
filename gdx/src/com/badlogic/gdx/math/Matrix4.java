41aaaacd0932bec4bc682d29b755b917952773df
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:43:00 -0700

    Revert "fix: 328 - DEREFERENCE_NULLABLE - return set(matrix.val); - dereferenced expression matrix is @Nullable"
    This reverts commit 8eba20e123e7e6818bcd28746ec4d0a228c485ef.

--- gdx/src/com/badlogic/gdx/math/Matrix4.java
+++ gdx/src/com/badlogic/gdx/math/Matrix4.java
@@ -192,9 +192,6 @@ public class Matrix4 implements Serializable {
    * @return This matrix for the purpose of chaining methods together.
    */
   public Matrix4 set(@Nullable Matrix4 matrix) {
-    if (matrix == null) {
-      throw new NullPointerException("Matrix cannot be null");
-    }
     return set(matrix.val);
   }
 
