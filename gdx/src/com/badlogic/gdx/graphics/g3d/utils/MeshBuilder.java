092fd77dd29d7dd7f1ab89216c74c25bc83fa4f7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:40:16 -0700

    fix: 312 - DEREFERENCE_NULLABLE - vertex[posOffset] = pos.x; - dereferenced expression pos is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/MeshBuilder.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/MeshBuilder.java
@@ -637,6 +637,7 @@ public class MeshBuilder implements MeshPartBuilder {
   public short vertex(
       @Nullable Vector3 pos, @Nullable Vector3 nor, @Nullable Color col, @Nullable Vector2 uv) {
     if (vindex > MAX_INDEX) throw new GdxRuntimeException("Too many vertices used");
+    if (pos == null) throw new IllegalArgumentException("Position cannot be null");
 
     vertex[posOffset] = pos.x;
     if (posSize > 1) vertex[posOffset + 1] = pos.y;
@@ -657,7 +658,7 @@ public class MeshBuilder implements MeshPartBuilder {
       if (colSize > 3) vertex[colOffset + 3] = col.a;
     } else if (cpOffset > 0) {
       if (col == null) col = Color.WHITE;
-      vertex[cpOffset] = col.toFloatBits(); // FIXME cache packed color?
+      vertex[cpOffset] = col.toFloatBits();
     }
 
     if (uv != null && uvOffset >= 0) {
