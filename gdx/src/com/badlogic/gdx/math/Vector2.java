51a14a8b01c60d55aa0ebc404a796cefb571f4ad
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:56:59 -0700

    Revert "fix: 393 - DEREFERENCE_NULLABLE - x = v.x; - dereferenced expression v is @Nullable"
    This reverts commit e90b23240c48e6f80987dcd0e3ab8fa94827b808.

--- gdx/src/com/badlogic/gdx/math/Vector2.java
+++ gdx/src/com/badlogic/gdx/math/Vector2.java
@@ -87,9 +87,6 @@ public class Vector2 implements Serializable, Vector<Vector2> {
 
   @Override
   public Vector2 set(@Nullable Vector2 v) {
-    if (v == null) {
-      throw new IllegalArgumentException("Vector2 argument cannot be null");
-    }
     x = v.x;
     y = v.y;
     return this;
