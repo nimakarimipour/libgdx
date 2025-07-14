6e04a3c94d12838b0efff31280289afe4890192e
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:46:21 -0700

    fix: 348 - FIELD_NO_INIT - protected Actor target; - @NonNull field target not initialized

--- gdx/src/com/badlogic/gdx/scenes/scene2d/Action.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/Action.java
@@ -33,7 +33,7 @@ public abstract class Action implements Poolable {
   @Nullable protected Actor actor;
 
   /** The actor this action targets, or null if a target has not been set. */
-  protected Actor target;
+  protected Actor target = new Actor();
 
   @Nullable private @Null Pool pool;
 
