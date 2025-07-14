12f8f7ea6cd08746d5d8a39ce90308ca4ac62332
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:08:04 -0700

    fix: 122 - ASSIGN_FIELD_NULLABLE - this.name = name; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -1067,6 +1067,9 @@ public class JsonValue implements Iterable<JsonValue> {
    * @param name May be null.
    */
   public void setName(@Nullable @Null String name) {
+    if (name == null) {
+      throw new IllegalArgumentException("name cannot be null");
+    }
     this.name = name;
   }
 
