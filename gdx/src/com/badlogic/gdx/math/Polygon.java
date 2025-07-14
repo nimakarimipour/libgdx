ea65186589bbe0a094926807c654453d38eaf1df
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:38:38 -0700

    Revert "fix: 302 - RETURN_NULLABLE - if (!dirty) return worldVertices; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit d2ebc69df4a268341ea5389e6adcbd7ade87b45f.

--- gdx/src/com/badlogic/gdx/math/Polygon.java
+++ gdx/src/com/badlogic/gdx/math/Polygon.java
@@ -64,7 +64,7 @@ public class Polygon implements Shape2D {
    * @return vertices scaled, rotated, and offset by the polygon position.
    */
   public float[] getTransformedVertices() {
-    if (!dirty) return worldVertices != null ? worldVertices : new float[0];
+    if (!dirty) return worldVertices;
     dirty = false;
 
     final float[] localVertices = this.localVertices;
