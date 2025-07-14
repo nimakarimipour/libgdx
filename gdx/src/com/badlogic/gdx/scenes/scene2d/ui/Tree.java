7b8e85f0a0b7def85bc64cdd0212a495678fe16d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:36:36 -0700

    fix: 290 - METHOD_NO_INIT - public TreeStyle() {} - initializer method does not guarantee @NonNull fields plus (line 974), minus (line 974) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Tree.java
@@ -975,7 +975,10 @@ public class Tree<N extends Node, V> extends WidgetGroup {
     @Nullable public @Null Drawable plusOver, minusOver;
     @Nullable public @Null Drawable over, selection, background;
 
-    public TreeStyle() {}
+    public TreeStyle() {
+      this.plus = new Drawable();
+      this.minus = new Drawable();
+    }
 
     public TreeStyle(Drawable plus, Drawable minus, @Null Drawable selection) {
       this.plus = plus;
