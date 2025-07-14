c1a3688f45fcc7cdc024a716a6f62cfff5868e4f
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:07:32 -0700

    fix: 119 - ASSIGN_FIELD_NULLABLE - this.program = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/g3d/shaders/DefaultShader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/shaders/DefaultShader.java
@@ -847,6 +847,9 @@ public class DefaultShader extends BaseShader {
   @Override
   public void init() {
     final ShaderProgram program = this.program;
+    if (program == null) {
+      return; // or handle the null case appropriately instead of setting this.program to null
+    }
     this.program = null;
     init(program, renderable);
     renderable = null;
