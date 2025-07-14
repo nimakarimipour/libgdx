516bef58c1dc95c85e9f8566b6b4429f09ca332c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:17:54 -0700

    fix: 179 - DEREFERENCE_NULLABLE - controllerChannel.data[i].draw(); - dereferenced expression controllerChannel is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerControllerRenderer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerControllerRenderer.java
@@ -44,8 +44,10 @@ public class ParticleControllerControllerRenderer extends ParticleControllerRend
 
   @Override
   public void update() {
-    for (int i = 0, c = controller.particles.size; i < c; ++i) {
-      controllerChannel.data[i].draw();
+    if (controllerChannel != null) {
+      for (int i = 0, c = controller.particles.size; i < c; ++i) {
+        controllerChannel.data[i].draw();
+      }
     }
   }
 
