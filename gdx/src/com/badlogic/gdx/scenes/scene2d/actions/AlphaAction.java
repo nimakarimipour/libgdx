e0863712cffa0ce6993e8171fbc075f77413acaa
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:22:16 -0700

    Revert "fix: 205 - FIELD_NO_INIT - private @Null Color color; - @NonNull field color not initialized"
    This reverts commit 5b4efd1698f9d5ac44cc92d0984b51cca499bcb3.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/AlphaAction.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/AlphaAction.java
@@ -29,7 +29,7 @@ import com.badlogic.gdx.utils.Null;
  */
 public class AlphaAction extends TemporalAction {
   private float start, end;
-  private @NonNull Color color = new Color();
+  private @Null Color color;
 
   protected void begin() {
     if (color == null) color = target.getColor();
