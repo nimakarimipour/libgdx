6b90bcee7d32872c62b7554f49ae2491bf08e336
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 15:53:36 -0700

    Revert "fix: 37 - ASSIGN_FIELD_NULLABLE - this.a = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit d197f1f648c60ac0879e330ece6c61500efcdaf4.

--- gdx/src/com/badlogic/gdx/utils/TimSort.java
+++ gdx/src/com/badlogic/gdx/utils/TimSort.java
@@ -122,8 +122,9 @@ class TimSort<T> {
     stackSize = 0;
     rangeCheck(a.length, lo, hi);
     int nRemaining = hi - lo;
-    if (nRemaining < 2) return;
+    if (nRemaining < 2) return; // Arrays of size 0 and 1 are always sorted
 
+    // If array is small, do a "mini-TimSort" with no merges
     if (nRemaining < MIN_MERGE) {
       int initRunLen = countRunAndMakeAscending(a, lo, hi, c);
       binarySort(a, lo, hi, lo + initRunLen, c);
@@ -134,25 +135,37 @@ class TimSort<T> {
     this.c = c;
     tmpCount = 0;
 
+    /**
+     * March over the array once, left to right, finding natural runs, extending short natural runs
+     * to minRun elements, and merging runs to maintain stack invariant.
+     */
     int minRun = minRunLength(nRemaining);
     do {
+      // Identify next run
       int runLen = countRunAndMakeAscending(a, lo, hi, c);
+
+      // If run is short, extend to min(minRun, nRemaining)
       if (runLen < minRun) {
         int force = nRemaining <= minRun ? nRemaining : minRun;
         binarySort(a, lo, lo + force, lo + runLen, c);
         runLen = force;
       }
+
+      // Push run onto pending-run stack, and maybe merge
       pushRun(lo, runLen);
       mergeCollapse();
+
+      // Advance to find next run
       lo += runLen;
       nRemaining -= runLen;
     } while (nRemaining != 0);
 
+    // Merge all remaining runs to complete sort
     if (DEBUG) assert lo == hi;
     mergeForceCollapse();
     if (DEBUG) assert stackSize == 1;
 
-    this.a = a.length > 0 ? a : null;
+    this.a = null;
     this.c = null;
     T[] tmp = this.tmp;
     for (int i = 0, n = tmpCount; i < n; i++) tmp[i] = null;
