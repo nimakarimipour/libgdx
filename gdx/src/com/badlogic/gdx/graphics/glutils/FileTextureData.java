d6faab476f0b12d2a3220cc1e7a821d56ebcd144
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:58:22 -0700

    Revert "fix: 401 - WRONG_OVERRIDE_RETURN - @Nullable - method returns @Nullable, but superclass method com.badlogic.gdx.graphics.TextureData.consumePixmap() returns @NonNull"
    This reverts commit 7ddbc325b8bf4f622aca62b79d1a9269bcc4b5e4.

--- gdx/src/com/badlogic/gdx/graphics/glutils/FileTextureData.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/FileTextureData.java
@@ -70,7 +70,6 @@ public class FileTextureData implements TextureData {
   public Pixmap consumePixmap() {
     if (!isPrepared) throw new GdxRuntimeException("Call prepare() before calling getPixmap()");
     isPrepared = false;
-    if (this.pixmap == null) throw new GdxRuntimeException("Pixmap cannot be null");
     Pixmap pixmap = this.pixmap;
     this.pixmap = null;
     return pixmap;
