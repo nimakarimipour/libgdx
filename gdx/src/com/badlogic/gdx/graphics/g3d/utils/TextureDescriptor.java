83c3b47005e3da7df809d8e10de9a39f33918beb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:38:58 -0700

    Revert "fix: 304 - ASSIGN_FIELD_NULLABLE - public T texture = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit a74b8e11afb5870f696b385657a9535d0f263862.

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/TextureDescriptor.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/TextureDescriptor.java
@@ -22,17 +22,7 @@ import com.uber.nullaway.annotations.Initializer;
 import javax.annotation.Nullable;
 
 public class TextureDescriptor<T extends GLTexture> implements Comparable<TextureDescriptor<T>> {
-  public T texture;
-
-  public TextureDescriptor(
-      final @Nullable T texture,
-      final Texture.TextureFilter minFilter,
-      final Texture.TextureFilter magFilter,
-      final Texture.TextureWrap uWrap,
-      final Texture.TextureWrap vWrap) {
-    set(texture, minFilter, magFilter, uWrap, vWrap);
-  }
-
+  public T texture = null;
   @Nullable public Texture.TextureFilter minFilter;
   @Nullable public Texture.TextureFilter magFilter;
   @Nullable public Texture.TextureWrap uWrap;
