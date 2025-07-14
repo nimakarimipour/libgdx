29a8af3744f57d2e45fdb3677dbdd43e848d242c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:09:58 -0700

    fix: 133 - DEREFERENCE_NULLABLE - float spriteWidth = sprite.getWidth(); - dereferenced expression sprite is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/ParticleEmitter.java
@@ -405,6 +405,8 @@ public class ParticleEmitter {
         break;
     }
 
+    if (sprite == null) return; // Ensure 'sprite' is not null
+
     Particle particle = particles[index];
     if (particle == null) {
       particles[index] = particle = newParticle(sprite);
