c1fe6aeda600530c6cca1b9960d755f61ad13fa6
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:28:47 -0700

    Revert "fix: 244 - FIELD_NO_INIT - public E value; - @NonNull field SortedIntList$Node.value not initialized"
    This reverts commit 4afce70cc6e709ed17b6465dc4694b800158abea.

--- gdx/src/com/badlogic/gdx/utils/SortedIntList.java
+++ gdx/src/com/badlogic/gdx/utils/SortedIntList.java
@@ -192,7 +192,7 @@ public class SortedIntList<E> implements Iterable<SortedIntList.Node<E>> {
     @Nullable protected Node<E> n;
 
     /** Value held */
-    public @NonNull E value;
+    public E value;
 
     /** Index value in list */
     public int index;
