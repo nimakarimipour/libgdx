fdd9cfc1a49c2901674df5dad264d1ebfa6e3480
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 16:13:26 -0700

    Revert "fix: 153 - METHOD_NO_INIT - public Button(ButtonStyle style) { - initializer method does not guarantee @NonNull field style (line 50) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit b2ed8f771f72faba97fccca466d0f96825b3d0e3.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Button.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Button.java
@@ -80,9 +80,6 @@ public class Button extends Table implements Disableable {
   }
 
   public Button(ButtonStyle style) {
-    if (style == null) {
-      throw new IllegalArgumentException("ButtonStyle must not be null");
-    }
     initialize();
     setStyle(style);
     setSize(getPrefWidth(), getPrefHeight());
