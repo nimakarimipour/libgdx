515d3845a026b55ab6e04957f6a0303fe813dbdc
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 17:02:44 -0700

    Revert "fix: 426 - METHOD_NO_INIT - public BitmapFontCache(BitmapFont font, boolean integer) { - initializer method does not guarantee @NonNull field pageGlyphIndices (line 61) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 039dc4e30f008b21d0a3087ee5407d475cf428f0.

--- gdx/src/com/badlogic/gdx/graphics/g2d/BitmapFontCache.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/BitmapFontCache.java
@@ -82,9 +82,9 @@ public class BitmapFontCache {
 
     pageVertices = new float[pageCount][];
     idx = new int[pageCount];
-    // Initialize pageGlyphIndices unconditionally
-    pageGlyphIndices = new IntArray[pageCount];
     if (pageCount > 1) {
+      // Contains the indices of the glyph in the cache as they are added.
+      pageGlyphIndices = new IntArray[pageCount];
       for (int i = 0, n = pageGlyphIndices.length; i < n; i++) pageGlyphIndices[i] = new IntArray();
     }
     tempGlyphCount = new int[pageCount];
