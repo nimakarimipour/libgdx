f9994c493278744b83573e26fe8df9d7410bcf68
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 17:01:50 -0700

    fix: 421 - DEREFERENCE_NULLABLE - current.setAttribute(name, value); - dereferenced expression current is @Nullable

--- gdx/src/com/badlogic/gdx/utils/XmlReader.java
+++ gdx/src/com/badlogic/gdx/utils/XmlReader.java
@@ -457,7 +457,9 @@ public class XmlReader {
   }
 
   protected void attribute(@Nullable String name, @Nullable String value) {
-    current.setAttribute(name, value);
+    if (current != null) {
+      current.setAttribute(name, value);
+    }
   }
 
   @Nullable
