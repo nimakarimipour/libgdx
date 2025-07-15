d86d691b047f8e9d7a8cd49ec43d2e0a9353daab
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 17:00:38 -0700

    fix: 414 - ASSIGN_FIELD_NULLABLE - animation.id = anim.id; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
@@ -135,7 +135,9 @@ public class Model implements Disposable {
   protected void loadAnimations(Iterable<ModelAnimation> modelAnimations) {
     for (final ModelAnimation anim : modelAnimations) {
       Animation animation = new Animation();
-      animation.id = anim.id;
+      if (anim.id != null) {
+        animation.id = anim.id;
+      }
       for (ModelNodeAnimation nanim : anim.nodeAnimations) {
         final Node node = getNode(nanim.nodeId);
         if (node == null) continue;
