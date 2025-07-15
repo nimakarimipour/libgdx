557d4b33c49f053eb375c54bbf5341ee38a0974e
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 17:00:17 -0700

    fix: 412 - ASSIGN_FIELD_NULLABLE - renderable.material = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/shapebuilders/RenderableShapeBuilder.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/shapebuilders/RenderableShapeBuilder.java
@@ -46,7 +46,8 @@ public class RenderableShapeBuilder extends BaseShapeBuilder {
     public Renderable obtain() {
       Renderable renderable = super.obtain();
       renderable.environment = null;
-      renderable.material = null;
+      // Assuming `material` needs to be non-null, you can assign a default value.
+      renderable.material = new Material();
       renderable.meshPart.set("", null, 0, 0, 0);
       renderable.shader = null;
       renderable.userData = null;
