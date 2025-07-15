f7e5b57aae1b3553705445df1f128660bef4d725
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 17:02:32 -0700

    fix: 425 - PASS_NULLABLE - return c1.isAssignableFrom(c2); - passing @Nullable parameter 'c2' where @NonNull is required

--- gdx/src/com/badlogic/gdx/utils/reflect/ClassReflection.java
+++ gdx/src/com/badlogic/gdx/utils/reflect/ClassReflection.java
@@ -56,6 +56,9 @@ public final class ClassReflection {
    * Class parameter.
    */
   public static boolean isAssignableFrom(Class c1, @Nullable Class c2) {
+    if (c2 == null) {
+      throw new IllegalArgumentException("Parameter 'c2' cannot be null");
+    }
     return c1.isAssignableFrom(c2);
   }
 
