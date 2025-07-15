ff8c4a2ca5bbd768b233b0021f321c14cae885f9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 16:38:34 -0700

    before:411to410 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression root is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/maps/tiled/TideMapLoader.java:116     Element properties = root.getChildByName("Properties");

--- gdx/src/com/badlogic/gdx/maps/tiled/TideMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/TideMapLoader.java
@@ -111,22 +111,33 @@ public class TideMapLoader extends SynchronousAssetLoader<TiledMap, TideMapLoade
    * @return the {@link TiledMap}
    */
   private TiledMap loadMap(
-      @Nullable Element root, FileHandle tmxFile, ImageResolver imageResolver) {
-    TiledMap map = new TiledMap();
-    Element properties = root.getChildByName("Properties");
-    if (properties != null) {
-      loadProperties(map.getProperties(), properties);
+         @Nullable Element root, FileHandle tmxFile, ImageResolver imageResolver) {
+      if (root == null) {
+        throw new IllegalArgumentException("Root element must not be null");
+      }
+  
+      TiledMap map = new TiledMap();
+      Element properties = root.getChildByName("Properties");
+      if (properties != null) {
+        loadProperties(map.getProperties(), properties);
+      }
+  
+      Element tilesheets = root.getChildByName("TileSheets");
+      if (tilesheets != null) {
+        for (Element tilesheet : tilesheets.getChildrenByName("TileSheet")) {
+          loadTileSheet(map, tilesheet, tmxFile, imageResolver);
+        }
+      }
+  
+      Element layers = root.getChildByName("Layers");
+      if (layers != null) {
+        for (Element layer : layers.getChildrenByName("Layer")) {
+          loadLayer(map, layer);
+        }
+      }
+  
+      return map;
     }
-    Element tilesheets = root.getChildByName("TileSheets");
-    for (Element tilesheet : tilesheets.getChildrenByName("TileSheet")) {
-      loadTileSheet(map, tilesheet, tmxFile, imageResolver);
-    }
-    Element layers = root.getChildByName("Layers");
-    for (Element layer : layers.getChildrenByName("Layer")) {
-      loadLayer(map, layer);
-    }
-    return map;
-  }
 
   /**
    * Loads the tilesets
