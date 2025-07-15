c345a49be7e0d98fe64caa00aa37fcb633eef64d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 16:40:20 -0700

    before:405to404 - Type='PASS_NULLABLE', message='passing @Nullable parameter 'actor' where @NonNull is required' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java:777     return prefWidth.get(actor);

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -774,8 +774,11 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getPrefWidth() {
-    return prefWidth.get(actor);
-  }
+        if (actor == null) {
+            throw new IllegalArgumentException("actor cannot be null");
+        }
+        return prefWidth.get(actor);
+    }
 
   /**
    * @return May be null if this cell is row defaults.
