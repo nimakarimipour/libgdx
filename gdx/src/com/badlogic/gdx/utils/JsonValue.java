38c6e650e28de380b04bed7d3b875c12f67c3113
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 17:00:58 -0700

    fix: 416 - FIELD_NO_INIT - public OutputType outputType; - @NonNull field JsonValue$PrettyPrintSettings.outputType not initialized

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -1495,7 +1495,7 @@ public class JsonValue implements Iterable<JsonValue> {
   }
 
   public static class PrettyPrintSettings {
-    public OutputType outputType;
+    public OutputType outputType = OutputType.DEFAULT_VALUE;
 
     /** If an object on a single line fits this many columns, it won't wrap. */
     public int singleLineColumns;
