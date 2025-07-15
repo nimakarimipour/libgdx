b4860735778b7772f3869cf81e387b62478d1bd1
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-14 17:02:02 -0700

    Revert "fix: 422 - FIELD_NO_INIT - public static GL30 gl30; - @NonNull static field gl30 not initialized"
    This reverts commit 8efcd45c712f0fc147fe4efd8190ffa19f96318c.

--- gdx/src/com/badlogic/gdx/Gdx.java
+++ gdx/src/com/badlogic/gdx/Gdx.java
@@ -40,8 +40,5 @@ public class Gdx {
 
   public static GL20 gl;
   public static GL20 gl20;
-  public static GL30 gl30 = new GL30() {
-        // Implement necessary methods here as anonymous inner class, or if GL30 is an interface,
-        // consider initializing it properly with a concrete class instance.
-      };
+  public static GL30 gl30;
 }
