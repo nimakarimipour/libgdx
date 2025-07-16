424ec9849186eeb6b0a1928c07e0fe4218b3376f
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:16:02 -0700

    fix: 55 - ASSIGN_FIELD_NULLABLE - constructor = findConstructor(type); - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/utils/ReflectionPool.java
+++ gdx/src/com/badlogic/gdx/utils/ReflectionPool.java
@@ -19,6 +19,7 @@ package com.badlogic.gdx.utils;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
 import com.badlogic.gdx.utils.reflect.Constructor;
 import com.badlogic.gdx.utils.reflect.ReflectionException;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -29,7 +30,7 @@ import javax.annotation.Nullable;
  * @author Nathan Sweet
  */
 public class ReflectionPool<T> extends Pool<T> {
-  private final Constructor constructor;
+  @Nullable private final Constructor constructor;
 
   public ReflectionPool(Class<T> type) {
     this(type, 16, Integer.MAX_VALUE);
@@ -64,10 +65,16 @@ public class ReflectionPool<T> extends Pool<T> {
 
   protected T newObject() {
     try {
+      if (constructor == null) {
+        throw new IllegalStateException("Constructor is not initialized.");
+      }
       return (T) constructor.newInstance((Object[]) null);
     } catch (Exception ex) {
-      throw new GdxRuntimeException(
-          "Unable to create new instance: " + constructor.getDeclaringClass().getName(), ex);
+      String className =
+          Nullability.castToNonnull(constructor, "checked before catch")
+              .getDeclaringClass()
+              .getName();
+      throw new GdxRuntimeException("Unable to create new instance: " + className, ex);
     }
   }
 }
