5b2bc3bd38e352548857f7ec4f47d50c25b73b80
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:35:22 -0700

    Revert "fix: 119 - ASSIGN_FIELD_NULLABLE - this.program = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit e98814a925591ac12cef76103a3ae64d0b5c7414.

--- gdx/src/com/badlogic/gdx/graphics/g3d/shaders/BaseShader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/shaders/BaseShader.java
@@ -197,7 +197,7 @@ public abstract class BaseShader implements Shader {
 
   /** Initialize this shader, causing all registered uniforms/attributes to be fetched. */
   @Initializer
-  public void init(@Nullable final ShaderProgram program, @Nullable final Renderable renderable) {
+  public void init(final ShaderProgram program, @Nullable final Renderable renderable) {
     if (locations != null) throw new GdxRuntimeException("Already initialized");
     if (!program.isCompiled()) throw new GdxRuntimeException(program.getLog());
     this.program = program;
