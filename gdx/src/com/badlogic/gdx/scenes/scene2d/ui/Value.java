7d6fa942dd5f29d423d656eed2e629bb8b05f126
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:16:10 -0700

    fix: 197 - PASS_NULLABLE - return get(null); - passing @Nullable parameter 'null' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Value.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Value.java
@@ -19,6 +19,7 @@ package com.badlogic.gdx.scenes.scene2d.ui;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.utils.Layout;
 import com.badlogic.gdx.utils.Null;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * Value placeholder, allowing the value to be computed on request. Values can be provided an actor
@@ -30,7 +31,7 @@ import com.badlogic.gdx.utils.Null;
 public abstract class Value {
   /** Calls {@link #get(Actor)} with null. */
   public float get() {
-    return get(null);
+    return get(Nullability.castToNonnull(null));
   }
 
   /**
