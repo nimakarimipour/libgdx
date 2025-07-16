3c8308bf7e15685bf7ef9b25069e5a81b362ff77
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 11:45:12 -0700

    before:310to309 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression controllerChannel is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerControllerRenderer.java:48       controllerChannel.data[i].draw();

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerControllerRenderer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerControllerRenderer.java
@@ -43,11 +43,15 @@ public class ParticleControllerControllerRenderer extends ParticleControllerRend
   }
 
   @Override
-  public void update() {
-    for (int i = 0, c = controller.particles.size; i < c; ++i) {
-      controllerChannel.data[i].draw();
+    public void update() {
+      if (controllerChannel == null) {
+        throw new GdxRuntimeException(
+            "ParticleController channel not initialized. Make sure to call init() before update().");
+      }
+      for (int i = 0, c = controller.particles.size; i < c; ++i) {
+        controllerChannel.data[i].draw();
+      }
     }
-  }
 
   @Override
   public ParticleControllerComponent copy() {
