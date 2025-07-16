72f400809dce1543b7d3b05a6c014548ca4c9171
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 18:21:05 -0700

    before:184to183 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression directionalVelocityChannel is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java:421               c = i + controller.particles.size * directionalVelocityChannel.strideSize;

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
@@ -419,46 +419,50 @@ public abstract class DynamicsModifier extends Influencer {
     }
 
     @Override
-    public void update() {
-      for (int i = 0,
-              l = ParticleChannels.LifePercentOffset,
-              s = 0,
-              a = 0,
-              c = i + controller.particles.size * directionalVelocityChannel.strideSize;
-          i < c;
-          s += strengthChannel.strideSize, i += directionalVelocityChannel.strideSize,
-              a += angularChannel.strideSize, l += lifeChannel.strideSize) {
-
-        float lifePercent = lifeChannel.data[l],
-            strength =
-                strengthChannel.data[s + ParticleChannels.VelocityStrengthStartOffset]
-                    + strengthChannel.data[s + ParticleChannels.VelocityStrengthDiffOffset]
-                        * strengthValue.getScale(lifePercent),
-            phi =
-                angularChannel.data[a + ParticleChannels.VelocityPhiStartOffset]
-                    + angularChannel.data[a + ParticleChannels.VelocityPhiDiffOffset]
-                        * phiValue.getScale(lifePercent),
-            theta =
-                angularChannel.data[a + ParticleChannels.VelocityThetaStartOffset]
-                    + angularChannel.data[a + ParticleChannels.VelocityThetaDiffOffset]
-                        * thetaValue.getScale(lifePercent);
-
-        float cosTheta = MathUtils.cosDeg(theta),
-            sinTheta = MathUtils.sinDeg(theta),
-            cosPhi = MathUtils.cosDeg(phi),
-            sinPhi = MathUtils.sinDeg(phi);
-        TMP_V3.set(cosTheta * sinPhi, cosPhi, sinTheta * sinPhi).nor().scl(strength);
-
-        if (!isGlobal) {
-          controller.transform.getRotation(TMP_Q, true);
-          TMP_V3.mul(TMP_Q);
+        public void update() {
+          if (directionalVelocityChannel == null) {
+            throw new IllegalStateException("DirectionalVelocityChannel is not allocated");
+          }
+          
+          for (int i = 0,
+                  l = ParticleChannels.LifePercentOffset,
+                  s = 0,
+                  a = 0,
+                  c = i + controller.particles.size * directionalVelocityChannel.strideSize;
+              i < c;
+              s += strengthChannel.strideSize, i += directionalVelocityChannel.strideSize,
+                  a += angularChannel.strideSize, l += lifeChannel.strideSize) {
+    
+            float lifePercent = lifeChannel.data[l],
+                strength =
+                    strengthChannel.data[s + ParticleChannels.VelocityStrengthStartOffset]
+                        + strengthChannel.data[s + ParticleChannels.VelocityStrengthDiffOffset]
+                            * strengthValue.getScale(lifePercent),
+                phi =
+                    angularChannel.data[a + ParticleChannels.VelocityPhiStartOffset]
+                        + angularChannel.data[a + ParticleChannels.VelocityPhiDiffOffset]
+                            * phiValue.getScale(lifePercent),
+                theta =
+                    angularChannel.data[a + ParticleChannels.VelocityThetaStartOffset]
+                        + angularChannel.data[a + ParticleChannels.VelocityThetaDiffOffset]
+                            * thetaValue.getScale(lifePercent);
+    
+            float cosTheta = MathUtils.cosDeg(theta),
+                sinTheta = MathUtils.sinDeg(theta),
+                cosPhi = MathUtils.cosDeg(phi),
+                sinPhi = MathUtils.sinDeg(phi);
+            TMP_V3.set(cosTheta * sinPhi, cosPhi, sinTheta * sinPhi).nor().scl(strength);
+    
+            if (!isGlobal) {
+              controller.transform.getRotation(TMP_Q, true);
+              TMP_V3.mul(TMP_Q);
+            }
+    
+            directionalVelocityChannel.data[i + ParticleChannels.XOffset] += TMP_V3.x;
+            directionalVelocityChannel.data[i + ParticleChannels.YOffset] += TMP_V3.y;
+            directionalVelocityChannel.data[i + ParticleChannels.ZOffset] += TMP_V3.z;
+          }
         }
-
-        directionalVelocityChannel.data[i + ParticleChannels.XOffset] += TMP_V3.x;
-        directionalVelocityChannel.data[i + ParticleChannels.YOffset] += TMP_V3.y;
-        directionalVelocityChannel.data[i + ParticleChannels.ZOffset] += TMP_V3.z;
-      }
-    }
 
     @Override
     public PolarAcceleration copy() {
