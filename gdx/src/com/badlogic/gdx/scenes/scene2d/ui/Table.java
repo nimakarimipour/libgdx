87170b49ba922761eee0d76472f75e413ac4a73b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:31:55 -0700

    Revert "fix: 101 - PASS_NULLABLE - float maxWidth = c.maxWidth.get(a), maxHeight = c.maxHeight.get(a); - passing @Nullable parameter 'a' where @NonNull is required"
    This reverts commit 0b06d08ea67e6c0e97e4de20311b698f3e55c55a.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -33,7 +33,6 @@ import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
 import com.uber.nullaway.annotations.Initializer;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Arrays;
 import javax.annotation.Nullable;
 
@@ -907,8 +906,7 @@ public class Table extends WidgetGroup {
       // Determine minimum and preferred cell sizes.
       float prefWidth = c.prefWidth.get(a), prefHeight = c.prefHeight.get(a);
       float minWidth = c.minWidth.get(a), minHeight = c.minHeight.get(a);
-      float maxWidth = c.maxWidth.get(Nullability.castToNonnull(a)),
-          maxHeight = c.maxHeight.get(Nullability.castToNonnull(a));
+      float maxWidth = c.maxWidth.get(a), maxHeight = c.maxHeight.get(a);
       if (prefWidth < minWidth) prefWidth = minWidth;
       if (prefHeight < minHeight) prefHeight = minHeight;
       if (maxWidth > 0 && prefWidth > maxWidth) prefWidth = maxWidth;
