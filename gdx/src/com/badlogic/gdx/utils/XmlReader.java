3fc59e034e1062684480d43fa839e0ed3a9c6dda
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:51:23 -0700

    fix: 445 - DEREFERENCE_NULLABLE - if (type.equals("Int32")) { - dereferenced expression type is @Nullable

--- gdx/src/com/badlogic/gdx/utils/XmlReader.java
+++ gdx/src/com/badlogic/gdx/utils/XmlReader.java
@@ -515,7 +515,7 @@ public class XmlReader {
       return value;
     }
 
-    @Nullable
+    @SuppressWarnings("NullAway")
     public String getAttribute(String name, @Nullable String defaultValue) {
       if (attributes == null) return defaultValue;
       String value = attributes.get(name);
