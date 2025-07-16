0d4a0b3af2b0f8a636bfd812b92d39145d300258
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:01:11 -0700

    Revert "fix: 337 - METHOD_NO_INIT - public LongMap(int initialCapacity, float loadFactor) { - initializer method does not guarantee @NonNull fields entries1 (line 80), entries2 (line 80), values1 (line 81), values2 (line 81), keys1 (line 82), keys2 (line 82) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 5685034bfd1e68ba226672f0bfe9fe95b4779164.

--- gdx/src/com/badlogic/gdx/utils/LongMap.java
+++ gdx/src/com/badlogic/gdx/utils/LongMap.java
@@ -18,7 +18,6 @@ package com.badlogic.gdx.utils;
 
 import static com.badlogic.gdx.utils.ObjectSet.tableSize;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -78,9 +77,9 @@ public class LongMap<V> implements Iterable<LongMap.Entry<V>> {
    */
   protected int mask;
 
-  @Nullable private transient Entries entries1, entries2;
-  @Nullable private transient Values values1, values2;
-  @Nullable private transient Keys keys1, keys2;
+  private transient Entries entries1, entries2;
+  private transient Values values1, values2;
+  private transient Keys keys1, keys2;
 
   /** Creates a new map with an initial capacity of 51 and a load factor of 0.8. */
   public LongMap() {
@@ -503,12 +502,11 @@ public class LongMap<V> implements Iterable<LongMap.Entry<V>> {
     if (!entries1.valid) {
       entries1.reset();
       entries1.valid = true;
-      Nullability.castToNonnull(entries2, "both are initialized");
-      Nullability.castToNonnull(entries2, "both are initialized").valid = false;
+      entries2.valid = false;
       return entries1;
     }
-    Nullability.castToNonnull(entries2, "both are initialized").reset();
-    Nullability.castToNonnull(entries2, "both are initialized").valid = true;
+    entries2.reset();
+    entries2.valid = true;
     entries1.valid = false;
     return entries2;
   }
@@ -529,11 +527,11 @@ public class LongMap<V> implements Iterable<LongMap.Entry<V>> {
     if (!values1.valid) {
       values1.reset();
       values1.valid = true;
-      Nullability.castToNonnull(values2, "initialized when needed").valid = false;
+      values2.valid = false;
       return values1;
     }
-    Nullability.castToNonnull(values2, "initialized when needed").reset();
-    Nullability.castToNonnull(values2, "initialized when needed").valid = true;
+    values2.reset();
+    values2.valid = true;
     values1.valid = false;
     return values2;
   }
@@ -554,11 +552,11 @@ public class LongMap<V> implements Iterable<LongMap.Entry<V>> {
     if (!keys1.valid) {
       keys1.reset();
       keys1.valid = true;
-      Nullability.castToNonnull(keys2, "already initialized").valid = false;
+      keys2.valid = false;
       return keys1;
     }
-    Nullability.castToNonnull(keys2, "already initialized").reset();
-    Nullability.castToNonnull(keys2, "already initialized").valid = true;
+    keys2.reset();
+    keys2.valid = true;
     keys1.valid = false;
     return keys2;
   }
