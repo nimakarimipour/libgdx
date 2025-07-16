eee04f0521b2678f2faa31fa7a1d5ba130012879
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:48:50 -0700

    fix: 301 - DEREFERENCE_NULLABLE - Color spriteColor = sprite.getColor(); - dereferenced expression sprite is @Nullable

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java
@@ -43,6 +43,9 @@ public class SpriteDrawable extends BaseDrawable implements TransformDrawable {
   }
 
   public void draw(Batch batch, float x, float y, float width, float height) {
+    if (sprite == null) {
+      throw new IllegalStateException("sprite cannot be null");
+    }
     Color spriteColor = sprite.getColor();
     float oldColor = spriteColor.toFloatBits();
     sprite.setColor(spriteColor.mul(batch.getColor()));
@@ -67,6 +70,10 @@ public class SpriteDrawable extends BaseDrawable implements TransformDrawable {
       float scaleY,
       float rotation) {
 
+    if (sprite == null) {
+      throw new IllegalStateException("sprite cannot be null");
+    }
+
     Color spriteColor = sprite.getColor();
     float oldColor = spriteColor.toFloatBits();
     sprite.setColor(spriteColor.mul(batch.getColor()));
@@ -81,6 +88,9 @@ public class SpriteDrawable extends BaseDrawable implements TransformDrawable {
   }
 
   public void setSprite(@Nullable Sprite sprite) {
+    if (sprite == null) {
+      throw new IllegalArgumentException("sprite cannot be null");
+    }
     this.sprite = sprite;
     setMinWidth(sprite.getWidth());
     setMinHeight(sprite.getHeight());
