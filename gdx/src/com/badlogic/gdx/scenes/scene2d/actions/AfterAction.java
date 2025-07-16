09e6255c1d06ce81f3d2e8dd68f6621fef0d7d83
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 18:18:25 -0700

    before:191to190 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression action is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/actions/AfterAction.java:51     return action.act(delta);

--- gdx/src/com/badlogic/gdx/scenes/scene2d/actions/AfterAction.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/actions/AfterAction.java
@@ -40,14 +40,14 @@ public class AfterAction extends DelegateAction {
   }
 
   protected boolean delegate(float delta) {
-    Array<Action> currentActions = target.getActions();
-    if (currentActions.size == 1) waitForActions.clear();
-    for (int i = waitForActions.size - 1; i >= 0; i--) {
-      Action action = waitForActions.get(i);
-      int index = currentActions.indexOf(action, true);
-      if (index == -1) waitForActions.removeIndex(i);
+      Array<Action> currentActions = target.getActions();
+      if (currentActions.size == 1) waitForActions.clear();
+      for (int i = waitForActions.size - 1; i >= 0; i--) {
+        Action action = waitForActions.get(i);
+        int index = currentActions.indexOf(action, true);
+        if (index == -1) waitForActions.removeIndex(i);
+      }
+      if (waitForActions.size > 0 || action == null) return false;
+      return action.act(delta);
     }
-    if (waitForActions.size > 0) return false;
-    return action.act(delta);
-  }
 }
