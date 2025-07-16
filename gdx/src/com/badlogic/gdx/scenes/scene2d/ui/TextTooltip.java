36ce0314fc3305ec18b44954476855bbac88a12d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:16:17 -0700

    before:372to371 - Type='PASS_NULLABLE', message='passing @Nullable parameter 'style.label' where @NonNull is required' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java:72     label.setStyle(style.label);

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java
@@ -61,17 +61,18 @@ public class TextTooltip extends Tooltip<Label> {
   }
 
   public void setStyle(TextTooltipStyle style) {
-    if (style == null) throw new NullPointerException("style cannot be null");
-    container.setBackground(style.background);
-    container.maxWidth(style.wrapWidth);
-
-    boolean wrap = style.wrapWidth != 0;
-    container.fill(wrap);
-
-    Label label = container.getActor();
-    label.setStyle(style.label);
-    label.setWrap(wrap);
-  }
+      if (style == null) throw new NullPointerException("style cannot be null");
+      if (style.label == null) throw new NullPointerException("style.label cannot be null");
+      container.setBackground(style.background);
+      container.maxWidth(style.wrapWidth);
+  
+      boolean wrap = style.wrapWidth != 0;
+      container.fill(wrap);
+  
+      Label label = container.getActor();
+      label.setStyle(style.label);
+      label.setWrap(wrap);
+    }
 
   /**
    * The style for a text tooltip, see {@link TextTooltip}.
