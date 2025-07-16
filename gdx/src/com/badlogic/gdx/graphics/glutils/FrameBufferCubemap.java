2f7bf5992e3aea936036f736d2f0dd4468a3a0e1
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 12:47:32 -0700

    before:211to210 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression side is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/glutils/FrameBufferCubemap.java:183         side.glEnum,

--- gdx/src/com/badlogic/gdx/graphics/glutils/FrameBufferCubemap.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/FrameBufferCubemap.java
@@ -180,13 +180,13 @@ public class FrameBufferCubemap extends GLFrameBuffer<Cubemap> {
    *
    * @param side The side to bind
    */
-  protected void bindSide(@Nullable final Cubemap.CubemapSide side) {
-    Gdx.gl20.glFramebufferTexture2D(
-        GL20.GL_FRAMEBUFFER,
-        GL20.GL_COLOR_ATTACHMENT0,
-        side.glEnum,
-        getColorBufferTexture().getTextureObjectHandle(),
-        0);
+  protected void bindSide( @Nullable final Cubemap.CubemapSide side) {
+      Gdx.gl20.glFramebufferTexture2D(
+          GL20.GL_FRAMEBUFFER,
+          GL20.GL_COLOR_ATTACHMENT0,
+          Nullability.castToNonnull(side).glEnum,
+          getColorBufferTexture().getTextureObjectHandle(),
+          0);
   }
 
   /** Get the currently bound side. */
