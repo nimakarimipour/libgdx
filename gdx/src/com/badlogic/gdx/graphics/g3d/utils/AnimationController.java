52e943cbc321766faef91c0531cf4f7ea7b1104a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:41:01 -0700

    before:297to296 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression anim is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/utils/AnimationController.java:706     if (anim.loopCount < 0) throw new GdxRuntimeException("An action cannot be continuous");

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/AnimationController.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/AnimationController.java
@@ -703,15 +703,15 @@ public class AnimationController extends BaseAnimationController {
 
   /** Apply an action animation on top of the current animation. */
   protected AnimationDesc action(@Nullable final AnimationDesc anim, float transitionTime) {
-    if (anim.loopCount < 0) throw new GdxRuntimeException("An action cannot be continuous");
-    if (current == null || current.loopCount == 0) animate(anim, transitionTime);
-    else {
-      AnimationDesc toQueue = inAction ? null : obtain(current);
-      inAction = false;
-      animate(anim, transitionTime);
-      inAction = true;
-      if (toQueue != null) queue(toQueue, transitionTime);
+      if (anim == null || anim.loopCount < 0) throw new GdxRuntimeException("An action cannot be continuous");
+      if (current == null || current.loopCount == 0) animate(anim, transitionTime);
+      else {
+        AnimationDesc toQueue = inAction ? null : obtain(current);
+        inAction = false;
+        animate(anim, transitionTime);
+        inAction = true;
+        if (toQueue != null) queue(toQueue, transitionTime);
+      }
+      return anim;
     }
-    return anim;
-  }
 }
