bb467871cebb5af74de84006d5a5c2d874cf5a9a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 18:19:57 -0700

    before:187to186 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression cell is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java:1021     minWidth = cell.minWidth;

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -1066,31 +1066,33 @@ public class Cell<T extends Actor> implements Poolable {
       set(defaults());
     }
 
-  @Initializer
   void set(@Nullable Cell cell) {
-    minWidth = cell.minWidth;
-    minHeight = cell.minHeight;
-    prefWidth = cell.prefWidth;
-    prefHeight = cell.prefHeight;
-    maxWidth = cell.maxWidth;
-    maxHeight = cell.maxHeight;
-    spaceTop = cell.spaceTop;
-    spaceLeft = cell.spaceLeft;
-    spaceBottom = cell.spaceBottom;
-    spaceRight = cell.spaceRight;
-    padTop = cell.padTop;
-    padLeft = cell.padLeft;
-    padBottom = cell.padBottom;
-    padRight = cell.padRight;
-    fillX = cell.fillX;
-    fillY = cell.fillY;
-    align = cell.align;
-    expandX = cell.expandX;
-    expandY = cell.expandY;
-    colspan = cell.colspan;
-    uniformX = cell.uniformX;
-    uniformY = cell.uniformY;
-  }
+      if (cell == null) {
+        throw new IllegalArgumentException("Cell cannot be null");
+      }
+      minWidth = cell.minWidth;
+      minHeight = cell.minHeight;
+      prefWidth = cell.prefWidth;
+      prefHeight = cell.prefHeight;
+      maxWidth = cell.maxWidth;
+      maxHeight = cell.maxHeight;
+      spaceTop = cell.spaceTop;
+      spaceLeft = cell.spaceLeft;
+      spaceBottom = cell.spaceBottom;
+      spaceRight = cell.spaceRight;
+      padTop = cell.padTop;
+      padLeft = cell.padLeft;
+      padBottom = cell.padBottom;
+      padRight = cell.padRight;
+      fillX = cell.fillX;
+      fillY = cell.fillY;
+      align = cell.align;
+      expandX = cell.expandX;
+      expandY = cell.expandY;
+      colspan = cell.colspan;
+      uniformX = cell.uniformX;
+      uniformY = cell.uniformY;
+    }
 
   void merge(@Nullable @Null Cell cell) {
     if (cell == null) return;
