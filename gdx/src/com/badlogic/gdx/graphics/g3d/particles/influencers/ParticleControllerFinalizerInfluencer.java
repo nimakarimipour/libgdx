b527e8d756954ed3438294d375b39ca88166db9c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:08:37 -0700

    Revert "fix: 174 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull field positionChannel (line 34) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 21ec2ea77dd7b0b303324db53fb5af89e7482a00.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ParticleControllerFinalizerInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ParticleControllerFinalizerInfluencer.java
@@ -22,8 +22,6 @@ import com.badlogic.gdx.graphics.g3d.particles.ParticleChannels;
 import com.badlogic.gdx.graphics.g3d.particles.ParticleController;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.uber.nullaway.annotations.Initializer;
-import edu.ucr.cs.riple.annotator.util.Nullability;
-import javax.annotation.Nullable;
 
 /**
  * It's an {@link Influencer} which updates the simulation of particles containing a {@link
@@ -33,7 +31,7 @@ import javax.annotation.Nullable;
  * @author Inferno
  */
 public class ParticleControllerFinalizerInfluencer extends Influencer {
-  @Nullable FloatChannel positionChannel, scaleChannel, rotationChannel;
+  FloatChannel positionChannel, scaleChannel, rotationChannel;
   ObjectChannel<ParticleController> controllerChannel;
   boolean hasScale, hasRotation;
 
@@ -59,27 +57,14 @@ public class ParticleControllerFinalizerInfluencer extends Influencer {
 
   @Override
   public void update() {
-    if (positionChannel == null) {
-      throw new IllegalStateException("positionChannel has not been initialized");
-    }
-
-    if (hasScale && scaleChannel == null) {
-      throw new IllegalStateException("scaleChannel has not been initialized");
-    }
-
-    if (hasRotation && rotationChannel == null) {
-      throw new IllegalStateException("rotationChannel has not been initialized");
-    }
-
     for (int i = 0, positionOffset = 0, c = controller.particles.size;
         i < c;
         ++i, positionOffset += positionChannel.strideSize) {
       ParticleController particleController = controllerChannel.data[i];
-      float scale = hasScale ? Nullability.castToNonnull(scaleChannel, "not null here").data[i] : 1;
+      float scale = hasScale ? scaleChannel.data[i] : 1;
       float qx = 0, qy = 0, qz = 0, qw = 1;
       if (hasRotation) {
-        int rotationOffset =
-            i * Nullability.castToNonnull(rotationChannel, "not null when used").strideSize;
+        int rotationOffset = i * rotationChannel.strideSize;
         qx = rotationChannel.data[rotationOffset + ParticleChannels.XOffset];
         qy = rotationChannel.data[rotationOffset + ParticleChannels.YOffset];
         qz = rotationChannel.data[rotationOffset + ParticleChannels.ZOffset];
