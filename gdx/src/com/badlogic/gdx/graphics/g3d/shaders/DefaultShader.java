0103b8975ac289d573c368415eee3b74ec60939c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:08:31 -0700

    fix: 352 - DEREFERENCE_NULLABLE - this.shadowMap = lighting && renderable.environment.shadowMap != null; - dereferenced expression renderable.environment is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/shaders/DefaultShader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/shaders/DefaultShader.java
@@ -47,6 +47,7 @@ import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.GdxRuntimeException;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 public class DefaultShader extends BaseShader {
@@ -770,7 +771,10 @@ public class DefaultShader extends BaseShader {
     this.environmentCubemap =
         attributes.has(CubemapAttribute.EnvironmentMap)
             || (lighting && attributes.has(CubemapAttribute.EnvironmentMap));
-    this.shadowMap = lighting && renderable.environment.shadowMap != null;
+    this.shadowMap =
+        lighting
+            && Nullability.castToNonnull(renderable.environment, "checked previously").shadowMap
+                != null;
     this.renderable = renderable;
     attributesMask = attributes.getMask() | optionalAttributes;
     vertexMask = renderable.meshPart.mesh.getVertexAttributes().getMaskWithSizePacked();
