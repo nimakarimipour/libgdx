ae1727763bce785298c8b149add6f00ef30745f9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:43:53 -0700

    fix: 419 - FIELD_NO_INIT - public @Null int[][] values; - @NonNull field TextureAtlas$TextureAtlasData$Region.values not initialized

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
@@ -585,13 +585,17 @@ public class TextureAtlas implements Disposable {
       public boolean rotate;
       public int index = -1;
       @Nullable public @Null String[] names;
-      public @Null int[][] values;
+      @Nullable public @Null int[][] values;
       public boolean flip;
 
       @Nullable
       public @Null int[] findValue(String name) {
-        if (names != null) {
-          for (int i = 0, n = names.length; i < n; i++) if (name.equals(names[i])) return values[i];
+        if (names != null && values != null) {
+          for (int i = 0, n = names.length; i < n; i++) {
+            if (name.equals(names[i])) {
+              return values[i];
+            }
+          }
         }
         return null;
       }
@@ -667,7 +671,7 @@ public class TextureAtlas implements Disposable {
      * Values for name/value pairs other than the fields provided on this class, each entry
      * corresponding to {@link #names}.
      */
-    public @Null int[][] values;
+    @Nullable public @Null int[][] values;
 
     public AtlasRegion(@Nullable Texture texture, int x, int y, int width, int height) {
       super(texture, x, y, width, height);
@@ -730,8 +734,12 @@ public class TextureAtlas implements Disposable {
 
     @Nullable
     public @Null int[] findValue(String name) {
-      if (names != null) {
-        for (int i = 0, n = names.length; i < n; i++) if (name.equals(names[i])) return values[i];
+      if (names != null && values != null) {
+        for (int i = 0, n = names.length; i < n; i++) {
+          if (name.equals(names[i])) {
+            return values[i];
+          }
+        }
       }
       return null;
     }
