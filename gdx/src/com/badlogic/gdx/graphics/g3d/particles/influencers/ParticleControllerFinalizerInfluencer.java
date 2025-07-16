13366ecc800c45a0c1d7c82b63768b71bae331ff
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:27:29 -0700

    before:344to343 - Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field positionChannel (line 34) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ParticleControllerFinalizerInfluencer.java:40   @Initializer

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ParticleControllerFinalizerInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/ParticleControllerFinalizerInfluencer.java
@@ -38,17 +38,22 @@ public class ParticleControllerFinalizerInfluencer extends Influencer {
   public ParticleControllerFinalizerInfluencer() {}
 
   @Initializer
-  @Override
-  public void init() {
-    controllerChannel = controller.particles.getChannel(ParticleChannels.ParticleController);
-    if (controllerChannel == null)
-      throw new GdxRuntimeException(
-          "ParticleController channel not found, specify an influencer which will allocate it please.");
-    scaleChannel = controller.particles.getChannel(ParticleChannels.Scale);
-    rotationChannel = controller.particles.getChannel(ParticleChannels.Rotation3D);
-    hasScale = scaleChannel != null;
-    hasRotation = rotationChannel != null;
-  }
+    @Override
+    public void init() {
+      controllerChannel = controller.particles.getChannel(ParticleChannels.ParticleController);
+      if (controllerChannel == null)
+        throw new GdxRuntimeException(
+            "ParticleController channel not found, specify an influencer which will allocate it please.");
+      scaleChannel = controller.particles.getChannel(ParticleChannels.Scale);
+      rotationChannel = controller.particles.getChannel(ParticleChannels.Rotation3D);
+      hasScale = scaleChannel != null;
+      hasRotation = rotationChannel != null;
+  
+      // Initialize positionChannel to avoid METHOD_NO_INIT
+      positionChannel = controller.particles.getChannel(ParticleChannels.Position);
+      if (positionChannel == null)
+        throw new GdxRuntimeException("Position channel not found, allocation is required.");
+    }
 
   @Override
   public void allocateChannels() {
