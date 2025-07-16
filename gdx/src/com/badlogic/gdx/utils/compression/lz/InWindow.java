317781c514b4def166da85f65f97c72042f1b53c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:28:46 -0700

    fix: 400 - DEREFERENCE_NULLABLE - int numReadBytes = _stream.read(_bufferBase, _bufferOffset + _streamPos, size); - dereferenced expression _stream is @Nullable

--- gdx/src/com/badlogic/gdx/utils/compression/lz/InWindow.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lz/InWindow.java
@@ -35,6 +35,7 @@ public class InWindow {
 
   public void ReadBlock() throws IOException {
     if (_streamEndWasReached) return;
+    if (_stream == null) return;
     while (true) {
       int size = (0 - _bufferOffset) + _blockSize - _streamPos;
       if (size == 0) return;
