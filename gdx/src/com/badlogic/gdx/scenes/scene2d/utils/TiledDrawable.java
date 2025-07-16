d659ad8f47cc7a0eeb2b2317335b1f9cc879cb24
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:43:02 -0700

    Revert "fix: 417 - METHOD_NO_INIT - public TextureRegionDrawable() {} - initializer method does not guarantee @NonNull field region (line 33) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 24b3369864b8099f73c57006fb679f245989c287.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/TiledDrawable.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/TiledDrawable.java
@@ -47,11 +47,6 @@ public class TiledDrawable extends TextureRegionDrawable {
     batch.setColor(batch.getColor().mul(color));
 
     TextureRegion region = getRegion();
-    if (region == null) {
-      // Handle the null case, maybe log or return
-      return;
-    }
-
     float regionWidth = region.getRegionWidth() * scale,
         regionHeight = region.getRegionHeight() * scale;
     int fullX = (int) (width / regionWidth), fullY = (int) (height / regionHeight);
