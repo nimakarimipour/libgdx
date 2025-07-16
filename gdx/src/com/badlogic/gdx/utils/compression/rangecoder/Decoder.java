1fb470accee50e74ed8449ca177d85d817c4de68
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 12:40:18 -0700

    before:221to219 - Type='FIELD_NO_INIT', message='@NonNull field Stream not initialized' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/compression/rangecoder/Decoder.java:31   java.io.InputStream Stream;

--- gdx/src/com/badlogic/gdx/utils/compression/rangecoder/Decoder.java
+++ gdx/src/com/badlogic/gdx/utils/compression/rangecoder/Decoder.java
@@ -18,6 +18,7 @@ package com.badlogic.gdx.utils.compression.rangecoder;
 
 import java.io.IOException;
 import javax.annotation.Nullable;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 public class Decoder {
   static final int kTopMask = ~((1 << 24) - 1);
@@ -29,7 +30,7 @@ public class Decoder {
   int Range;
   int Code;
 
-  java.io.InputStream Stream;
+  @Nullable java.io.InputStream Stream;
 
   public final void SetStream(java.io.InputStream stream) {
     Stream = stream;
@@ -40,48 +41,57 @@ public class Decoder {
   }
 
   public final void Init() throws IOException {
-    Code = 0;
-    Range = -1;
-    for (int i = 0; i < 5; i++) Code = (Code << 8) | Stream.read();
-  }
+          if (Stream == null) {
+              throw new IllegalStateException("Stream must be initialized before calling Init()");
+          }
+          Code = 0;
+          Range = -1;
+          for (int i = 0; i < 5; i++) Code = (Code << 8) | Nullability.castToNonnull(Stream, "checked before use").read();
+    }
 
   public final int DecodeDirectBits(int numTotalBits) throws IOException {
-    int result = 0;
-    for (int i = numTotalBits; i != 0; i--) {
-      Range >>>= 1;
-      int t = ((Code - Range) >>> 31);
-      Code -= Range & (t - 1);
-      result = (result << 1) | (1 - t);
-
-      if ((Range & kTopMask) == 0) {
-        Code = (Code << 8) | Stream.read();
-        Range <<= 8;
-      }
-    }
-    return result;
+          if (Stream == null) {
+              throw new NullPointerException("Stream is null");
+          }
+          int result = 0;
+          for (int i = numTotalBits; i != 0; i--) {
+              Range >>>= 1;
+              int t = ((Code - Range) >>> 31);
+              Code -= Range & (t - 1);
+              result = (result << 1) | (1 - t);
+    
+              if ((Range & kTopMask) == 0) {
+                  Code = (Code << 8) | Nullability.castToNonnull(Stream, "checked for null").read();
+                  Range <<= 8;
+              }
+          }
+          return result;
   }
 
   public int DecodeBit(short[] probs, int index) throws IOException {
-    int prob = probs[index];
-    int newBound = (Range >>> kNumBitModelTotalBits) * prob;
-    if ((Code ^ 0x80000000) < (newBound ^ 0x80000000)) {
-      Range = newBound;
-      probs[index] = (short) (prob + ((kBitModelTotal - prob) >>> kNumMoveBits));
-      if ((Range & kTopMask) == 0) {
-        Code = (Code << 8) | Stream.read();
-        Range <<= 8;
-      }
-      return 0;
-    } else {
-      Range -= newBound;
-      Code -= newBound;
-      probs[index] = (short) (prob - ((prob) >>> kNumMoveBits));
-      if ((Range & kTopMask) == 0) {
-        Code = (Code << 8) | Stream.read();
-        Range <<= 8;
-      }
-      return 1;
-    }
+          if (Stream == null) {
+              throw new IllegalStateException("Stream is not set");
+          }
+          int prob = probs[index];
+          int newBound = (Range >>> kNumBitModelTotalBits) * prob;
+          if ((Code ^ 0x80000000) < (newBound ^ 0x80000000)) {
+              Range = newBound;
+              probs[index] = (short) (prob + ((kBitModelTotal - prob) >>> kNumMoveBits));
+              if ((Range & kTopMask) == 0) {
+                  Code = (Code << 8) | Nullability.castToNonnull(Stream, "cannot be null").read();
+                  Range <<= 8;
+              }
+              return 0;
+          } else {
+              Range -= newBound;
+              Code -= newBound;
+              probs[index] = (short) (prob - ((prob) >>> kNumMoveBits));
+              if ((Range & kTopMask) == 0) {
+                  Code = (Code << 8) | Nullability.castToNonnull(Stream, "cannot be null").read();
+                  Range <<= 8;
+              }
+              return 1;
+          }
   }
 
   public static void InitBitModels(short[] probs) {
