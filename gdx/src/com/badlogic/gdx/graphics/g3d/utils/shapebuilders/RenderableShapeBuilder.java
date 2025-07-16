6c4ffd729fca54b2175e5d46cc8b47a1530889c6
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 18:18:58 -0700

    before:190to189 - Type='FIELD_NO_INIT', message='@NonNull static field indices not initialized' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/utils/shapebuilders/RenderableShapeBuilder.java:57   private static short[] indices;

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/shapebuilders/RenderableShapeBuilder.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/shapebuilders/RenderableShapeBuilder.java
@@ -54,7 +54,7 @@ public class RenderableShapeBuilder extends BaseShapeBuilder {
     }
   }
 
-  private static short[] indices;
+  private static short[] indices = new short[0];
   private static float[] vertices = new float[0];
   private static final RenderablePool renderablesPool = new RenderablePool();
   private static final Array<Renderable> renderables = new Array<Renderable>();
