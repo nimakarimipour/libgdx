6dfe747db281fb6c3978b65b0dc6c19db4403df3
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 12:24:29 -0700

    before:248to247 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression other.id is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/loader/G3dModelLoader.java:107             if (other.id.equals(partId)) {

--- gdx/src/com/badlogic/gdx/graphics/g3d/loader/G3dModelLoader.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/loader/G3dModelLoader.java
@@ -42,6 +42,7 @@ import com.badlogic.gdx.utils.BaseJsonReader;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.JsonValue;
 import javax.annotation.Nullable;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 public class G3dModelLoader extends ModelLoader<ModelLoader.ModelParameters> {
   public static final short VERSION_HI = 0;
@@ -81,49 +82,49 @@ public class G3dModelLoader extends ModelLoader<ModelLoader.ModelParameters> {
   }
 
   protected void parseMeshes(ModelData model, JsonValue json) {
-    JsonValue meshes = json.get("meshes");
-    if (meshes != null) {
-
-      model.meshes.ensureCapacity(meshes.size);
-      for (JsonValue mesh = meshes.child; mesh != null; mesh = mesh.next) {
-        ModelMesh jsonMesh = new ModelMesh();
-
-        String id = mesh.getString("id", "");
-        jsonMesh.id = id;
-
-        JsonValue attributes = mesh.require("attributes");
-        jsonMesh.attributes = parseAttributes(attributes);
-        jsonMesh.vertices = mesh.require("vertices").asFloatArray();
-
-        JsonValue meshParts = mesh.require("parts");
-        Array<ModelMeshPart> parts = new Array<ModelMeshPart>();
-        for (JsonValue meshPart = meshParts.child; meshPart != null; meshPart = meshPart.next) {
-          ModelMeshPart jsonPart = new ModelMeshPart();
-          String partId = meshPart.getString("id", null);
-          if (partId == null) {
-            throw new GdxRuntimeException("Not id given for mesh part");
-          }
-          for (ModelMeshPart other : parts) {
-            if (other.id.equals(partId)) {
-              throw new GdxRuntimeException(
-                  "Mesh part with id '" + partId + "' already in defined");
+      JsonValue meshes = json.get("meshes");
+      if (meshes != null) {
+  
+        model.meshes.ensureCapacity(meshes.size);
+        for (JsonValue mesh = meshes.child; mesh != null; mesh = mesh.next) {
+          ModelMesh jsonMesh = new ModelMesh();
+  
+          String id = mesh.getString("id", "");
+          jsonMesh.id = id;
+  
+          JsonValue attributes = mesh.require("attributes");
+          jsonMesh.attributes = parseAttributes(attributes);
+          jsonMesh.vertices = mesh.require("vertices").asFloatArray();
+  
+          JsonValue meshParts = mesh.require("parts");
+          Array<ModelMeshPart> parts = new Array<ModelMeshPart>();
+          for (JsonValue meshPart = meshParts.child; meshPart != null; meshPart = meshPart.next) {
+            ModelMeshPart jsonPart = new ModelMeshPart();
+            String partId = meshPart.getString("id", null);
+            if (partId == null) {
+              throw new GdxRuntimeException("Not id given for mesh part");
             }
+            for (ModelMeshPart other : parts) {
+              if (Nullability.castToNonnull(other.id, "added with non-null id").equals(partId)) {
+                throw new GdxRuntimeException(
+                    "Mesh part with id '" + partId + "' already in defined");
+              }
+            }
+            jsonPart.id = partId;
+  
+            String type = meshPart.getString("type", null);
+            if (type == null) {
+              throw new GdxRuntimeException("No primitive type given for mesh part '" + partId + "'");
+            }
+            jsonPart.primitiveType = parseType(type);
+  
+            jsonPart.indices = meshPart.require("indices").asShortArray();
+            parts.add(jsonPart);
           }
-          jsonPart.id = partId;
-
-          String type = meshPart.getString("type", null);
-          if (type == null) {
-            throw new GdxRuntimeException("No primitive type given for mesh part '" + partId + "'");
-          }
-          jsonPart.primitiveType = parseType(type);
-
-          jsonPart.indices = meshPart.require("indices").asShortArray();
-          parts.add(jsonPart);
+          jsonMesh.parts = parts.toArray(ModelMeshPart.class);
+          model.meshes.add(jsonMesh);
         }
-        jsonMesh.parts = parts.toArray(ModelMeshPart.class);
-        model.meshes.add(jsonMesh);
       }
-    }
   }
 
   protected int parseType(String type) {
