a15df7875d35268d3155f9f72cd07385cfed8d07
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 12:48:13 -0700

    before:208to207 - Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/math/Polyline.java:54     if (!dirty) return worldVertices;

--- gdx/src/com/badlogic/gdx/math/Polyline.java
+++ gdx/src/com/badlogic/gdx/math/Polyline.java
@@ -50,7 +50,7 @@ public class Polyline implements Shape2D {
   }
 
   /** Returns vertices scaled, rotated, and offset by the polygon position. */
-  public float[] getTransformedVertices() {
+  @SuppressWarnings("NullAway") public float[] getTransformedVertices() {
     if (!dirty) return worldVertices;
     dirty = false;
 
