caf3f0f1b1ca57c6b018e747ad13dbad6439d0bc
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:23:15 -0700

    fix: 384 - PASS_NULLABLE - float prefWidth = c.prefWidth.get(a), prefHeight = c.prefHeight.get(a); - passing @Nullable parameter 'a' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -33,6 +33,7 @@ import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
 import com.uber.nullaway.annotations.Initializer;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Arrays;
 import javax.annotation.Nullable;
 
@@ -1089,9 +1090,12 @@ public class Table extends WidgetGroup {
         spannedWeightedWidth += columnWeightedWidth[ii];
       float weightedHeight = rowWeightedHeight[row];
 
-      float prefWidth = c.prefWidth.get(a), prefHeight = c.prefHeight.get(a);
-      float minWidth = c.minWidth.get(a), minHeight = c.minHeight.get(a);
-      float maxWidth = c.maxWidth.get(a), maxHeight = c.maxHeight.get(a);
+      float prefWidth = c.prefWidth.get(Nullability.castToNonnull(a)),
+          prefHeight = c.prefHeight.get(Nullability.castToNonnull(a));
+      float minWidth = c.minWidth.get(Nullability.castToNonnull(a)),
+          minHeight = c.minHeight.get(Nullability.castToNonnull(a));
+      float maxWidth = c.maxWidth.get(Nullability.castToNonnull(a)),
+          maxHeight = c.maxHeight.get(Nullability.castToNonnull(a));
       if (prefWidth < minWidth) prefWidth = minWidth;
       if (prefHeight < minHeight) prefHeight = minHeight;
       if (maxWidth > 0 && prefWidth > maxWidth) prefWidth = maxWidth;
