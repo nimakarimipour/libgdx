4614587d4613cc3403c48f5148239fa865c25057
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:45:35 -0700

    fix: 289 - RETURN_NULLABLE - return style.background; - returning @Nullable expression from method with @NonNull return type

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ProgressBar.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/ProgressBar.java
@@ -280,6 +280,7 @@ public class ProgressBar extends Widget implements Disableable {
     return visualInterpolation.apply((getVisualValue() - min) / (max - min));
   }
 
+  @Nullable
   protected @Null Drawable getBackgroundDrawable() {
     if (disabled && style.disabledBackground != null) return style.disabledBackground;
     return style.background;
