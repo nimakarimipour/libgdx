b74f2dca86fba339e6a165c3e8f64d7736377123
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 12:56:37 -0700

    before:198to197 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression camera is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultRenderableSorter.java:63         (int) (1000f * camera.position.dst2(tmpV1)) - (int) (1000f * camera.position.dst2(tmpV2));

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultRenderableSorter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/DefaultRenderableSorter.java
@@ -45,23 +45,26 @@ public class DefaultRenderableSorter implements RenderableSorter, Comparator<Ren
   }
 
   @Override
-  public int compare(final Renderable o1, final Renderable o2) {
-    final boolean b1 =
-        o1.material.has(BlendingAttribute.Type)
-            && ((BlendingAttribute) o1.material.get(BlendingAttribute.Type)).blended;
-    final boolean b2 =
-        o2.material.has(BlendingAttribute.Type)
-            && ((BlendingAttribute) o2.material.get(BlendingAttribute.Type)).blended;
-    if (b1 != b2) return b1 ? 1 : -1;
-    // FIXME implement better sorting algorithm
-    // final boolean same = o1.shader == o2.shader && o1.mesh == o2.mesh && (o1.lights == null) ==
-    // (o2.lights == null) &&
-    // o1.material.equals(o2.material);
-    getTranslation(o1.worldTransform, o1.meshPart.center, tmpV1);
-    getTranslation(o2.worldTransform, o2.meshPart.center, tmpV2);
-    final float dst =
-        (int) (1000f * camera.position.dst2(tmpV1)) - (int) (1000f * camera.position.dst2(tmpV2));
-    final int result = dst < 0 ? -1 : (dst > 0 ? 1 : 0);
-    return b1 ? -result : result;
-  }
+    public int compare(final Renderable o1, final Renderable o2) {
+      if (camera == null) {
+        throw new IllegalStateException("Camera is not set");
+      }
+      final boolean b1 =
+          o1.material.has(BlendingAttribute.Type)
+              && ((BlendingAttribute) o1.material.get(BlendingAttribute.Type)).blended;
+      final boolean b2 =
+          o2.material.has(BlendingAttribute.Type)
+              && ((BlendingAttribute) o2.material.get(BlendingAttribute.Type)).blended;
+      if (b1 != b2) return b1 ? 1 : -1;
+      // FIXME implement better sorting algorithm
+      // final boolean same = o1.shader == o2.shader && o1.mesh == o2.mesh && (o1.lights == null) ==
+      // (o2.lights == null) &&
+      // o1.material.equals(o2.material);
+      getTranslation(o1.worldTransform, o1.meshPart.center, tmpV1);
+      getTranslation(o2.worldTransform, o2.meshPart.center, tmpV2);
+      final float dst =
+          (int) (1000f * camera.position.dst2(tmpV1)) - (int) (1000f * camera.position.dst2(tmpV2));
+      final int result = dst < 0 ? -1 : (dst > 0 ? 1 : 0);
+      return b1 ? -result : result;
+    }
 }
