ce2916b9ea1f1b5fd46a56f3f49d3ddd19315144
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:38:30 -0700

    before:305to304 - Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/Mesh.java:806     return null;

--- gdx/src/com/badlogic/gdx/graphics/Mesh.java
+++ gdx/src/com/badlogic/gdx/graphics/Mesh.java
@@ -806,12 +806,15 @@ public class Mesh implements Disposable {
    * @return the VertexAttribute or null if no attribute with that usage was found.
    */
   public VertexAttribute getVertexAttribute(int usage) {
-    VertexAttributes attributes = vertices.getAttributes();
-    int len = attributes.size();
-    for (int i = 0; i < len; i++) if (attributes.get(i).usage == usage) return attributes.get(i);
-
-    return null;
-  }
+      VertexAttributes attributes = vertices.getAttributes();
+      int len = attributes.size();
+      for (int i = 0; i < len; i++) {
+        if (attributes.get(i).usage == usage) {
+          return attributes.get(i);
+        }
+      }
+      throw new IllegalStateException("No VertexAttribute found with the given usage");
+    }
 
   /**
    * @return the vertex attributes of this Mesh
