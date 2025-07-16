4d44d98008109dc381f179d6f25a46e214edca7e
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:51:22 -0700

    before:260to259 - Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull field font (line 405) is initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Label.java:409     public LabelStyle() {}

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Label.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Label.java
@@ -406,7 +406,9 @@ public class Label extends Widget {
     @Nullable public @Null Color fontColor;
     @Nullable public @Null Drawable background;
 
-    public LabelStyle() {}
+    public LabelStyle() {
+            this.font = new BitmapFont(); // Initialize font with a default BitmapFont
+        }
 
     public LabelStyle(BitmapFont font, @Nullable @Null Color fontColor) {
       this.font = font;
