35f1200bf489dd26b3d9901e89828472ffae6e4e
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 12:32:06 -0700

    before:232to231 - Type='PASS_NULLABLE', message='passing @Nullable parameter 'style.label' where @NonNull is required' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java:96       label = new LabelStyle(style.label);

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java
@@ -94,9 +94,9 @@ public class TextTooltip extends Tooltip<Label> {
     }
 
     public TextTooltipStyle(TextTooltipStyle style) {
-      label = new LabelStyle(style.label);
-      background = style.background;
-      wrapWidth = style.wrapWidth;
+          label = new LabelStyle(Nullability.castToNonnull(style.label));
+          background = style.background;
+          wrapWidth = style.wrapWidth;
     }
   }
 }
