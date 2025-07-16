cfa525a95533167982512d74b0a267f5f703d1e6
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:06:32 -0700

    Revert "fix: 347 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 51fbcfb4145a0abc1098bfeeff98b9d62b91fbe6.

--- gdx/src/com/badlogic/gdx/graphics/VertexAttributes.java
+++ gdx/src/com/badlogic/gdx/graphics/VertexAttributes.java
@@ -95,7 +95,6 @@ public final class VertexAttributes
    *
    * @param usage The usage of the VertexAttribute to find.
    */
-  @SuppressWarnings("NullAway")
   public VertexAttribute findByUsage(int usage) {
     int len = size();
     for (int i = 0; i < len; i++) if (get(i).usage == usage) return get(i);
