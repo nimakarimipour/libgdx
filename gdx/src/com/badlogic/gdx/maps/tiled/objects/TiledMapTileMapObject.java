cef0eddd3e206000f2f5334c2bce7cc47c6276e7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 18:30:02 -0700

    before:154to153 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression tile is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/maps/tiled/objects/TiledMapTileMapObject.java:47     TextureRegion textureRegion = new TextureRegion(tile.getTextureRegion());

--- gdx/src/com/badlogic/gdx/maps/tiled/objects/TiledMapTileMapObject.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/objects/TiledMapTileMapObject.java
@@ -39,15 +39,17 @@ public class TiledMapTileMapObject extends TextureMapObject {
   @Nullable private TiledMapTile tile;
 
   public TiledMapTileMapObject(
-      @Nullable TiledMapTile tile, boolean flipHorizontally, boolean flipVertically) {
-    this.flipHorizontally = flipHorizontally;
-    this.flipVertically = flipVertically;
-    this.tile = tile;
-
-    TextureRegion textureRegion = new TextureRegion(tile.getTextureRegion());
-    textureRegion.flip(flipHorizontally, flipVertically);
-    setTextureRegion(textureRegion);
-  }
+         @Nullable TiledMapTile tile, boolean flipHorizontally, boolean flipVertically) {
+      this.flipHorizontally = flipHorizontally;
+      this.flipVertically = flipVertically;
+      this.tile = tile;
+  
+      if (tile != null && tile.getTextureRegion() != null) {
+        TextureRegion textureRegion = new TextureRegion(tile.getTextureRegion());
+        textureRegion.flip(flipHorizontally, flipVertically);
+        setTextureRegion(textureRegion);
+      }
+    }
 
   public boolean isFlipHorizontally() {
     return flipHorizontally;
