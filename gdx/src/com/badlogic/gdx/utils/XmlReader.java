e585e605e8d34a5935731b664ff1c759c1dedecf
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:10:13 -0700

    fix: 44 - DEREFERENCE_NULLABLE - parent.removeChild(this); - dereferenced expression parent is @Nullable

--- gdx/src/com/badlogic/gdx/utils/XmlReader.java
+++ gdx/src/com/badlogic/gdx/utils/XmlReader.java
@@ -568,7 +568,9 @@ public class XmlReader {
     }
 
     public void remove() {
-      parent.removeChild(this);
+      if (parent != null) {
+        parent.removeChild(this);
+      }
     }
 
     @Nullable
