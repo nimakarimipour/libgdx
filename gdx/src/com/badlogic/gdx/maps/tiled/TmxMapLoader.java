62af04052a7296835d94d462fdb705495c51a128
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 18:29:51 -0700

    before:155to154 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression image is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/maps/tiled/TmxMapLoader.java:231         TextureRegion texture = imageResolver.getImage(image.path());

--- gdx/src/com/badlogic/gdx/maps/tiled/TmxMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/TmxMapLoader.java
@@ -170,68 +170,71 @@ public class TmxMapLoader extends BaseTmxMapLoader<TmxMapLoader.Parameters> {
   }
 
   @Override
-  protected void addStaticTiles(
-      FileHandle tmxFile,
-      ImageResolver imageResolver,
-      TiledMapTileSet tileSet,
-      Element element,
-      Array<Element> tileElements,
-      @Nullable String name,
-      int firstgid,
-      int tilewidth,
-      int tileheight,
-      int spacing,
-      int margin,
-      @Nullable String source,
-      int offsetX,
-      int offsetY,
-      String imageSource,
-      int imageWidth,
-      int imageHeight,
-      @Nullable FileHandle image) {
-
-    MapProperties props = tileSet.getProperties();
-    if (image != null) {
-      // One image for the whole tileSet
-      TextureRegion texture = imageResolver.getImage(image.path());
-
-      props.put("imagesource", imageSource);
-      props.put("imagewidth", imageWidth);
-      props.put("imageheight", imageHeight);
-      props.put("tilewidth", tilewidth);
-      props.put("tileheight", tileheight);
-      props.put("margin", margin);
-      props.put("spacing", spacing);
-
-      int stopWidth = texture.getRegionWidth() - tilewidth;
-      int stopHeight = texture.getRegionHeight() - tileheight;
-
-      int id = firstgid;
-
-      for (int y = margin; y <= stopHeight; y += tileheight + spacing) {
-        for (int x = margin; x <= stopWidth; x += tilewidth + spacing) {
-          TextureRegion tileRegion = new TextureRegion(texture, x, y, tilewidth, tileheight);
-          int tileId = id++;
-          addStaticTiledMapTile(tileSet, tileRegion, tileId, offsetX, offsetY);
-        }
-      }
-    } else {
-      // Every tile has its own image source
-      for (Element tileElement : tileElements) {
-        Element imageElement = tileElement.getChildByName("image");
-        if (imageElement != null) {
-          imageSource = imageElement.getAttribute("source");
-
-          if (source != null) {
-            image = getRelativeFileHandle(getRelativeFileHandle(tmxFile, source), imageSource);
-          } else {
-            image = getRelativeFileHandle(tmxFile, imageSource);
+    protected void addStaticTiles(
+        FileHandle tmxFile,
+        ImageResolver imageResolver,
+        TiledMapTileSet tileSet,
+        Element element,
+        Array<Element> tileElements,
+        @Nullable String name,
+        int firstgid,
+        int tilewidth,
+        int tileheight,
+        int spacing,
+        int margin,
+        @Nullable String source,
+        int offsetX,
+        int offsetY,
+        String imageSource,
+        int imageWidth,
+        int imageHeight,
+        @Nullable FileHandle image) {
+  
+      MapProperties props = tileSet.getProperties();
+      if (image != null) {
+        // One image for the whole tileSet
+        TextureRegion texture = imageResolver.getImage(image.path());
+  
+        props.put("imagesource", imageSource);
+        props.put("imagewidth", imageWidth);
+        props.put("imageheight", imageHeight);
+        props.put("tilewidth", tilewidth);
+        props.put("tileheight", tileheight);
+        props.put("margin", margin);
+        props.put("spacing", spacing);
+  
+        int stopWidth = texture.getRegionWidth() - tilewidth;
+        int stopHeight = texture.getRegionHeight() - tileheight;
+  
+        int id = firstgid;
+  
+        for (int y = margin; y <= stopHeight; y += tileheight + spacing) {
+          for (int x = margin; x <= stopWidth; x += tilewidth + spacing) {
+            TextureRegion tileRegion = new TextureRegion(texture, x, y, tilewidth, tileheight);
+            int tileId = id++;
+            addStaticTiledMapTile(tileSet, tileRegion, tileId, offsetX, offsetY);
           }
         }
-        TextureRegion texture = imageResolver.getImage(image.path());
-        int tileId = firstgid + tileElement.getIntAttribute("id");
-        addStaticTiledMapTile(tileSet, texture, tileId, offsetX, offsetY);
+      } else {
+        // Every tile has its own image source
+        for (Element tileElement : tileElements) {
+          Element imageElement = tileElement.getChildByName("image");
+          if (imageElement != null) {
+            imageSource = imageElement.getAttribute("source");
+  
+            if (source != null) {
+              image = getRelativeFileHandle(getRelativeFileHandle(tmxFile, source), imageSource);
+            } else {
+              image = getRelativeFileHandle(tmxFile, imageSource);
+            }
+  
+            if (image != null) {
+              TextureRegion texture = imageResolver.getImage(image.path());
+              int tileId = firstgid + tileElement.getIntAttribute("id");
+              addStaticTiledMapTile(tileSet, texture, tileId, offsetX, offsetY);
+            }
+          }
+        }
       }
     }
-  }
 }
