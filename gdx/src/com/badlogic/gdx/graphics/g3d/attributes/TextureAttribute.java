00d9d2a05028fc5bf9804f32877330fe6fee7268
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:28:15 -0700

    before:342to341 - Type='ASSIGN_FIELD_NULLABLE', message='assigning @Nullable expression to @NonNull field' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/attributes/TextureAttribute.java:160     textureDescription.texture = texture;

--- gdx/src/com/badlogic/gdx/graphics/g3d/attributes/TextureAttribute.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/attributes/TextureAttribute.java
@@ -156,9 +156,11 @@ public class TextureAttribute extends Attribute {
   }
 
   public TextureAttribute(final long type, @Nullable final Texture texture) {
-    this(type);
-    textureDescription.texture = texture;
-  }
+      this(type);
+      if(texture != null) {
+        textureDescription.texture = texture;
+      }
+    }
 
   public TextureAttribute(final long type, final TextureRegion region) {
     this(type);
