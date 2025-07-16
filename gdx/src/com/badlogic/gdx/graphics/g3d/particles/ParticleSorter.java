cc87b1e0f78d0477e30105b57c46dd5fd2a5ed60
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 18:17:45 -0700

    before:193to192 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression camera is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java:73       float[] val = camera.view.val;

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java
@@ -69,30 +69,33 @@ public abstract class ParticleSorter {
     }
 
     @Override
-    public <T extends ParticleControllerRenderData> int[] sort(Array<T> renderData) {
-      float[] val = camera.view.val;
-      float cx = val[Matrix4.M20], cy = val[Matrix4.M21], cz = val[Matrix4.M22];
-      int count = 0, i = 0;
-      for (ParticleControllerRenderData data : renderData) {
-        for (int k = 0, c = i + data.controller.particles.size;
-            i < c;
-            ++i, k += data.positionChannel.strideSize) {
-          distances[i] =
-              cx * data.positionChannel.data[k + ParticleChannels.XOffset]
-                  + cy * data.positionChannel.data[k + ParticleChannels.YOffset]
-                  + cz * data.positionChannel.data[k + ParticleChannels.ZOffset];
-          particleIndices[i] = i;
+        public <T extends ParticleControllerRenderData> int[] sort(Array<T> renderData) {
+          if (camera == null || camera.view == null || camera.view.val == null) {
+            throw new NullPointerException("Camera or its view matrix is null");
+          }
+          float[] val = camera.view.val;
+          float cx = val[Matrix4.M20], cy = val[Matrix4.M21], cz = val[Matrix4.M22];
+          int count = 0, i = 0;
+          for (ParticleControllerRenderData data : renderData) {
+            for (int k = 0, c = i + data.controller.particles.size;
+                i < c;
+                ++i, k += data.positionChannel.strideSize) {
+              distances[i] =
+                  cx * data.positionChannel.data[k + ParticleChannels.XOffset]
+                      + cy * data.positionChannel.data[k + ParticleChannels.YOffset]
+                      + cz * data.positionChannel.data[k + ParticleChannels.ZOffset];
+              particleIndices[i] = i;
+            }
+            count += data.controller.particles.size;
+          }
+    
+          qsort(0, count - 1);
+    
+          for (i = 0; i < count; ++i) {
+            particleOffsets[particleIndices[i]] = i;
+          }
+          return particleOffsets;
         }
-        count += data.controller.particles.size;
-      }
-
-      qsort(0, count - 1);
-
-      for (i = 0; i < count; ++i) {
-        particleOffsets[particleIndices[i]] = i;
-      }
-      return particleOffsets;
-    }
 
     public void qsort(int si, int ei) {
       // base case
