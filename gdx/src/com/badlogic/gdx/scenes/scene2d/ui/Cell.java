b961bd436b3103edd92151294f5d5251abae53b1
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:42:53 -0700

    Revert "fix: 276 - PASS_NULLABLE - return maxHeight.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required"
    This reverts commit 3d80ba198762d0169ca80f493015573421c4a262.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.Pool.Poolable;
 import com.uber.nullaway.annotations.Initializer;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -808,7 +807,7 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getMaxHeight() {
-    return maxHeight.get(Nullability.castToNonnull(actor));
+    return maxHeight.get(actor);
   }
 
   /**
