0b167cc6491b84043c6b6f77d0691833dfd26351
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:29:30 -0700

    fix: 88 - DEREFERENCE_NULLABLE - return keyNames.get(keyname, -1); - dereferenced expression keyNames is @Nullable

--- gdx/src/com/badlogic/gdx/Input.java
+++ gdx/src/com/badlogic/gdx/Input.java
@@ -17,6 +17,7 @@
 package com.badlogic.gdx;
 
 import com.badlogic.gdx.utils.ObjectIntMap;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -637,7 +638,7 @@ public interface Input {
      */
     public static int valueOf(String keyname) {
       if (keyNames == null) initializeKeyNames();
-      return keyNames.get(keyname, -1);
+      return Nullability.castToNonnull(keyNames, "initialized if null").get(keyname, -1);
     }
 
     /** lazily intialized in {@link Keys#valueOf(String)} */
