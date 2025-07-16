1b5b540065bb783ad77ec76f3ba7721f9caacb37
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:20:49 -0700

    fix: 66 - DEREFERENCE_NULLABLE - Element properties = root.getChildByName("Properties"); - dereferenced expression root is @Nullable

--- gdx/src/com/badlogic/gdx/maps/tiled/TideMapLoader.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/TideMapLoader.java
@@ -37,6 +37,7 @@ import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.XmlReader;
 import com.badlogic.gdx.utils.XmlReader.Element;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.IOException;
 import java.util.StringTokenizer;
 import javax.annotation.Nullable;
@@ -113,7 +114,7 @@ public class TideMapLoader extends SynchronousAssetLoader<TiledMap, TideMapLoade
   private TiledMap loadMap(
       @Nullable Element root, FileHandle tmxFile, ImageResolver imageResolver) {
     TiledMap map = new TiledMap();
-    Element properties = root.getChildByName("Properties");
+    Element properties = Nullability.castToNonnull(root).getChildByName("Properties");
     if (properties != null) {
       loadProperties(map.getProperties(), properties);
     }
