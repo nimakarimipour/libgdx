69fea61993484c6cca0b690e91f00ad3eea83a5f
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 08:45:56 -0700

    before:375to374 - Type='FIELD_NO_INIT', message='@NonNull field _son not initialized' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/compression/lz/BinTree.java:12   int[] _son;

--- gdx/src/com/badlogic/gdx/utils/compression/lz/BinTree.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lz/BinTree.java
@@ -4,13 +4,14 @@ package com.badlogic.gdx.utils.compression.lz;
 
 import java.io.IOException;
 import javax.annotation.Nullable;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 public class BinTree extends InWindow {
   int _cyclicBufferPos;
   int _cyclicBufferSize = 0;
   int _matchMaxLen;
 
-  int[] _son;
+  @Nullable int[] _son;
   int[] _hash;
 
   int _cutValue = 0xFF;
@@ -94,190 +95,202 @@ public class BinTree extends InWindow {
   }
 
   public int GetMatches(int[] distances) throws IOException {
-    int lenLimit;
-    if (_pos + _matchMaxLen <= _streamPos) lenLimit = _matchMaxLen;
-    else {
-      lenLimit = _streamPos - _pos;
-      if (lenLimit < kMinMatchCheck) {
-        MovePos();
-        return 0;
-      }
-    }
-
-    int offset = 0;
-    int matchMinPos = (_pos > _cyclicBufferSize) ? (_pos - _cyclicBufferSize) : 0;
-    int cur = _bufferOffset + _pos;
-    int maxLen = kStartMaxLen; // to avoid items for len < hashSize;
-    int hashValue, hash2Value = 0, hash3Value = 0;
-
-    if (HASH_ARRAY) {
-      int temp = CrcTable[_bufferBase[cur] & 0xFF] ^ (_bufferBase[cur + 1] & 0xFF);
-      hash2Value = temp & (kHash2Size - 1);
-      temp ^= ((int) (_bufferBase[cur + 2] & 0xFF) << 8);
-      hash3Value = temp & (kHash3Size - 1);
-      hashValue = (temp ^ (CrcTable[_bufferBase[cur + 3] & 0xFF] << 5)) & _hashMask;
-    } else hashValue = ((_bufferBase[cur] & 0xFF) ^ ((int) (_bufferBase[cur + 1] & 0xFF) << 8));
-
-    int curMatch = _hash[kFixHashSize + hashValue];
-    if (HASH_ARRAY) {
-      int curMatch2 = _hash[hash2Value];
-      int curMatch3 = _hash[kHash3Offset + hash3Value];
-      _hash[hash2Value] = _pos;
-      _hash[kHash3Offset + hash3Value] = _pos;
-      if (curMatch2 > matchMinPos)
-        if (_bufferBase[_bufferOffset + curMatch2] == _bufferBase[cur]) {
-          distances[offset++] = maxLen = 2;
-          distances[offset++] = _pos - curMatch2 - 1;
-        }
-      if (curMatch3 > matchMinPos)
-        if (_bufferBase[_bufferOffset + curMatch3] == _bufferBase[cur]) {
-          if (curMatch3 == curMatch2) offset -= 2;
-          distances[offset++] = maxLen = 3;
-          distances[offset++] = _pos - curMatch3 - 1;
-          curMatch2 = curMatch3;
-        }
-      if (offset != 0 && curMatch2 == curMatch) {
-        offset -= 2;
-        maxLen = kStartMaxLen;
-      }
-    }
-
-    _hash[kFixHashSize + hashValue] = _pos;
-
-    int ptr0 = (_cyclicBufferPos << 1) + 1;
-    int ptr1 = (_cyclicBufferPos << 1);
-
-    int len0, len1;
-    len0 = len1 = kNumHashDirectBytes;
-
-    if (kNumHashDirectBytes != 0) {
-      if (curMatch > matchMinPos) {
-        if (_bufferBase[_bufferOffset + curMatch + kNumHashDirectBytes]
-            != _bufferBase[cur + kNumHashDirectBytes]) {
-          distances[offset++] = maxLen = kNumHashDirectBytes;
-          distances[offset++] = _pos - curMatch - 1;
-        }
-      }
-    }
-
-    int count = _cutValue;
-
-    while (true) {
-      if (curMatch <= matchMinPos || count-- == 0) {
-        _son[ptr0] = _son[ptr1] = kEmptyHashValue;
-        break;
-      }
-      int delta = _pos - curMatch;
-      int cyclicPos =
-          ((delta <= _cyclicBufferPos)
-                  ? (_cyclicBufferPos - delta)
-                  : (_cyclicBufferPos - delta + _cyclicBufferSize))
-              << 1;
-
-      int pby1 = _bufferOffset + curMatch;
-      int len = Math.min(len0, len1);
-      if (_bufferBase[pby1 + len] == _bufferBase[cur + len]) {
-        while (++len != lenLimit) if (_bufferBase[pby1 + len] != _bufferBase[cur + len]) break;
-        if (maxLen < len) {
-          distances[offset++] = maxLen = len;
-          distances[offset++] = delta - 1;
-          if (len == lenLimit) {
-            _son[ptr1] = _son[cyclicPos];
-            _son[ptr0] = _son[cyclicPos + 1];
-            break;
-          }
-        }
-      }
-      if ((_bufferBase[pby1 + len] & 0xFF) < (_bufferBase[cur + len] & 0xFF)) {
-        _son[ptr1] = curMatch;
-        ptr1 = cyclicPos + 1;
-        curMatch = _son[ptr1];
-        len1 = len;
-      } else {
-        _son[ptr0] = curMatch;
-        ptr0 = cyclicPos;
-        curMatch = _son[ptr0];
-        len0 = len;
-      }
-    }
-    MovePos();
-    return offset;
+              int lenLimit;
+              if (_pos + _matchMaxLen <= _streamPos) lenLimit = _matchMaxLen;
+              else {
+                lenLimit = _streamPos - _pos;
+                if (lenLimit < kMinMatchCheck) {
+                  MovePos();
+                  return 0;
+                }
+              }
+          
+              int offset = 0;
+              int matchMinPos = (_pos > _cyclicBufferSize) ? (_pos - _cyclicBufferSize) : 0;
+              int cur = _bufferOffset + _pos;
+              int maxLen = kStartMaxLen;
+              int hashValue, hash2Value = 0, hash3Value = 0;
+          
+              if (HASH_ARRAY) {
+                int temp = CrcTable[_bufferBase[cur] & 0xFF] ^ (_bufferBase[cur + 1] & 0xFF);
+                hash2Value = temp & (kHash2Size - 1);
+                temp ^= ((int) (_bufferBase[cur + 2] & 0xFF) << 8);
+                hash3Value = temp & (kHash3Size - 1);
+                hashValue = (temp ^ (CrcTable[_bufferBase[cur + 3] & 0xFF] << 5)) & _hashMask;
+              } else hashValue = ((_bufferBase[cur] & 0xFF) ^ ((int) (_bufferBase[cur + 1] & 0xFF) << 8));
+          
+              int curMatch = _hash[kFixHashSize + hashValue];
+              if (HASH_ARRAY) {
+                int curMatch2 = _hash[hash2Value];
+                int curMatch3 = _hash[kHash3Offset + hash3Value];
+                _hash[hash2Value] = _pos;
+                _hash[kHash3Offset + hash3Value] = _pos;
+                if (curMatch2 > matchMinPos)
+                  if (_bufferBase[_bufferOffset + curMatch2] == _bufferBase[cur]) {
+                    distances[offset++] = maxLen = 2;
+                    distances[offset++] = _pos - curMatch2 - 1;
+                  }
+                if (curMatch3 > matchMinPos)
+                  if (_bufferBase[_bufferOffset + curMatch3] == _bufferBase[cur]) {
+                    if (curMatch3 == curMatch2) offset -= 2;
+                    distances[offset++] = maxLen = 3;
+                    distances[offset++] = _pos - curMatch3 - 1;
+                    curMatch2 = curMatch3;
+                  }
+                if (offset != 0 && curMatch2 == curMatch) {
+                  offset -= 2;
+                  maxLen = kStartMaxLen;
+                }
+              }
+          
+              _hash[kFixHashSize + hashValue] = _pos;
+          
+              int ptr0 = (_cyclicBufferPos << 1) + 1;
+              int ptr1 = (_cyclicBufferPos << 1);
+          
+              int len0, len1;
+              len0 = len1 = kNumHashDirectBytes;
+          
+              if (_son == null || _son.length != _cyclicBufferSize * 2) {
+                _son = new int[_cyclicBufferSize * 2];
+              }
+          
+              if (kNumHashDirectBytes != 0) {
+                if (curMatch > matchMinPos) {
+                  if (_bufferBase[_bufferOffset + curMatch + kNumHashDirectBytes]
+                      != _bufferBase[cur + kNumHashDirectBytes]) {
+                    distances[offset++] = maxLen = kNumHashDirectBytes;
+                    distances[offset++] = _pos - curMatch - 1;
+                  }
+                }
+              }
+          
+              int count = _cutValue;
+          
+              while (true) {
+                if (curMatch <= matchMinPos || count-- == 0) {
+                  Nullability.castToNonnull(_son, "checked for nullity")[ptr0] = Nullability.castToNonnull(_son, "checked for nullity")[ptr1] = kEmptyHashValue;
+                  break;
+                }
+                int delta = _pos - curMatch;
+                int cyclicPos =
+                    ((delta <= _cyclicBufferPos)
+                            ? (_cyclicBufferPos - delta)
+                            : (_cyclicBufferPos - delta + _cyclicBufferSize))
+                        << 1;
+          
+                int pby1 = _bufferOffset + curMatch;
+                int len = Math.min(len0, len1);
+                if (_bufferBase[pby1 + len] == _bufferBase[cur + len]) {
+                  while (++len != lenLimit) if (_bufferBase[pby1 + len] != _bufferBase[cur + len]) break;
+                  if (maxLen < len) {
+                    distances[offset++] = maxLen = len;
+                    distances[offset++] = delta - 1;
+                    if (len == lenLimit) {
+                      Nullability.castToNonnull(_son, "checked for nullity")[ptr1] = Nullability.castToNonnull(_son, "checked for nullity")[cyclicPos];
+                      _son[ptr0] = Nullability.castToNonnull(_son, "checked for nullity")[cyclicPos + 1];
+                      break;
+                    }
+                  }
+                }
+                if ((_bufferBase[pby1 + len] & 0xFF) < (_bufferBase[cur + len] & 0xFF)) {
+                  _son[ptr1] = curMatch;
+                  ptr1 = cyclicPos + 1;
+                  curMatch = _son[ptr1];
+                  len1 = len;
+                } else {
+                  _son[ptr0] = curMatch;
+                  ptr0 = cyclicPos;
+                  curMatch = _son[ptr0];
+                  len0 = len;
+                }
+              }
+              MovePos();
+              return offset;
   }
 
   public void Skip(int num) throws IOException {
-    do {
-      int lenLimit;
-      if (_pos + _matchMaxLen <= _streamPos) lenLimit = _matchMaxLen;
-      else {
-        lenLimit = _streamPos - _pos;
-        if (lenLimit < kMinMatchCheck) {
-          MovePos();
-          continue;
-        }
-      }
-
-      int matchMinPos = (_pos > _cyclicBufferSize) ? (_pos - _cyclicBufferSize) : 0;
-      int cur = _bufferOffset + _pos;
-
-      int hashValue;
-
-      if (HASH_ARRAY) {
-        int temp = CrcTable[_bufferBase[cur] & 0xFF] ^ (_bufferBase[cur + 1] & 0xFF);
-        int hash2Value = temp & (kHash2Size - 1);
-        _hash[hash2Value] = _pos;
-        temp ^= ((int) (_bufferBase[cur + 2] & 0xFF) << 8);
-        int hash3Value = temp & (kHash3Size - 1);
-        _hash[kHash3Offset + hash3Value] = _pos;
-        hashValue = (temp ^ (CrcTable[_bufferBase[cur + 3] & 0xFF] << 5)) & _hashMask;
-      } else hashValue = ((_bufferBase[cur] & 0xFF) ^ ((int) (_bufferBase[cur + 1] & 0xFF) << 8));
-
-      int curMatch = _hash[kFixHashSize + hashValue];
-      _hash[kFixHashSize + hashValue] = _pos;
-
-      int ptr0 = (_cyclicBufferPos << 1) + 1;
-      int ptr1 = (_cyclicBufferPos << 1);
-
-      int len0, len1;
-      len0 = len1 = kNumHashDirectBytes;
-
-      int count = _cutValue;
-      while (true) {
-        if (curMatch <= matchMinPos || count-- == 0) {
-          _son[ptr0] = _son[ptr1] = kEmptyHashValue;
-          break;
-        }
-
-        int delta = _pos - curMatch;
-        int cyclicPos =
-            ((delta <= _cyclicBufferPos)
-                    ? (_cyclicBufferPos - delta)
-                    : (_cyclicBufferPos - delta + _cyclicBufferSize))
-                << 1;
-
-        int pby1 = _bufferOffset + curMatch;
-        int len = Math.min(len0, len1);
-        if (_bufferBase[pby1 + len] == _bufferBase[cur + len]) {
-          while (++len != lenLimit) if (_bufferBase[pby1 + len] != _bufferBase[cur + len]) break;
-          if (len == lenLimit) {
-            _son[ptr1] = _son[cyclicPos];
-            _son[ptr0] = _son[cyclicPos + 1];
-            break;
-          }
-        }
-        if ((_bufferBase[pby1 + len] & 0xFF) < (_bufferBase[cur + len] & 0xFF)) {
-          _son[ptr1] = curMatch;
-          ptr1 = cyclicPos + 1;
-          curMatch = _son[ptr1];
-          len1 = len;
-        } else {
-          _son[ptr0] = curMatch;
-          ptr0 = cyclicPos;
-          curMatch = _son[ptr0];
-          len0 = len;
-        }
-      }
-      MovePos();
-    } while (--num != 0);
+              if (_son == null) {
+                throw new NullPointerException("_son array is not initialized.");
+              }
+              
+              do {
+                int lenLimit;
+                if (_pos + _matchMaxLen <= _streamPos)
+                  lenLimit = _matchMaxLen;
+                else {
+                  lenLimit = _streamPos - _pos;
+                  if (lenLimit < kMinMatchCheck) {
+                    MovePos();
+                    continue;
+                  }
+                }
+            
+                int matchMinPos = (_pos > _cyclicBufferSize) ? (_pos - _cyclicBufferSize) : 0;
+                int cur = _bufferOffset + _pos;
+            
+                int hashValue;
+            
+                if (HASH_ARRAY) {
+                  int temp = CrcTable[_bufferBase[cur] & 0xFF] ^ (_bufferBase[cur + 1] & 0xFF);
+                  int hash2Value = temp & (kHash2Size - 1);
+                  _hash[hash2Value] = _pos;
+                  temp ^= ((int) (_bufferBase[cur + 2] & 0xFF) << 8);
+                  int hash3Value = temp & (kHash3Size - 1);
+                  _hash[kHash3Offset + hash3Value] = _pos;
+                  hashValue = (temp ^ (CrcTable[_bufferBase[cur + 3] & 0xFF] << 5)) & _hashMask;
+                } else {
+                  hashValue = ((_bufferBase[cur] & 0xFF) ^ ((int) (_bufferBase[cur + 1] & 0xFF) << 8));
+                }
+            
+                int curMatch = _hash[kFixHashSize + hashValue];
+                _hash[kFixHashSize + hashValue] = _pos;
+            
+                int ptr0 = (_cyclicBufferPos << 1) + 1;
+                int ptr1 = (_cyclicBufferPos << 1);
+            
+                int len0, len1;
+                len0 = len1 = kNumHashDirectBytes;
+            
+                int count = _cutValue;
+                while (true) {
+                  if (curMatch <= matchMinPos || count-- == 0) {
+                    Nullability.castToNonnull(_son, "null check passed")[ptr0] = Nullability.castToNonnull(_son, "null check passed")[ptr1] = kEmptyHashValue;
+                    break;
+                  }
+            
+                  int delta = _pos - curMatch;
+                  int cyclicPos =
+                      ((delta <= _cyclicBufferPos)
+                              ? (_cyclicBufferPos - delta)
+                              : (_cyclicBufferPos - delta + _cyclicBufferSize))
+                          << 1;
+            
+                  int pby1 = _bufferOffset + curMatch;
+                  int len = Math.min(len0, len1);
+                  if (_bufferBase[pby1 + len] == _bufferBase[cur + len]) {
+                    while (++len != lenLimit)
+                      if (_bufferBase[pby1 + len] != _bufferBase[cur + len]) break;
+                    if (len == lenLimit) {
+                      Nullability.castToNonnull(_son, "null check passed")[ptr1] = _son[cyclicPos];
+                      Nullability.castToNonnull(_son, "null check passed")[ptr0] = _son[cyclicPos + 1];
+                      break;
+                    }
+                  }
+                  if ((_bufferBase[pby1 + len] & 0xFF) < (_bufferBase[cur + len] & 0xFF)) {
+                    Nullability.castToNonnull(_son, "null check passed")[ptr1] = curMatch;
+                    ptr1 = cyclicPos + 1;
+                    curMatch = _son[ptr1];
+                    len1 = len;
+                  } else {
+                    Nullability.castToNonnull(_son, "null check passed")[ptr0] = curMatch;
+                    ptr0 = cyclicPos;
+                    curMatch = _son[ptr0];
+                    len0 = len;
+                  }
+                }
+                MovePos();
+              } while (--num != 0);
   }
 
   void NormalizeLinks(int[] items, int numItems, int subValue) {
@@ -290,10 +303,15 @@ public class BinTree extends InWindow {
   }
 
   void Normalize() {
-    int subValue = _pos - _cyclicBufferSize;
-    NormalizeLinks(_son, _cyclicBufferSize * 2, subValue);
-    NormalizeLinks(_hash, _hashSizeSum, subValue);
-    ReduceOffsets(subValue);
+          int subValue = _pos - _cyclicBufferSize;
+          
+          if (_son == null) {
+              _son = new int[_cyclicBufferSize * 2];
+          }
+          
+          NormalizeLinks(Nullability.castToNonnull(_son), _cyclicBufferSize * 2, subValue);
+          NormalizeLinks(_hash, _hashSizeSum, subValue);
+          ReduceOffsets(subValue);
   }
 
   public void SetCutValue(int cutValue) {
