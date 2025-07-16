a78e8ae270c147c711c20915855b3f9a5920cfbe
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:55:42 -0700

    before:245to244 - Type='PASS_NULLABLE', message='passing @Nullable parameter 'info.data' where @NonNull is required' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java:91       texture.load(info.data);

--- gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java
@@ -80,26 +80,25 @@ public class TextureLoader
       }
     }
 
-  @Nullable
-  @Override
-  public Texture loadSync(
-      AssetManager manager,
-      String fileName,
-      FileHandle file,
-      @Nullable TextureParameter parameter) {
-    if (info == null) return null;
-    Texture texture = info.texture;
-    if (texture != null) {
-      texture.load(info.data);
-    } else {
-      texture = new Texture(info.data);
+  @Nullable @Override
+    public Texture loadSync(
+        AssetManager manager,
+        String fileName,
+        FileHandle file,
+        @Nullable TextureParameter parameter) {
+      if (info == null || info.data == null) return null;
+      Texture texture = info.texture;
+      if (texture != null) {
+        texture.load(info.data);
+      } else {
+        texture = new Texture(info.data);
+      }
+      if (parameter != null) {
+        texture.setFilter(parameter.minFilter, parameter.magFilter);
+        texture.setWrap(parameter.wrapU, parameter.wrapV);
+      }
+      return texture;
     }
-    if (parameter != null) {
-      texture.setFilter(parameter.minFilter, parameter.magFilter);
-      texture.setWrap(parameter.wrapU, parameter.wrapV);
-    }
-    return texture;
-  }
 
   @Nullable
   @Override
