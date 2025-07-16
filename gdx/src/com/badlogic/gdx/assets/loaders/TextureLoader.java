d5aaa5318a0b2de66fd32e8fa4ae2670b05cfe69
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:36:36 -0700

    before:311to310 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression info.data is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java:78     if (!info.data.isPrepared()) info.data.prepare();

--- gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java
@@ -53,30 +53,32 @@ public class TextureLoader
   }
 
   @Override
-  public void loadAsync(
-      AssetManager manager,
-      String fileName,
-      FileHandle file,
-      @Nullable TextureParameter parameter) {
-    info.filename = fileName;
-    if (parameter == null || parameter.textureData == null) {
-      Format format = null;
-      boolean genMipMaps = false;
-      info.texture = null;
-
-      if (parameter != null) {
-        format = parameter.format;
-        genMipMaps = parameter.genMipMaps;
+    public void loadAsync(
+        AssetManager manager,
+        String fileName,
+        FileHandle file,
+        @Nullable TextureParameter parameter) {
+      info.filename = fileName;
+      if (parameter == null || parameter.textureData == null) {
+        Format format = null;
+        boolean genMipMaps = false;
+        info.texture = null;
+  
+        if (parameter != null) {
+          format = parameter.format;
+          genMipMaps = parameter.genMipMaps;
+          info.texture = parameter.texture;
+        }
+  
+        info.data = TextureData.Factory.loadFromFile(file, format, genMipMaps);
+      } else {
+        info.data = parameter.textureData;
         info.texture = parameter.texture;
       }
-
-      info.data = TextureData.Factory.loadFromFile(file, format, genMipMaps);
-    } else {
-      info.data = parameter.textureData;
-      info.texture = parameter.texture;
+      if (info.data != null && !info.data.isPrepared()) {
+        info.data.prepare();
+      }
     }
-    if (!info.data.isPrepared()) info.data.prepare();
-  }
 
   @Nullable
   @Override
