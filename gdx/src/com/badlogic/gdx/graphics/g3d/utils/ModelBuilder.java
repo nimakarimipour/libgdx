0a4e4ef2272f36204f917fb71ea98aed44a38a76
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:54:28 -0700

    fix: 315 - DEREFERENCE_NULLABLE - node.id = "node" + model.nodes.size; - dereferenced expression model is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/utils/ModelBuilder.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/utils/ModelBuilder.java
@@ -117,6 +117,7 @@ public class ModelBuilder {
    * @return The node being created.
    */
   public Node node() {
+    if (model == null) throw new GdxRuntimeException("Call begin() first");
     final Node node = new Node();
     node(node);
     node.id = "node" + model.nodes.size;
