736362f9efd4d536a4d0abc82d1e3301796fe06a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:39:30 -0700

    before:302to301 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression sprite is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java:85     setMinWidth(sprite.getWidth());

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/SpriteDrawable.java
@@ -81,10 +81,12 @@ public class SpriteDrawable extends BaseDrawable implements TransformDrawable {
   }
 
   public void setSprite(@Nullable Sprite sprite) {
-    this.sprite = sprite;
-    setMinWidth(sprite.getWidth());
-    setMinHeight(sprite.getHeight());
-  }
+      if (sprite != null) {
+          this.sprite = sprite;
+          setMinWidth(sprite.getWidth());
+          setMinHeight(sprite.getHeight());
+      }
+    }
 
   @Nullable
   public Sprite getSprite() {
