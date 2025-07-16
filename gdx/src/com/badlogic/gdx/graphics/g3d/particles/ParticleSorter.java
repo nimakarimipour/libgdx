0334f62e7a6a140caaaf14371b4f8845e833c311
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 18:01:23 -0700

    before:223to222 - Type='FIELD_NO_INIT', message='@NonNull field ParticleSorter$Distance.particleOffsets not initialized' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java:58     private int[] particleIndices, particleOffsets;

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/ParticleSorter.java
@@ -55,7 +55,7 @@ public abstract class ParticleSorter {
   /** This class will sort all the particles using the distance from camera. */
   public static class Distance extends ParticleSorter {
     private float[] distances = new float[0];
-    private int[] particleIndices = new int[0], particleOffsets;
+    private int[] particleIndices = new int[0], particleOffsets = new int[0];
     private int currentSize = 0;
 
     @Override
