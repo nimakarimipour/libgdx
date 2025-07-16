967fee9f640c25050b1c4738d766906d22b55aa6
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:21:46 -0700

    Revert "fix: 214 - METHOD_NO_INIT - public RemoteSender(String ip, int port) { - initializer method does not guarantee @NonNull field out (line 32) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit a99600fdf6465a3e4d1a5d17ca55d5e3df0fe72e.

--- gdx/src/com/badlogic/gdx/input/RemoteSender.java
+++ gdx/src/com/badlogic/gdx/input/RemoteSender.java
@@ -19,10 +19,8 @@ package com.badlogic.gdx.input;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Peripheral;
 import com.badlogic.gdx.InputProcessor;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.DataOutputStream;
 import java.net.Socket;
-import javax.annotation.Nullable;
 
 /**
  * Sends all inputs from touch, key, accelerometer and compass to a {@link RemoteInput} at the given
@@ -31,7 +29,7 @@ import javax.annotation.Nullable;
  * @author mzechner
  */
 public class RemoteSender implements InputProcessor {
-  @Nullable private DataOutputStream out;
+  private DataOutputStream out;
   private boolean connected = false;
 
   public static final int KEY_DOWN = 0;
@@ -63,10 +61,10 @@ public class RemoteSender implements InputProcessor {
 
   public void sendUpdate() {
     synchronized (this) {
-      if (!connected || out == null) return;
+      if (!connected) return;
     }
     try {
-      Nullability.castToNonnull(out, "synchronized null check").writeInt(ACCEL);
+      out.writeInt(ACCEL);
       out.writeFloat(Gdx.input.getAccelerometerX());
       out.writeFloat(Gdx.input.getAccelerometerY());
       out.writeFloat(Gdx.input.getAccelerometerZ());
@@ -90,11 +88,11 @@ public class RemoteSender implements InputProcessor {
   @Override
   public boolean keyDown(int keycode) {
     synchronized (this) {
-      if (!connected || out == null) return false;
+      if (!connected) return false;
     }
 
     try {
-      Nullability.castToNonnull(out, "checked before usage").writeInt(KEY_DOWN);
+      out.writeInt(KEY_DOWN);
       out.writeInt(keycode);
     } catch (Throwable t) {
       synchronized (this) {
@@ -107,11 +105,11 @@ public class RemoteSender implements InputProcessor {
   @Override
   public boolean keyUp(int keycode) {
     synchronized (this) {
-      if (!connected || out == null) return false;
+      if (!connected) return false;
     }
 
     try {
-      Nullability.castToNonnull(out, "checked not null").writeInt(KEY_UP);
+      out.writeInt(KEY_UP);
       out.writeInt(keycode);
     } catch (Throwable t) {
       synchronized (this) {
@@ -124,11 +122,10 @@ public class RemoteSender implements InputProcessor {
   @Override
   public boolean keyTyped(char character) {
     synchronized (this) {
-      if (!connected || out == null) return false;
+      if (!connected) return false;
     }
 
     try {
-      Nullability.castToNonnull(out, "synchronized block prevents null");
       out.writeInt(KEY_TYPED);
       out.writeChar(character);
     } catch (Throwable t) {
@@ -142,7 +139,7 @@ public class RemoteSender implements InputProcessor {
   @Override
   public boolean touchDown(int x, int y, int pointer, int button) {
     synchronized (this) {
-      if (!connected || out == null) return false;
+      if (!connected) return false;
     }
 
     try {
@@ -165,12 +162,10 @@ public class RemoteSender implements InputProcessor {
     }
 
     try {
-      if (out != null) {
-        Nullability.castToNonnull(out, "if condition ensures non-null").writeInt(TOUCH_UP);
-        out.writeInt(x);
-        out.writeInt(y);
-        out.writeInt(pointer);
-      }
+      out.writeInt(TOUCH_UP);
+      out.writeInt(x);
+      out.writeInt(y);
+      out.writeInt(pointer);
     } catch (Throwable t) {
       synchronized (this) {
         connected = false;
@@ -187,11 +182,11 @@ public class RemoteSender implements InputProcessor {
   @Override
   public boolean touchDragged(int x, int y, int pointer) {
     synchronized (this) {
-      if (!connected || out == null) return false;
+      if (!connected) return false;
     }
 
     try {
-      Nullability.castToNonnull(out, "synchronized block check").writeInt(TOUCH_DRAGGED);
+      out.writeInt(TOUCH_DRAGGED);
       out.writeInt(x);
       out.writeInt(y);
       out.writeInt(pointer);
