10481bc0584083845d78b3b65f300ddef5c62f95
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:23:42 -0700

    before:360to359 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression style.background is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java:167     if (background != null) x -= style.background.getLeftWidth();

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextField.java
@@ -158,23 +158,23 @@ public class TextField extends Widget implements Disableable {
   }
 
   protected int letterUnderCursor(float x) {
-    x -=
-        textOffset
-            + fontOffset
-            - style.font.getData().cursorX
-            - glyphPositions.get(visibleTextStart);
-    Drawable background = getBackgroundDrawable();
-    if (background != null) x -= style.background.getLeftWidth();
-    int n = this.glyphPositions.size;
-    float[] glyphPositions = this.glyphPositions.items;
-    for (int i = 1; i < n; i++) {
-      if (glyphPositions[i] > x) {
-        if (glyphPositions[i] - x <= x - glyphPositions[i - 1]) return i;
-        return i - 1;
+      x -=
+          textOffset
+              + fontOffset
+              - style.font.getData().cursorX
+              - glyphPositions.get(visibleTextStart);
+      Drawable background = getBackgroundDrawable();
+      if (style.background != null) x -= style.background.getLeftWidth();
+      int n = this.glyphPositions.size;
+      float[] glyphPositions = this.glyphPositions.items;
+      for (int i = 1; i < n; i++) {
+        if (glyphPositions[i] > x) {
+          if (glyphPositions[i] - x <= x - glyphPositions[i - 1]) return i;
+          return i - 1;
+        }
       }
+      return n - 1;
     }
-    return n - 1;
-  }
 
   protected boolean isWordCharacter(char c) {
     return Character.isLetterOrDigit(c);
