3a73769533800bc3f37abc611ce4c3c91f7cc680
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:55:50 -0700

    fix: 321 - PASS_NULLABLE - texture.load(info.data); - passing @Nullable parameter 'info.data' where @NonNull is required

--- gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java
+++ gdx/src/com/badlogic/gdx/assets/loaders/TextureLoader.java
@@ -26,6 +26,7 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.TextureData;
 import com.badlogic.gdx.utils.Array;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -88,9 +89,9 @@ public class TextureLoader
     if (info == null) return null;
     Texture texture = info.texture;
     if (texture != null) {
-      texture.load(info.data);
+      texture.load(Nullability.castToNonnull(info.data));
     } else {
-      texture = new Texture(info.data);
+      texture = new Texture(Nullability.castToNonnull(info.data));
     }
     if (parameter != null) {
       texture.setFilter(parameter.minFilter, parameter.magFilter);
