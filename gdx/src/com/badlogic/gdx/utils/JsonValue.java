8736070d4e0a7a1fd96962c7e8d41f98afe2c254
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:19:36 -0700

    before:366to365 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression current is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/JsonValue.java:1472       if (current.prev == null) {

--- gdx/src/com/badlogic/gdx/utils/JsonValue.java
+++ gdx/src/com/badlogic/gdx/utils/JsonValue.java
@@ -1474,15 +1474,18 @@ public class JsonValue implements Iterable<JsonValue> {
     }
 
     public void remove() {
-      if (current.prev == null) {
-        child = current.next;
-        if (child != null) child.prev = null;
-      } else {
-        current.prev.next = current.next;
-        if (current.next != null) current.next.prev = current.prev;
+        if (current == null) {
+          throw new IllegalStateException("No current element to remove");
+        }
+        if (current.prev == null) {
+          child = current.next;
+          if (child != null) child.prev = null;
+        } else {
+          current.prev.next = current.next;
+          if (current.next != null) current.next.prev = current.prev;
+        }
+        size--;
       }
-      size--;
-    }
 
     public Iterator<JsonValue> iterator() {
       return this;
