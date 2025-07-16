7ebf201363a34c963d04a3f28e3a08e49c950d51
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 23:55:30 -0700

    fix: 10 - METHOD_NO_INIT - public VerticalGroup() { - initializer method does not guarantee @NonNull field columnSizes (line 47) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/VerticalGroup.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/VerticalGroup.java
@@ -24,6 +24,8 @@ import com.badlogic.gdx.scenes.scene2d.utils.Layout;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.FloatArray;
 import com.badlogic.gdx.utils.SnapshotArray;
+import edu.ucr.cs.riple.annotator.util.Nullability;
+import javax.annotation.Nullable;
 
 /**
  * A group that lays out its children top to bottom vertically, with optional wrapping. {@link
@@ -44,7 +46,7 @@ import com.badlogic.gdx.utils.SnapshotArray;
 public class VerticalGroup extends WidgetGroup {
   private float prefWidth, prefHeight, lastPrefWidth;
   private boolean sizeInvalid = true;
-  private FloatArray columnSizes; // column height, column width, ...
+  @Nullable private FloatArray columnSizes; // column height, column width, ...
 
   private int align = Align.top, columnAlign;
   private boolean reverse, round = true, wrap, expand;
@@ -228,7 +230,10 @@ public class VerticalGroup extends WidgetGroup {
     groupHeight -= padTop;
     align = columnAlign;
 
+    // Ensure columnSizes is initialized
+    if (columnSizes == null) columnSizes = new FloatArray();
     FloatArray columnSizes = this.columnSizes;
+
     SnapshotArray<Actor> children = getChildren();
     int i = 0, n = children.size, incr = 1;
     if (reverse) {
@@ -255,8 +260,12 @@ public class VerticalGroup extends WidgetGroup {
         r =
             Math.min(
                 r,
-                columnSizes.size
-                    - 2); // In case an actor changed size without invalidating this layout.
+                Nullability.castToNonnull(columnSizes, "always initialized before use").size
+                    - 2); // In case an actor
+        // changed size
+        // without
+        // invalidating
+        // this layout.
         y = yStart;
         if ((align & Align.bottom) != 0) y -= maxHeight - columnSizes.get(r);
         else if ((align & Align.top) == 0) // center
@@ -304,7 +313,13 @@ public class VerticalGroup extends WidgetGroup {
 
   /** When wrapping is enabled, the number of columns may be > 1. */
   public int getColumns() {
-    return wrap ? columnSizes.size >> 1 : 1;
+    if (wrap) {
+      if (columnSizes == null) {
+        columnSizes = new FloatArray();
+      }
+      return columnSizes.size >> 1;
+    }
+    return 1;
   }
 
   /** If true (the default), positions and sizes are rounded to integers. */
