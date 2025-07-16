dc5b9ea10f1f37aac758ea26dec56436ffcd45c9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:03:49 -0700

    Revert "fix: 157 - METHOD_NO_INIT - public ObjectSet(int initialCapacity, float loadFactor) { - initializer method does not guarantee @NonNull fields iterator1 (line 75), iterator2 (line 75) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 522cf5f9ee3b83ab2152db035870cfb9c9b45332.

--- gdx/src/com/badlogic/gdx/utils/ObjectSet.java
+++ gdx/src/com/badlogic/gdx/utils/ObjectSet.java
@@ -17,7 +17,6 @@
 package com.badlogic.gdx.utils;
 
 import com.badlogic.gdx.math.MathUtils;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -73,7 +72,7 @@ public class ObjectSet<T> implements Iterable<T> {
    */
   protected int mask;
 
-  @Nullable private transient ObjectSetIterator iterator1, iterator2;
+  private transient ObjectSetIterator iterator1, iterator2;
 
   /** Creates a new set with an initial capacity of 51 and a load factor of 0.8. */
   public ObjectSet() {
@@ -378,11 +377,11 @@ public class ObjectSet<T> implements Iterable<T> {
     if (!iterator1.valid) {
       iterator1.reset();
       iterator1.valid = true;
-      Nullability.castToNonnull(iterator2, "initialized before use").valid = false;
+      iterator2.valid = false;
       return iterator1;
     }
-    Nullability.castToNonnull(iterator2, "initialized before use").reset();
-    Nullability.castToNonnull(iterator2, "initialized before use").valid = true;
+    iterator2.reset();
+    iterator2.valid = true;
     iterator1.valid = false;
     return iterator2;
   }
