8df87d2a9d1242faafd9a45dc51ee7343850302b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:41:34 -0700

    fix: 130 - METHOD_NO_INIT - public RegionInfluencer(int regionsCount) { - initializer method does not guarantee @NonNull field regionChannel (line 216) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/RegionInfluencer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/RegionInfluencer.java
@@ -27,6 +27,7 @@ import com.badlogic.gdx.graphics.g3d.particles.ResourceData.SaveData;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -54,6 +55,10 @@ public abstract class RegionInfluencer extends Influencer {
 
     @Override
     public void init() {
+      // Ensure that regionChannel is initialized before use.
+      if (regionChannel == null) {
+        allocateChannels();
+      }
       AspectTextureRegion region = regions.items[0];
       for (int i = 0, c = controller.emitter.maxParticleCount * regionChannel.strideSize;
           i < c;
@@ -91,7 +96,15 @@ public abstract class RegionInfluencer extends Influencer {
 
     @Override
     public void activateParticles(int startIndex, int count) {
-      for (int i = startIndex * regionChannel.strideSize, c = i + count * regionChannel.strideSize;
+      if (regionChannel == null) {
+        throw new IllegalStateException("regionChannel is not initialized");
+      }
+
+      for (int
+              i =
+                  startIndex
+                      * Nullability.castToNonnull(regionChannel, "not null when used").strideSize,
+              c = i + count * regionChannel.strideSize;
           i < c;
           i += regionChannel.strideSize) {
         AspectTextureRegion region = regions.random();
@@ -139,9 +152,14 @@ public abstract class RegionInfluencer extends Influencer {
 
     @Override
     public void update() {
+      if (regionChannel == null) {
+        allocateChannels();
+      }
       for (int i = 0,
               l = ParticleChannels.LifePercentOffset,
-              c = controller.particles.size * regionChannel.strideSize;
+              c =
+                  controller.particles.size
+                      * Nullability.castToNonnull(regionChannel, "allocated if null").strideSize;
           i < c;
           i += regionChannel.strideSize, l += lifeChannel.strideSize) {
         AspectTextureRegion region = regions.get((int) (lifeChannel.data[l] * (regions.size - 1)));
@@ -213,7 +231,7 @@ public abstract class RegionInfluencer extends Influencer {
   }
 
   public Array<AspectTextureRegion> regions;
-  FloatChannel regionChannel;
+  @Nullable FloatChannel regionChannel;
   @Nullable public String atlasName;
 
   public RegionInfluencer(int regionsCount) {
