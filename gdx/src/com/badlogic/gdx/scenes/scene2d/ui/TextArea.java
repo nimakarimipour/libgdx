0fccd81706dbc432961125566192972e086566b5
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:51:46 -0700

    fix: 307 - METHOD_NO_INIT - public TextArea(String text, TextFieldStyle style) { - initializer method does not guarantee @NonNull field linesBreak (line 38) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextArea.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextArea.java
@@ -30,11 +30,13 @@ import com.badlogic.gdx.utils.IntArray;
 import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
+import com.uber.nullaway.annotations.Initializer;
 import javax.annotation.Nullable;
 
 /** A text input field with multiple lines. */
 public class TextArea extends TextField {
   /** Array storing lines breaks positions * */
+  @SuppressWarnings("NullAway.Init")
   IntArray linesBreak;
 
   /**
@@ -72,6 +74,7 @@ public class TextArea extends TextField {
     super(text, style);
   }
 
+  @Initializer
   protected void initialize() {
     super.initialize();
     writeEnters = true;
