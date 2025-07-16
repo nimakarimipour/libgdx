c1af5ca120dd2b954db5e721a3edfd82d1bae786
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:40:00 -0700

    Revert "fix: 267 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull field properties (line 94) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit d0a032bb57a0cf0ee55e0068df9871e9a59a4251.

--- gdx/src/com/badlogic/gdx/utils/I18NBundle.java
+++ gdx/src/com/badlogic/gdx/utils/I18NBundle.java
@@ -91,7 +91,7 @@ public class I18NBundle {
   private Locale locale;
 
   /** The properties for this bundle. */
-  @Nullable private ObjectMap<String, String> properties;
+  private ObjectMap<String, String> properties;
 
   /** The formatter used for argument replacement. */
   private TextFormatter formatter;
@@ -502,9 +502,6 @@ public class I18NBundle {
    *     and {@link #getExceptionOnMissingKey()} returns {@code false}
    */
   public String get(String key) {
-    if (properties == null) {
-      throw new IllegalStateException("Properties not initialized");
-    }
     String result = properties.get(key);
     if (result == null) {
       if (parent != null) result = parent.get(key);
@@ -540,8 +537,6 @@ public class I18NBundle {
    * @param placeholder
    */
   public void debug(String placeholder) {
-    if (properties == null) return;
-
     ObjectMap.Keys<String> keys = properties.keys();
     if (keys == null) return;
 
