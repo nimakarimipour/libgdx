d4c8312d45d874ec8cc17526b19f6f14db716bd6
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:34:49 -0700

    fix: 256 - DEREFERENCE_NULLABLE - debugRects.add(rect); - dereferenced expression debugRects is @Nullable

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Table.java
@@ -1288,6 +1288,7 @@ public class Table extends WidgetGroup {
   }
 
   private void addDebugRect(float x, float y, float w, float h, Color color) {
+    if (debugRects == null) debugRects = new Array();
     DebugRect rect = DebugRect.pool.obtain();
     rect.color = color;
     rect.set(x, y, w, h);
