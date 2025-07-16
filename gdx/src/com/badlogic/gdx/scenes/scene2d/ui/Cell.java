edf52e2ed52b2d7647b59f618aac324cbde7a3c9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:23:37 -0700

    fix: 75 - PASS_NULLABLE - return prefWidth.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.Pool.Poolable;
 import com.uber.nullaway.annotations.Initializer;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -774,7 +775,7 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getPrefWidth() {
-    return prefWidth.get(actor);
+    return prefWidth.get(Nullability.castToNonnull(actor));
   }
 
   /**
