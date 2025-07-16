0f8f035d762d410774e4926faa4d9b0e703d48a4
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:03:36 -0700

    fix: 343 - SWITCH_EXPRESSION_NULLABLE - switch (focusEvent.getType()) { - switch expression focusEvent.getType() is @Nullable

--- gdx/src/com/badlogic/gdx/scenes/scene2d/utils/FocusListener.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/utils/FocusListener.java
@@ -20,6 +20,7 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.utils.Null;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -31,7 +32,7 @@ public abstract class FocusListener implements EventListener {
   public boolean handle(Event event) {
     if (!(event instanceof FocusEvent)) return false;
     FocusEvent focusEvent = (FocusEvent) event;
-    switch (focusEvent.getType()) {
+    switch (Nullability.castToNonnull(focusEvent.getType())) {
       case keyboard:
         keyboardFocusChanged(focusEvent, event.getTarget(), focusEvent.isFocused());
         break;
