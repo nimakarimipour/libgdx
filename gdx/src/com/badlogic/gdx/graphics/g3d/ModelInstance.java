53cfd1c73d042bbdfc8fde1838803bb95a6b0c01
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 12:23:55 -0700

    before:250to249 - Type='PASS_NULLABLE', message='passing @Nullable parameter 'nanim.node.id' where @NonNull is required' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java:429       final Node node = getNode(nanim.node.id);

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -428,39 +428,39 @@ public class ModelInstance implements RenderableProvider {
    *     deep copy.
    */
   public void copyAnimation(Animation sourceAnim, boolean shareKeyframes) {
-    Animation animation = new Animation();
-    animation.id = sourceAnim.id;
-    animation.duration = sourceAnim.duration;
-    for (final NodeAnimation nanim : sourceAnim.nodeAnimations) {
-      final Node node = getNode(nanim.node.id);
-      if (node == null) continue;
-      NodeAnimation nodeAnim = new NodeAnimation();
-      nodeAnim.node = node;
-      if (shareKeyframes) {
-        nodeAnim.translation = nanim.translation;
-        nodeAnim.rotation = nanim.rotation;
-        nodeAnim.scaling = nanim.scaling;
-      } else {
-        if (nanim.translation != null) {
-          nodeAnim.translation = new Array<NodeKeyframe<Vector3>>();
-          for (final NodeKeyframe<Vector3> kf : nanim.translation)
-            nodeAnim.translation.add(new NodeKeyframe<Vector3>(kf.keytime, kf.value));
+      Animation animation = new Animation();
+      animation.id = sourceAnim.id;
+      animation.duration = sourceAnim.duration;
+      for (final NodeAnimation nanim : sourceAnim.nodeAnimations) {
+        final Node node = getNode(Nullability.castToNonnull(nanim.node.id));
+        if (node == null) continue;
+        NodeAnimation nodeAnim = new NodeAnimation();
+        nodeAnim.node = node;
+        if (shareKeyframes) {
+          nodeAnim.translation = nanim.translation;
+          nodeAnim.rotation = nanim.rotation;
+          nodeAnim.scaling = nanim.scaling;
+        } else {
+          if (nanim.translation != null) {
+            nodeAnim.translation = new Array<NodeKeyframe<Vector3>>();
+            for (final NodeKeyframe<Vector3> kf : nanim.translation)
+              nodeAnim.translation.add(new NodeKeyframe<Vector3>(kf.keytime, kf.value));
+          }
+          if (nanim.rotation != null) {
+            nodeAnim.rotation = new Array<NodeKeyframe<Quaternion>>();
+            for (final NodeKeyframe<Quaternion> kf : nanim.rotation)
+              nodeAnim.rotation.add(new NodeKeyframe<Quaternion>(kf.keytime, kf.value));
+          }
+          if (nanim.scaling != null) {
+            nodeAnim.scaling = new Array<NodeKeyframe<Vector3>>();
+            for (final NodeKeyframe<Vector3> kf : nanim.scaling)
+              nodeAnim.scaling.add(new NodeKeyframe<Vector3>(kf.keytime, kf.value));
+          }
         }
-        if (nanim.rotation != null) {
-          nodeAnim.rotation = new Array<NodeKeyframe<Quaternion>>();
-          for (final NodeKeyframe<Quaternion> kf : nanim.rotation)
-            nodeAnim.rotation.add(new NodeKeyframe<Quaternion>(kf.keytime, kf.value));
-        }
-        if (nanim.scaling != null) {
-          nodeAnim.scaling = new Array<NodeKeyframe<Vector3>>();
-          for (final NodeKeyframe<Vector3> kf : nanim.scaling)
-            nodeAnim.scaling.add(new NodeKeyframe<Vector3>(kf.keytime, kf.value));
-        }
+        if (nodeAnim.translation != null || nodeAnim.rotation != null || nodeAnim.scaling != null)
+          animation.nodeAnimations.add(nodeAnim);
       }
-      if (nodeAnim.translation != null || nodeAnim.rotation != null || nodeAnim.scaling != null)
-        animation.nodeAnimations.add(nodeAnim);
-    }
-    if (animation.nodeAnimations.size > 0) animations.add(animation);
+      if (animation.nodeAnimations.size > 0) animations.add(animation);
   }
 
   /**
