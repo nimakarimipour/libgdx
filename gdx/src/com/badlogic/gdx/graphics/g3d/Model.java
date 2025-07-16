0555bc6653b07b6f741186630f383f3e0e28e1eb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 11:55:40 -0700

    before:296to295 - Type='FIELD_NO_INIT', message='@NonNull field parts not initialized' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/model/data/ModelMesh.java:26   public ModelMeshPart[] parts;

--- gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/Model.java
@@ -56,6 +56,7 @@ import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.ObjectMap;
 import java.nio.Buffer;
 import javax.annotation.Nullable;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * A model represents a 3D assets. It stores a hierarchy of nodes. A node has a transform and
@@ -259,36 +260,40 @@ public class Model implements Disposable {
   }
 
   protected void convertMesh(ModelMesh modelMesh) {
-    int numIndices = 0;
-    for (ModelMeshPart part : modelMesh.parts) {
-      numIndices += part.indices.length;
-    }
-    boolean hasIndices = numIndices > 0;
-    VertexAttributes attributes = new VertexAttributes(modelMesh.attributes);
-    int numVertices = modelMesh.vertices.length / (attributes.vertexSize / 4);
-
-    Mesh mesh = new Mesh(true, numVertices, numIndices, attributes);
-    meshes.add(mesh);
-    disposables.add(mesh);
-
-    BufferUtils.copy(modelMesh.vertices, mesh.getVerticesBuffer(), modelMesh.vertices.length, 0);
-    int offset = 0;
-    ((Buffer) mesh.getIndicesBuffer()).clear();
-    for (ModelMeshPart part : modelMesh.parts) {
-      MeshPart meshPart = new MeshPart();
-      meshPart.id = part.id;
-      meshPart.primitiveType = part.primitiveType;
-      meshPart.offset = offset;
-      meshPart.size = hasIndices ? part.indices.length : numVertices;
-      meshPart.mesh = mesh;
-      if (hasIndices) {
-        mesh.getIndicesBuffer().put(part.indices);
-      }
-      offset += meshPart.size;
-      meshParts.add(meshPart);
-    }
-    ((Buffer) mesh.getIndicesBuffer()).position(0);
-    for (MeshPart part : meshParts) part.update();
+          if (modelMesh.parts == null) {
+              throw new NullPointerException("ModelMesh parts cannot be null");
+          }
+    
+          int numIndices = 0;
+          for (ModelMeshPart part : Nullability.castToNonnull(modelMesh.parts, "checked for null")) {
+              numIndices += part.indices.length;
+          }
+          boolean hasIndices = numIndices > 0;
+          VertexAttributes attributes = new VertexAttributes(modelMesh.attributes);
+          int numVertices = modelMesh.vertices.length / (attributes.vertexSize / 4);
+    
+          Mesh mesh = new Mesh(true, numVertices, numIndices, attributes);
+          meshes.add(mesh);
+          disposables.add(mesh);
+    
+          BufferUtils.copy(modelMesh.vertices, mesh.getVerticesBuffer(), modelMesh.vertices.length, 0);
+          int offset = 0;
+          ((Buffer) mesh.getIndicesBuffer()).clear();
+          for (ModelMeshPart part : Nullability.castToNonnull(modelMesh.parts, "checked for null")) {
+              MeshPart meshPart = new MeshPart();
+              meshPart.id = part.id;
+              meshPart.primitiveType = part.primitiveType;
+              meshPart.offset = offset;
+              meshPart.size = hasIndices ? part.indices.length : numVertices;
+              meshPart.mesh = mesh;
+              if (hasIndices) {
+                  mesh.getIndicesBuffer().put(part.indices);
+              }
+              offset += meshPart.size;
+              meshParts.add(meshPart);
+          }
+          ((Buffer) mesh.getIndicesBuffer()).position(0);
+          for (MeshPart part : meshParts) part.update();
   }
 
   protected void loadMaterials(
