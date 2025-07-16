bc9a115d3244826d461c6238582e2f871bca61f3
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:59:05 -0700

    Revert "fix: 332 - DEREFERENCE_NULLABLE - region.page.texture, - dereferenced expression region.page is @Nullable"
    This reverts commit a9694f9f34942cf7f3cc56d2bb602e287c0bed88.

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
@@ -32,7 +32,6 @@ import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.ObjectSet;
 import com.badlogic.gdx.utils.StreamUtils;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -109,7 +108,7 @@ public class TextureAtlas implements Disposable {
     for (Region region : data.regions) {
       AtlasRegion atlasRegion =
           new AtlasRegion(
-              Nullability.castToNonnull(region.page, "always initialized").texture,
+              region.page.texture,
               region.left,
               region.top, //
               region.rotate ? region.height : region.width, //
