a6de9369783592f20b834a3f1d49f0f0d0778d9a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:20:24 -0700

    fix: 64 - PASS_NULLABLE - currentShader.begin(camera, context); - passing @Nullable parameter 'camera' where @NonNull is required

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelBatch.java
@@ -30,6 +30,7 @@ import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.FlushablePool;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.Pool;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -277,7 +278,7 @@ public class ModelBatch implements Disposable {
       if (currentShader != renderable.shader) {
         if (currentShader != null) currentShader.end();
         currentShader = renderable.shader;
-        currentShader.begin(camera, context);
+        currentShader.begin(Nullability.castToNonnull(camera), context);
       }
       currentShader.render(renderable);
     }
