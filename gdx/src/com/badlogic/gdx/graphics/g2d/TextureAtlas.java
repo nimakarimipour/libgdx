e6c441324c7955b81826ec9c10aed2096b8de295
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:12:34 -0700

    fix: 359 - PASS_NULLABLE - super(texture, x, y, width, height); - passing @Nullable parameter 'texture' where @NonNull is required

--- gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/TextureAtlas.java
@@ -32,6 +32,7 @@ import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.ObjectSet;
 import com.badlogic.gdx.utils.StreamUtils;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -670,7 +671,7 @@ public class TextureAtlas implements Disposable {
     public @Null int[][] values;
 
     public AtlasRegion(@Nullable Texture texture, int x, int y, int width, int height) {
-      super(texture, x, y, width, height);
+      super(Nullability.castToNonnull(texture), x, y, width, height);
       originalWidth = width;
       originalHeight = height;
       packedWidth = width;
