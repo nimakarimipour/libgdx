9189366326e416cdb680e09ccf275238a5cbd8fd
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 11:46:48 -0700

    before:309to308 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression _stream is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/compression/lz/OutWindow.java:42     _stream.write(_buffer, _streamPos, size);

--- gdx/src/com/badlogic/gdx/utils/compression/lz/OutWindow.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lz/OutWindow.java
@@ -37,12 +37,13 @@ public class OutWindow {
   }
 
   public void Flush() throws IOException {
-    int size = _pos - _streamPos;
-    if (size == 0) return;
-    _stream.write(_buffer, _streamPos, size);
-    if (_pos >= _windowSize) _pos = 0;
-    _streamPos = _pos;
-  }
+        if (_stream == null) return; // Ensure _stream is not null before proceeding
+        int size = _pos - _streamPos;
+        if (size == 0) return;
+        _stream.write(_buffer, _streamPos, size);
+        if (_pos >= _windowSize) _pos = 0;
+        _streamPos = _pos;
+    }
 
   public void CopyBlock(int distance, int len) throws IOException {
     int pos = _pos - distance - 1;
