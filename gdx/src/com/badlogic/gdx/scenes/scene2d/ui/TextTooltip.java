780250ef7e285afaf99473db279d31ed5964c3cc
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:29:18 -0700

    fix: 243 - PASS_NULLABLE - container.setActor(newLabel(text, style.label)); - passing @Nullable parameter 'style.label' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/TextTooltip.java
@@ -19,6 +19,7 @@ package com.badlogic.gdx.scenes.scene2d.ui;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Null;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -51,7 +52,7 @@ public class TextTooltip extends Tooltip<Label> {
       @Null String text, @Nullable final TooltipManager manager, TextTooltipStyle style) {
     super(null, manager);
 
-    container.setActor(newLabel(text, style.label));
+    container.setActor(newLabel(text, Nullability.castToNonnull(style.label)));
 
     setStyle(style);
   }
