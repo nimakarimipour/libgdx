fe8e135376a180596b407a93e4149bc4b53c795d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:06:19 -0700

    fix: 346 - DEREFERENCE_NULLABLE - c = i + controller.particles.size * rotationalVelocity2dChannel.strideSize; - dereferenced expression rotationalVelocity2dChannel is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/influencers/DynamicsModifier.java
@@ -227,17 +227,19 @@ public abstract class DynamicsModifier extends Influencer {
 
     @Override
     public void update() {
-      for (int i = 0,
-              l = ParticleChannels.LifePercentOffset,
-              s = 0,
-              c = i + controller.particles.size * rotationalVelocity2dChannel.strideSize;
-          i < c;
-          s += strengthChannel.strideSize, i += rotationalVelocity2dChannel.strideSize,
-              l += lifeChannel.strideSize) {
-        rotationalVelocity2dChannel.data[i] +=
-            strengthChannel.data[s + ParticleChannels.VelocityStrengthStartOffset]
-                + strengthChannel.data[s + ParticleChannels.VelocityStrengthDiffOffset]
-                    * strengthValue.getScale(lifeChannel.data[l]);
+      if (rotationalVelocity2dChannel != null) { // Add null check
+        for (int i = 0,
+                l = ParticleChannels.LifePercentOffset,
+                s = 0,
+                c = i + controller.particles.size * rotationalVelocity2dChannel.strideSize;
+            i < c;
+            s += strengthChannel.strideSize, i += rotationalVelocity2dChannel.strideSize,
+                l += lifeChannel.strideSize) {
+          rotationalVelocity2dChannel.data[i] +=
+              strengthChannel.data[s + ParticleChannels.VelocityStrengthStartOffset]
+                  + strengthChannel.data[s + ParticleChannels.VelocityStrengthDiffOffset]
+                      * strengthValue.getScale(lifeChannel.data[l]);
+        }
       }
     }
 
