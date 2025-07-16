ab50e8d9d2f624b546e0a2496b6b7d09fa9a509e
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 12:34:18 -0700

    before:230to227 - Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/maps/tiled/TiledMapTileSets.java:53     return null;

--- gdx/src/com/badlogic/gdx/maps/tiled/TiledMapTileSets.java
+++ gdx/src/com/badlogic/gdx/maps/tiled/TiledMapTileSets.java
@@ -44,7 +44,7 @@ public class TiledMapTileSets implements Iterable<TiledMapTileSet> {
    * @param name Name of the {@link TiledMapTileSet} to retrieve.
    * @return tileset with matching name, null if it doesn't exist
    */
-  public TiledMapTileSet getTileSet(String name) {
+  @Nullable public TiledMapTileSet getTileSet(String name) {
     for (TiledMapTileSet tileset : tilesets) {
       if (name.equals(tileset.getName())) {
         return tileset;
