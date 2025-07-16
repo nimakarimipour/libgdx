482ad79a1b4bccbbd9d986191c67172110692167
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:51:42 -0700

    Revert "fix: 446 - DEREFERENCE_NULLABLE - return this.set(vector.x, vector.y, vector.z); - dereferenced expression vector is @Nullable"
    This reverts commit e9176ead6a575e6731fd8c098c37faae8c20394d.

--- gdx/src/com/badlogic/gdx/math/Vector3.java
+++ gdx/src/com/badlogic/gdx/math/Vector3.java
@@ -18,7 +18,6 @@ package com.badlogic.gdx.math;
 
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.NumberUtils;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.Serializable;
 import javax.annotation.Nullable;
 
@@ -106,10 +105,7 @@ public class Vector3 implements Serializable, Vector<Vector3> {
 
   @Override
   public Vector3 set(@Nullable final Vector3 vector) {
-    return this.set(
-        Nullability.castToNonnull(vector).x,
-        Nullability.castToNonnull(vector).y,
-        Nullability.castToNonnull(vector).z);
+    return this.set(vector.x, vector.y, vector.z);
   }
 
   /**
