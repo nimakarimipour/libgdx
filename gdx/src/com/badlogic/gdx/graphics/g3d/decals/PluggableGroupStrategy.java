71b10f8986b5671e83dee3a584cb27e81941b5e4
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:16:43 -0700

    fix: 199 - DEREFERENCE_NULLABLE - plugs.get(group).afterGroup(); - dereferenced expression plugs.get(group) is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/decals/PluggableGroupStrategy.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/decals/PluggableGroupStrategy.java
@@ -34,7 +34,10 @@ public abstract class PluggableGroupStrategy implements GroupStrategy {
 
   @Override
   public void afterGroup(int group) {
-    plugs.get(group).afterGroup();
+    Plug plug = plugs.get(group);
+    if (plug != null) {
+      plug.afterGroup();
+    }
   }
 
   /**
