721773bb18ac74d0912df69e1925b7854d9c1d9b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:20:01 -0700

    before:365to364 - Type='ASSIGN_FIELD_NULLABLE', message='assigning @Nullable expression to @NonNull field' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java:1002     colspan = null;

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -992,29 +992,29 @@ public class Cell<T extends Actor> implements Poolable {
 
   /** Sets all constraint fields to null. */
   void clear() {
-    minWidth = null;
-    minHeight = null;
-    prefWidth = null;
-    prefHeight = null;
-    maxWidth = null;
-    maxHeight = null;
-    spaceTop = null;
-    spaceLeft = null;
-    spaceBottom = null;
-    spaceRight = null;
-    padTop = null;
-    padLeft = null;
-    padBottom = null;
-    padRight = null;
-    fillX = null;
-    fillY = null;
-    align = null;
-    expandX = null;
-    expandY = null;
-    colspan = null;
-    uniformX = null;
-    uniformY = null;
-  }
+      minWidth = null;
+      minHeight = null;
+      prefWidth = null;
+      prefHeight = null;
+      maxWidth = null;
+      maxHeight = null;
+      spaceTop = null;
+      spaceLeft = null;
+      spaceBottom = null;
+      spaceRight = null;
+      padTop = null;
+      padLeft = null;
+      padBottom = null;
+      padRight = null;
+      fillX = null;
+      fillY = null;
+      align = null;
+      expandX = null;
+      expandY = null;
+      colspan = 0;
+      uniformX = null;
+      uniformY = null;
+    }
 
   /**
    * Reset state so the cell can be reused, setting all constraints to their {@link #defaults()
