d7deeb688d7dec83190343454c7eb96ef4e21cea
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:42:18 -0700

    fix: 414 - ASSIGN_FIELD_NULLABLE - animation.id = anim.id; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -568,13 +568,14 @@ public class ModelInstance implements RenderableProvider {
   @Nullable
   public Animation getAnimation(final String id, boolean ignoreCase) {
     final int n = animations.size;
-    Animation animation;
-    if (ignoreCase) {
-      for (int i = 0; i < n; i++)
-        if ((animation = animations.get(i)).id.equalsIgnoreCase(id)) return animation;
-    } else {
-      for (int i = 0; i < n; i++)
-        if ((animation = animations.get(i)).id.equals(id)) return animation;
+    for (int i = 0; i < n; i++) {
+      Animation animation = animations.get(i);
+      if (animation == null || animation.id == null) continue;
+      if (ignoreCase) {
+        if (animation.id.equalsIgnoreCase(id)) return animation;
+      } else {
+        if (animation.id.equals(id)) return animation;
+      }
     }
     return null;
   }
