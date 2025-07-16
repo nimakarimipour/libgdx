c4b4f9b3ad1f7f6c9de9d23048a692746993a4ec
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:56:06 -0700

    before:243to241 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression stage is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java:186               if (clampPosition && windowY + height + amountY > stage.getHeight())

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Window.java
@@ -144,52 +144,52 @@ public class Window extends Table {
           }
 
           public void touchDragged(InputEvent event, float x, float y, int pointer) {
-            if (!dragging) return;
-            float width = getWidth(), height = getHeight();
-            float windowX = getX(), windowY = getY();
-
-            float minWidth = getMinWidth(), maxWidth = getMaxWidth();
-            float minHeight = getMinHeight(), maxHeight = getMaxHeight();
-            Stage stage = getStage();
-            boolean clampPosition =
-                keepWithinStage && stage != null && getParent() == stage.getRoot();
-
-            if ((edge & MOVE) != 0) {
-              float amountX = x - startX, amountY = y - startY;
-              windowX += amountX;
-              windowY += amountY;
-            }
-            if ((edge & Align.left) != 0) {
-              float amountX = x - startX;
-              if (width - amountX < minWidth) amountX = -(minWidth - width);
-              if (clampPosition && windowX + amountX < 0) amountX = -windowX;
-              width -= amountX;
-              windowX += amountX;
-            }
-            if ((edge & Align.bottom) != 0) {
-              float amountY = y - startY;
-              if (height - amountY < minHeight) amountY = -(minHeight - height);
-              if (clampPosition && windowY + amountY < 0) amountY = -windowY;
-              height -= amountY;
-              windowY += amountY;
-            }
-            if ((edge & Align.right) != 0) {
-              float amountX = x - lastX - width;
-              if (width + amountX < minWidth) amountX = minWidth - width;
-              if (clampPosition && windowX + width + amountX > stage.getWidth())
-                amountX = stage.getWidth() - windowX - width;
-              width += amountX;
-            }
-            if ((edge & Align.top) != 0) {
-              float amountY = y - lastY - height;
-              if (height + amountY < minHeight) amountY = minHeight - height;
-              if (clampPosition && windowY + height + amountY > stage.getHeight())
-                amountY = stage.getHeight() - windowY - height;
-              height += amountY;
-            }
-            setBounds(
-                Math.round(windowX), Math.round(windowY), Math.round(width), Math.round(height));
-          }
+                if (!dragging) return;
+                float width = getWidth(), height = getHeight();
+                float windowX = getX(), windowY = getY();
+          
+                float minWidth = getMinWidth(), maxWidth = getMaxWidth();
+                float minHeight = getMinHeight(), maxHeight = getMaxHeight();
+                Stage stage = getStage();
+                boolean clampPosition =
+                    keepWithinStage && stage != null && getParent() == stage.getRoot();
+          
+                if ((edge & MOVE) != 0) {
+                  float amountX = x - startX, amountY = y - startY;
+                  windowX += amountX;
+                  windowY += amountY;
+                }
+                if ((edge & Align.left) != 0) {
+                  float amountX = x - startX;
+                  if (width - amountX < minWidth) amountX = -(minWidth - width);
+                  if (clampPosition && windowX + amountX < 0) amountX = -windowX;
+                  width -= amountX;
+                  windowX += amountX;
+                }
+                if ((edge & Align.bottom) != 0) {
+                  float amountY = y - startY;
+                  if (height - amountY < minHeight) amountY = -(minHeight - height);
+                  if (clampPosition && windowY + amountY < 0) amountY = -windowY;
+                  height -= amountY;
+                  windowY += amountY;
+                }
+                if ((edge & Align.right) != 0) {
+                  float amountX = x - lastX - width;
+                  if (width + amountX < minWidth) amountX = minWidth - width;
+                  if (clampPosition && stage != null && windowX + width + amountX > stage.getWidth())
+                    amountX = stage.getWidth() - windowX - width;
+                  width += amountX;
+                }
+                if ((edge & Align.top) != 0) {
+                  float amountY = y - lastY - height;
+                  if (height + amountY < minHeight) amountY = minHeight - height;
+                  if (clampPosition && stage != null && windowY + height + amountY > stage.getHeight())
+                    amountY = stage.getHeight() - windowY - height;
+                  height += amountY;
+                }
+                setBounds(
+                    Math.round(windowX), Math.round(windowY), Math.round(width), Math.round(height));
+              }
 
           public boolean mouseMoved(InputEvent event, float x, float y) {
             updateEdge(x, y);
