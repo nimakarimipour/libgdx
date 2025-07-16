79a41b274e8f449cb81166a35ee8c2e79d1f0f06
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 12:10:57 -0700

    before:272to271 - Type='PASS_NULLABLE', message='passing @Nullable parameter 'bindPose.keys[j].id' where @NonNull is required' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java:360           bindPose.keys[j] = getNode(bindPose.keys[j].id);

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -358,23 +358,23 @@ public class ModelInstance implements RenderableProvider {
    * array.
    */
   private void invalidate(Node node) {
-    for (int i = 0, n = node.parts.size; i < n; ++i) {
-      NodePart part = node.parts.get(i);
-      ArrayMap<Node, Matrix4> bindPose = part.invBoneBindTransforms;
-      if (bindPose != null) {
-        for (int j = 0; j < bindPose.size; ++j) {
-          bindPose.keys[j] = getNode(bindPose.keys[j].id);
+      for (int i = 0, n = node.parts.size; i < n; ++i) {
+        NodePart part = node.parts.get(i);
+        ArrayMap<Node, Matrix4> bindPose = part.invBoneBindTransforms;
+        if (bindPose != null) {
+          for (int j = 0; j < bindPose.size; ++j) {
+            bindPose.keys[j] = getNode(Nullability.castToNonnull(bindPose.keys[j].id));
+          }
+        }
+        if (!materials.contains(part.material, true)) {
+          final int midx = materials.indexOf(part.material, false);
+          if (midx < 0) materials.add(part.material = part.material.copy());
+          else part.material = materials.get(midx);
         }
       }
-      if (!materials.contains(part.material, true)) {
-        final int midx = materials.indexOf(part.material, false);
-        if (midx < 0) materials.add(part.material = part.material.copy());
-        else part.material = materials.get(midx);
+      for (int i = 0, n = node.getChildCount(); i < n; ++i) {
+        invalidate(node.getChild(i));
       }
-    }
-    for (int i = 0, n = node.getChildCount(); i < n; ++i) {
-      invalidate(node.getChild(i));
-    }
   }
 
   /**
