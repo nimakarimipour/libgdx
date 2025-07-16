c60dfbd4dfee0851061d4ed0a65c59d2d5ee16b1
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:29:09 -0700

    Revert "fix: 242 - FIELD_NO_INIT - public E value; - @NonNull field SortedIntList$Node.value not initialized"
    This reverts commit cac0c70019a31b021b04885fdef576ed488c0afb.

--- gdx/src/com/badlogic/gdx/graphics/g3d/decals/DecalBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/decals/DecalBatch.java
@@ -27,7 +27,6 @@ import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.SortedIntList;
 import com.uber.nullaway.annotations.Initializer;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -169,9 +168,9 @@ public class DecalBatch implements Disposable {
   protected void render() {
     groupStrategy.beforeGroups();
     for (SortedIntList.Node<Array<Decal>> group : groupList) {
-      groupStrategy.beforeGroup(group.index, Nullability.castToNonnull(group.value));
+      groupStrategy.beforeGroup(group.index, group.value);
       ShaderProgram shader = groupStrategy.getGroupShader(group.index);
-      render(shader, Nullability.castToNonnull(group.value));
+      render(shader, group.value);
       groupStrategy.afterGroup(group.index);
     }
     groupStrategy.afterGroups();
