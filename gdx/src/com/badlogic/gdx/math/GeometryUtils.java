b63d68b14a813a59e96b357d0860333edf550469
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:50:45 -0700

    Revert "fix: 302 - RETURN_NULLABLE - if (!dirty) return worldVertices; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 478ad3bd2e4f5634444cd4a1f45cb787ac155ddb.

--- gdx/src/com/badlogic/gdx/math/GeometryUtils.java
+++ gdx/src/com/badlogic/gdx/math/GeometryUtils.java
@@ -16,8 +16,6 @@
 
 package com.badlogic.gdx.math;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 /**
  * @author Nathan Sweet
  */
@@ -226,8 +224,7 @@ public final class GeometryUtils {
 
     float area = 0, x = 0, y = 0;
     int last = offset + count - 2;
-    float x1 = Nullability.castToNonnull(polygon)[last],
-        y1 = Nullability.castToNonnull(polygon)[last + 1];
+    float x1 = polygon[last], y1 = polygon[last + 1];
     for (int i = offset; i <= last; i += 2) {
       float x2 = polygon[i], y2 = polygon[i + 1];
       float a = x1 * y2 - x2 * y1;
@@ -252,8 +249,7 @@ public final class GeometryUtils {
   public static float polygonArea(float[] polygon, int offset, int count) {
     float area = 0;
     int last = offset + count - 2;
-    float x1 = Nullability.castToNonnull(polygon)[last],
-        y1 = Nullability.castToNonnull(polygon)[last + 1];
+    float x1 = polygon[last], y1 = polygon[last + 1];
     for (int i = offset; i <= last; i += 2) {
       float x2 = polygon[i], y2 = polygon[i + 1];
       area += x1 * y2 - x2 * y1;
--- gdx/src/com/badlogic/gdx/math/Polygon.java
+++ gdx/src/com/badlogic/gdx/math/Polygon.java
@@ -16,7 +16,6 @@
 
 package com.badlogic.gdx.math;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -64,7 +63,6 @@ public class Polygon implements Shape2D {
    *
    * @return vertices scaled, rotated, and offset by the polygon position.
    */
-  @Nullable
   public float[] getTransformedVertices() {
     if (!dirty) return worldVertices;
     dirty = false;
@@ -195,8 +193,7 @@ public class Polygon implements Shape2D {
   /** Returns the area contained within the polygon. */
   public float area() {
     float[] vertices = getTransformedVertices();
-    return GeometryUtils.polygonArea(
-        Nullability.castToNonnull(vertices, "ensures non-null"), 0, vertices.length);
+    return GeometryUtils.polygonArea(vertices, 0, vertices.length);
   }
 
   public int getVertexCount() {
@@ -209,19 +206,13 @@ public class Polygon implements Shape2D {
   public Vector2 getVertex(int vertexNum, Vector2 pos) {
     if (vertexNum < 0 || vertexNum > getVertexCount())
       throw new IllegalArgumentException("the vertex " + vertexNum + " doesn't exist");
-    float[] vertices =
-        Nullability.castToNonnull(
-            this.getTransformedVertices(), "always initialized before return");
+    float[] vertices = this.getTransformedVertices();
     return pos.set(vertices[2 * vertexNum], vertices[2 * vertexNum + 1]);
   }
 
   public Vector2 getCentroid(Vector2 centroid) {
     float[] vertices = getTransformedVertices();
-    return GeometryUtils.polygonCentroid(
-        Nullability.castToNonnull(vertices, "always initialized before use"),
-        0,
-        vertices.length,
-        centroid);
+    return GeometryUtils.polygonCentroid(vertices, 0, vertices.length, centroid);
   }
 
   /**
@@ -235,7 +226,7 @@ public class Polygon implements Shape2D {
   public Rectangle getBoundingRectangle() {
     float[] vertices = getTransformedVertices();
 
-    float minX = Nullability.castToNonnull(vertices, "always initialized if null")[0];
+    float minX = vertices[0];
     float minY = vertices[1];
     float maxX = vertices[0];
     float maxY = vertices[1];
@@ -261,8 +252,7 @@ public class Polygon implements Shape2D {
   @Override
   public boolean contains(float x, float y) {
     final float[] vertices = getTransformedVertices();
-    final int numFloats =
-        Nullability.castToNonnull(vertices, "always initialized before used").length;
+    final int numFloats = vertices.length;
     int intersects = 0;
 
     for (int i = 0; i < numFloats; i += 2) {
