fc46f3f1e4f46ff990b8c47e7c0732819c0131bd
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:09:56 -0700

    Revert "fix: 43 - DEREFERENCE_NULLABLE - return managedCubemaps.get(Gdx.app).size; - dereferenced expression managedCubemaps.get(Gdx.app) is @Nullable"
    This reverts commit a1a438b95e4e25b6c0fb1958effbd2a73ff3dd42.

--- gdx/src/com/badlogic/gdx/graphics/Cubemap.java
+++ gdx/src/com/badlogic/gdx/graphics/Cubemap.java
@@ -29,7 +29,6 @@ import com.badlogic.gdx.graphics.glutils.PixmapTextureData;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.GdxRuntimeException;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Nullable;
@@ -366,6 +365,6 @@ public class Cubemap extends GLTexture {
    * @return the number of managed cubemaps currently loaded
    */
   public static int getNumManagedCubemaps() {
-    return Nullability.castToNonnull(managedCubemaps.get(Gdx.app)).size;
+    return managedCubemaps.get(Gdx.app).size;
   }
 }
