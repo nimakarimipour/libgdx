1cc9b17dd221139e5e8f7f2491685e1436161d94
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 08:39:40 -0700

    before:379to378 - Type='FIELD_NO_INIT', message='@NonNull field Encoder$LiteralEncoder.m_Coders not initialized' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/compression/lzma/Encoder.java:136     Encoder2[] m_Coders;

--- gdx/src/com/badlogic/gdx/utils/compression/lzma/Encoder.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lzma/Encoder.java
@@ -134,7 +134,7 @@ public class Encoder {
       }
     }
 
-    Encoder2[] m_Coders;
+    @Nullable Encoder2[] m_Coders;
     int m_NumPrevBits;
     int m_NumPosBits;
     int m_PosMask;
@@ -150,13 +150,18 @@ public class Encoder {
     }
 
     public void Init() {
-      int numStates = 1 << (m_NumPrevBits + m_NumPosBits);
-      for (int i = 0; i < numStates; i++) m_Coders[i].Init();
-    }
+          if (m_Coders == null) {
+              throw new IllegalStateException("m_Coders is not initialized.");
+          }
+          int numStates = 1 << (m_NumPrevBits + m_NumPosBits);
+          for (int i = 0; i < numStates; i++) m_Coders[i].Init();
+      }
 
     public Encoder2 GetSubCoder(int pos, byte prevByte) {
-      return m_Coders[
-          ((pos & m_PosMask) << m_NumPrevBits) + ((prevByte & 0xFF) >>> (8 - m_NumPrevBits))];
+              if (m_Coders == null) {
+                  throw new NullPointerException("m_Coders is not initialized");
+              }
+              return Nullability.castToNonnull(m_Coders, "checked for null")[((pos & m_PosMask) << m_NumPrevBits) + ((prevByte & 0xFF) >>> (8 - m_NumPrevBits))];
     }
   }
 
