dafa71e059861bfbd05a3689a9128c3b0b8ec51c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:10:25 -0700

    Revert "fix: 45 - PASS_NULLABLE - atlas.getTextures().add(page.texture); - passing @Nullable parameter 'page.texture' where @NonNull is required"
    This reverts commit 0b419f813f85e2518aaaf805bd6edd0fdc04ace6.

--- gdx/src/com/badlogic/gdx/graphics/g2d/PixmapPacker.java
+++ gdx/src/com/badlogic/gdx/graphics/g2d/PixmapPacker.java
@@ -30,7 +30,6 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.OrderedMap;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.regex.Matcher;
@@ -485,7 +484,7 @@ public class PixmapPacker implements Disposable {
           atlas.getRegions().add(region);
         }
         page.addedRects.clear();
-        atlas.getTextures().add(Nullability.castToNonnull(page.texture));
+        atlas.getTextures().add(page.texture);
       }
     }
   }
