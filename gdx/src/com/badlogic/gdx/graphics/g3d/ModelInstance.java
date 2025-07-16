a246a204acc26b5d7d05139a52c69da5b1205953
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:13:57 -0700

    Revert "fix: 366 - PASS_NULLABLE - this(model, transform, (String[]) null); - passing @Nullable parameter '(String[]) null' where @NonNull is required"
    This reverts commit 371a307d096a08329a3c77020ae558078f5e4fc6.

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -28,7 +28,6 @@ import com.badlogic.gdx.math.collision.BoundingBox;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ArrayMap;
 import com.badlogic.gdx.utils.Pool;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -284,7 +283,7 @@ public class ModelInstance implements RenderableProvider {
 
   /** Constructs a new ModelInstance with the specified transform. */
   public ModelInstance(final Model model, Matrix4 transform) {
-    this(model, transform, Nullability.castToNonnull((String[]) null));
+    this(model, transform, (String[]) null);
   }
 
   /** Constructs a new ModelInstance which is an copy of the specified ModelInstance. */
