42d5903416f9de2ccc0a8910e7af481945017f93
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:49:52 -0700

    fix: 439 - DEREFERENCE_NULLABLE - vertices[offset + CPU_POSITION_OFFSET] = - dereferenced expression vertices is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/PointSpriteParticleBatch.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/batches/PointSpriteParticleBatch.java
@@ -149,6 +149,9 @@ public class PointSpriteParticleBatch
 
   @Override
   protected void flush(int[] offsets) {
+    if (vertices == null) {
+      throw new IllegalStateException("Vertices array is not initialized.");
+    }
     int tp = 0;
     for (PointSpriteControllerRenderData data : renderData) {
       FloatChannel scaleChannel = data.scaleChannel;
@@ -157,9 +160,16 @@ public class PointSpriteParticleBatch
       FloatChannel colorChannel = data.colorChannel;
       FloatChannel rotationChannel = data.rotationChannel;
 
+      if (rotationChannel == null
+          || scaleChannel == null
+          || positionChannel == null
+          || colorChannel == null) {
+        continue; // Skip this data if any required channel is null
+      }
+
       for (int p = 0; p < data.controller.particles.size; ++p, ++tp) {
         int offset = offsets[tp] * CPU_VERTEX_SIZE;
-        int regionOffset = p * regionChannel.strideSize;
+        int regionOffset = regionChannel != null ? p * regionChannel.strideSize : 0;
         int positionOffset = p * positionChannel.strideSize;
         int colorOffset = p * colorChannel.strideSize;
         int rotationOffset = p * rotationChannel.strideSize;
@@ -184,14 +194,16 @@ public class PointSpriteParticleBatch
             rotationChannel.data[rotationOffset + ParticleChannels.CosineOffset];
         vertices[offset + CPU_SIZE_AND_ROTATION_OFFSET + 2] =
             rotationChannel.data[rotationOffset + ParticleChannels.SineOffset];
-        vertices[offset + CPU_REGION_OFFSET] =
-            regionChannel.data[regionOffset + ParticleChannels.UOffset];
-        vertices[offset + CPU_REGION_OFFSET + 1] =
-            regionChannel.data[regionOffset + ParticleChannels.VOffset];
-        vertices[offset + CPU_REGION_OFFSET + 2] =
-            regionChannel.data[regionOffset + ParticleChannels.U2Offset];
-        vertices[offset + CPU_REGION_OFFSET + 3] =
-            regionChannel.data[regionOffset + ParticleChannels.V2Offset];
+        if (regionChannel != null) {
+          vertices[offset + CPU_REGION_OFFSET] =
+              regionChannel.data[regionOffset + ParticleChannels.UOffset];
+          vertices[offset + CPU_REGION_OFFSET + 1] =
+              regionChannel.data[regionOffset + ParticleChannels.VOffset];
+          vertices[offset + CPU_REGION_OFFSET + 2] =
+              regionChannel.data[regionOffset + ParticleChannels.U2Offset];
+          vertices[offset + CPU_REGION_OFFSET + 3] =
+              regionChannel.data[regionOffset + ParticleChannels.V2Offset];
+        }
       }
     }
 
