0dadd169eaf6ba82df866657fe5c13bf5a60d1da
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 18:19:43 -0700

    before:188to187 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression Stream is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/compression/rangecoder/Encoder.java:68         Stream.write(temp + LowHi);

--- gdx/src/com/badlogic/gdx/utils/compression/rangecoder/Encoder.java
+++ gdx/src/com/badlogic/gdx/utils/compression/rangecoder/Encoder.java
@@ -62,19 +62,22 @@ public class Encoder {
     }
 
   public void ShiftLow() throws IOException {
-    int LowHi = (int) (Low >>> 32);
-    if (LowHi != 0 || Low < 0xFF000000L) {
-      _position += _cacheSize;
-      int temp = _cache;
-      do {
-        Stream.write(temp + LowHi);
-        temp = 0xFF;
-      } while (--_cacheSize != 0);
-      _cache = (((int) Low) >>> 24);
+      if (Stream == null) {
+        throw new IOException("Stream is null");
+      }
+      int LowHi = (int) (Low >>> 32);
+      if (LowHi != 0 || Low < 0xFF000000L) {
+        _position += _cacheSize;
+        int temp = _cache;
+        do {
+          Stream.write(temp + LowHi);
+          temp = 0xFF;
+        } while (--_cacheSize != 0);
+        _cache = (((int) Low) >>> 24);
+      }
+      _cacheSize++;
+      Low = (Low & 0xFFFFFF) << 8;
     }
-    _cacheSize++;
-    Low = (Low & 0xFFFFFF) << 8;
-  }
 
   public void EncodeDirectBits(int v, int numTotalBits) throws IOException {
     for (int i = numTotalBits - 1; i >= 0; i--) {
