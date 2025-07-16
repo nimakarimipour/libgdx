aa324e48b399d48918d5a5224b39f21160cc895a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 13:12:17 -0700

    before:189to188 - Type='PASS_NULLABLE', message='passing @Nullable parameter '(String[]) null' where @NonNull is required' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java:286     this(model, transform, (String[]) null);

--- gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/ModelInstance.java
@@ -289,8 +289,8 @@ public class ModelInstance implements RenderableProvider {
 
   /** Constructs a new ModelInstance with the specified transform. */
   public ModelInstance(final Model model, Matrix4 transform) {
-    this(model, transform, (String[]) null);
-  }
+        this(model, transform, Nullability.castToNonnull((String[]) null));
+    }
 
   /** Constructs a new ModelInstance which is an copy of the specified ModelInstance. */
   public ModelInstance(ModelInstance copyFrom) {
