1842113824be51d1aaf2717a8ae0bf62b82c5e2c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:09:29 -0700

    Revert "fix: 178 - DEREFERENCE_NULLABLE - controllerChannel.data[i].draw(); - dereferenced expression controllerChannel is @Nullable"
    This reverts commit b76c1deb488eb170eb7212798feb84754db5683d.

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerControllerRenderer.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/renderers/ParticleControllerControllerRenderer.java
@@ -44,10 +44,6 @@ public class ParticleControllerControllerRenderer extends ParticleControllerRend
 
   @Override
   public void update() {
-    if (controllerChannel == null) {
-      throw new GdxRuntimeException(
-          "ParticleController channel not initialized. Make sure to call init() before update().");
-    }
     for (int i = 0, c = controller.particles.size; i < c; ++i) {
       controllerChannel.data[i].draw();
     }
