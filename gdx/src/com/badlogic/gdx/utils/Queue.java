7a64a8d11e3bb84b62000c0d684b3da0479fa3be
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:41:39 -0700

    fix: 411 - METHOD_NO_INIT - public QueueIterable(Queue<T> queue, boolean allowRemove) { - initializer method does not guarantee @NonNull fields iterator1 (line 552), iterator2 (line 552) are initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/utils/Queue.java
+++ gdx/src/com/badlogic/gdx/utils/Queue.java
@@ -17,6 +17,7 @@
 package com.badlogic.gdx.utils;
 
 import com.badlogic.gdx.utils.reflect.ArrayReflection;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import javax.annotation.Nullable;
@@ -549,7 +550,7 @@ public class Queue<T> implements Iterable<T> {
   public static class QueueIterable<T> implements Iterable<T> {
     private final Queue<T> queue;
     private final boolean allowRemove;
-    private QueueIterator iterator1, iterator2;
+    @Nullable private QueueIterator iterator1, iterator2;
 
     // java.io.StringWriter lastAcquire = new java.io.StringWriter();
 
@@ -567,22 +568,18 @@ public class Queue<T> implements Iterable<T> {
      */
     public Iterator<T> iterator() {
       if (Collections.allocateIterators) return new QueueIterator(queue, allowRemove);
-      // lastAcquire.getBuffer().setLength(0);
-      // new Throwable().printStackTrace(new java.io.PrintWriter(lastAcquire));
       if (iterator1 == null) {
         iterator1 = new QueueIterator(queue, allowRemove);
         iterator2 = new QueueIterator(queue, allowRemove);
-        // iterator1.iterable = this;
-        // iterator2.iterable = this;
       }
       if (!iterator1.valid) {
         iterator1.index = 0;
         iterator1.valid = true;
-        iterator2.valid = false;
+        Nullability.castToNonnull(iterator2, "initialized together").valid = false;
         return iterator1;
       }
-      iterator2.index = 0;
-      iterator2.valid = true;
+      Nullability.castToNonnull(iterator2, "initialized together").index = 0;
+      Nullability.castToNonnull(iterator2, "initialized together").valid = true;
       iterator1.valid = false;
       return iterator2;
     }
