e7d97204cb1b8f52d568f16f6022191980a4a07d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:18:58 -0700

    Revert "fix: 204 - DEREFERENCE_NULLABLE - event.stop(); - dereferenced expression event is @Nullable"
    This reverts commit 57803cd1bb7b1056e20925729fcebffd3ffcd58d.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/SelectBox.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/SelectBox.java
@@ -43,7 +43,6 @@ import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.ObjectSet;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -543,7 +542,7 @@ public class SelectBox<T> extends Widget implements Disableable {
                   // Fall thru.
                 case Keys.ESCAPE:
                   hide();
-                  Nullability.castToNonnull(event, "expected non-null").stop();
+                  event.stop();
                   return true;
               }
               return false;
