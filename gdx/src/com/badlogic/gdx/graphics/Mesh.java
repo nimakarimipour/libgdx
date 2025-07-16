bd268ca07cef29b7631afd6a36934aad8e87cc57
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:08:00 -0700

    fix: 173 - ASSIGN_FIELD_NULLABLE - attributes = null; - assigning @Nullable expression to @NonNull field

--- gdx/src/com/badlogic/gdx/graphics/Mesh.java
+++ gdx/src/com/badlogic/gdx/graphics/Mesh.java
@@ -119,7 +119,8 @@ public class Mesh implements Disposable {
    * @param attributes the {@link VertexAttributes}. Each vertex attribute defines one property of a
    *     vertex such as position, normal or texture coordinate
    */
-  public Mesh(boolean isStatic, int maxVertices, int maxIndices, VertexAttributes attributes) {
+  public Mesh(
+      boolean isStatic, int maxVertices, int maxIndices, @Nullable VertexAttributes attributes) {
     vertices = makeVertexBuffer(isStatic, maxVertices, attributes);
     indices = new IndexBufferObject(isStatic, maxIndices);
     isVertexArray = false;
