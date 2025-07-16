19c494d107b6fd5f8daec0ffc20801c9b0562004
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 08:19:16 -0700

    before:449to448 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression assetDescriptor is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/assets/AssetManager.java:208     return get(assetDescriptor.fileName, assetDescriptor.type, true);

--- gdx/src/com/badlogic/gdx/assets/AssetManager.java
+++ gdx/src/com/badlogic/gdx/assets/AssetManager.java
@@ -64,6 +64,7 @@ import com.badlogic.gdx.utils.async.AsyncExecutor;
 import com.badlogic.gdx.utils.async.ThreadUtils;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
 import javax.annotation.Nullable;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /**
  * Loads and stores assets like textures, bitmapfonts, tile maps, sounds, music and so on.
@@ -204,8 +205,8 @@ public class AssetManager implements Disposable {
    * @return the asset
    * @throws GdxRuntimeException if the asset is not loaded
    */
-  public synchronized <T> T get(@Nullable AssetDescriptor<T> assetDescriptor) {
-    return get(assetDescriptor.fileName, assetDescriptor.type, true);
+  public synchronized <T> T get( @Nullable AssetDescriptor<T> assetDescriptor) {
+      return get(Nullability.castToNonnull(assetDescriptor).fileName, Nullability.castToNonnull(assetDescriptor).type, true);
   }
 
   /**
