f05333797746e98fdc7db01553aafbf5fd962e2a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:29:43 -0700

    fix: 245 - FIELD_NO_INIT - Decoder2[] m_Coders; - @NonNull field Decoder$LiteralDecoder.m_Coders not initialized

--- gdx/src/com/badlogic/gdx/utils/compression/lzma/Decoder.java
+++ gdx/src/com/badlogic/gdx/utils/compression/lzma/Decoder.java
@@ -18,7 +18,9 @@ package com.badlogic.gdx.utils.compression.lzma;
 
 import com.badlogic.gdx.utils.compression.lz.OutWindow;
 import com.badlogic.gdx.utils.compression.rangecoder.BitTreeDecoder;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.IOException;
+import javax.annotation.Nullable;
 
 public class Decoder {
   class LenDecoder {
@@ -92,7 +94,7 @@ public class Decoder {
       }
     }
 
-    Decoder2[] m_Coders;
+    @Nullable Decoder2[] m_Coders;
     int m_NumPrevBits;
     int m_NumPosBits;
     int m_PosMask;
@@ -108,12 +110,18 @@ public class Decoder {
     }
 
     public void Init() {
+      if (m_Coders == null) {
+        throw new IllegalStateException("m_Coders is not initialized");
+      }
       int numStates = 1 << (m_NumPrevBits + m_NumPosBits);
       for (int i = 0; i < numStates; i++) m_Coders[i].Init();
     }
 
     Decoder2 GetDecoder(int pos, byte prevByte) {
-      return m_Coders[
+      if (m_Coders == null) {
+        throw new NullPointerException("m_Coders is null");
+      }
+      return Nullability.castToNonnull(m_Coders, "check ensures non-null")[
           ((pos & m_PosMask) << m_NumPrevBits) + ((prevByte & 0xFF) >>> (8 - m_NumPrevBits))];
     }
   }
