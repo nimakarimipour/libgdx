75d0a9d14d2e7ba8047bdec952bd52a15532b9be
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 00:29:19 -0700

    fix: 87 - METHOD_NO_INIT - public SaveData() { - initializer method does not guarantee @NonNull field resources (line 68) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- gdx/src/com/badlogic/gdx/graphics/g3d/particles/ResourceData.java
+++ gdx/src/com/badlogic/gdx/graphics/g3d/particles/ResourceData.java
@@ -65,7 +65,7 @@ public class ResourceData<T> implements Json.Serializable {
     ObjectMap<String, Object> data;
     IntArray assets;
     private int loadIndex;
-    protected ResourceData resources;
+    @Nullable protected ResourceData resources;
 
     public SaveData() {
       data = new ObjectMap<String, Object>();
@@ -81,6 +81,10 @@ public class ResourceData<T> implements Json.Serializable {
     }
 
     public <K> void saveAsset(@Nullable String filename, Class<K> type) {
+      if (resources == null) {
+        throw new IllegalStateException("Resources not initialized");
+      }
+
       int i = resources.getAssetData(filename, type);
       if (i == -1) {
         resources.sharedAssets.add(new AssetData(filename, type));
@@ -96,6 +100,9 @@ public class ResourceData<T> implements Json.Serializable {
     @Nullable
     public AssetDescriptor loadAsset() {
       if (loadIndex == assets.size) return null;
+      if (resources == null) {
+        return null;
+      }
       AssetData data = (AssetData) resources.sharedAssets.get(assets.get(loadIndex++));
       return new AssetDescriptor(data.filename, data.type);
     }
