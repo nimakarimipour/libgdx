799ac72e40ac8e3d9d919568b1804e4edd96d3e0
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 12:57:15 -0700

    before:196to195 - Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/VertexAttributes.java:101     return null;

--- gdx/src/com/badlogic/gdx/graphics/VertexAttributes.java
+++ gdx/src/com/badlogic/gdx/graphics/VertexAttributes.java
@@ -96,7 +96,7 @@ public final class VertexAttributes
    *
    * @param usage The usage of the VertexAttribute to find.
    */
-  public VertexAttribute findByUsage(int usage) {
+  @SuppressWarnings("NullAway") public VertexAttribute findByUsage(int usage) {
     int len = size();
     for (int i = 0; i < len; i++) if (get(i).usage == usage) return get(i);
     return null;
