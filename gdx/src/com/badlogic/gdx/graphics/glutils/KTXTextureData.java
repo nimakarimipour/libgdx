f2f82d4718c3cb8bfbf901b73beb015b7f2f8b7d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:49:16 -0700

    before:268to267 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression compressedData is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/graphics/glutils/KTXTextureData.java:372       int faceLodSize = compressedData.getInt(pos);

--- gdx/src/com/badlogic/gdx/graphics/glutils/KTXTextureData.java
+++ gdx/src/com/badlogic/gdx/graphics/glutils/KTXTextureData.java
@@ -365,29 +365,31 @@ public class KTXTextureData implements TextureData, CubemapData {
     return glInternalFormat;
   }
 
-  @Nullable
-  public ByteBuffer getData(int requestedLevel, int requestedFace) {
-    int pos = imagePos;
-    for (int level = 0; level < numberOfMipmapLevels; level++) {
-      int faceLodSize = compressedData.getInt(pos);
-      int faceLodSizeRounded = (faceLodSize + 3) & ~3;
-      pos += 4;
-      if (level == requestedLevel) {
-        for (int face = 0; face < numberOfFaces; face++) {
-          if (face == requestedFace) {
-            ((Buffer) compressedData).position(pos);
-            ByteBuffer data = compressedData.slice();
-            ((Buffer) data).limit(faceLodSizeRounded);
-            return data;
-          }
-          pos += faceLodSizeRounded;
-        }
-      } else {
-        pos += faceLodSizeRounded * numberOfFaces;
+  @Nullable public ByteBuffer getData(int requestedLevel, int requestedFace) {
+      if (compressedData == null) {
+        throw new NullPointerException("compressedData is null");
       }
+      int pos = imagePos;
+      for (int level = 0; level < numberOfMipmapLevels; level++) {
+        int faceLodSize = compressedData.getInt(pos);
+        int faceLodSizeRounded = (faceLodSize + 3) & ~3;
+        pos += 4;
+        if (level == requestedLevel) {
+          for (int face = 0; face < numberOfFaces; face++) {
+            if (face == requestedFace) {
+              ((Buffer) compressedData).position(pos);
+              ByteBuffer data = compressedData.slice();
+              ((Buffer) data).limit(faceLodSizeRounded);
+              return data;
+            }
+            pos += faceLodSizeRounded;
+          }
+        } else {
+          pos += faceLodSizeRounded * numberOfFaces;
+        }
+      }
+      return null;
     }
-    return null;
-  }
 
   @Override
   public Format getFormat() {
