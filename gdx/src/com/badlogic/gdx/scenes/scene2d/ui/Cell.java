cb1be302aef45d92aaf6ccc357cdfea827ecdde8
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:05:45 -0700

    Revert "fix: 163 - PASS_NULLABLE - return prefHeight.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required"
    This reverts commit 0db295fd4b4d2befa25b239dff7b1e006f35a019.

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.Pool.Poolable;
 import com.uber.nullaway.annotations.Initializer;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -786,7 +785,7 @@ public class Cell<T extends Actor> implements Poolable {
   }
 
   public float getPrefHeight() {
-    return prefHeight.get(Nullability.castToNonnull(actor));
+    return prefHeight.get(actor);
   }
 
   /**
