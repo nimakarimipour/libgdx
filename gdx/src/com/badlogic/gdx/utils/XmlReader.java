e87447bcf279489623b5861604e2cd4e4f4a6250
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-15 17:16:40 -0700

    before:371to370 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression current is @Nullable' /home/nima/Developer/nullness-benchmarks/libgdx/gdx/src/com/badlogic/gdx/utils/XmlReader.java:476     String existing = current.getText();

--- gdx/src/com/badlogic/gdx/utils/XmlReader.java
+++ gdx/src/com/badlogic/gdx/utils/XmlReader.java
@@ -473,9 +473,11 @@ public class XmlReader {
   }
 
   protected void text(@Nullable String text) {
-    String existing = current.getText();
-    current.setText(existing != null ? existing + text : text);
-  }
+      if (current != null) {
+        String existing = current.getText();
+        current.setText(existing != null ? existing + text : text);
+      }
+    }
 
   protected void close() {
     root = elements.pop();
