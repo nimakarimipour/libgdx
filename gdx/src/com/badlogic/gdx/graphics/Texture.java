5a559fe4711b1bbd380c8b6625721b7a926b82db
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:47:30 -0700

    fix: 296 - DEREFERENCE_NULLABLE - return managedTextures.get(Gdx.app).size; - dereferenced expression managedTextures.get(Gdx.app) is @Nullable

--- gdx/src/com/badlogic/gdx/graphics/Texture.java
+++ gdx/src/com/badlogic/gdx/graphics/Texture.java
@@ -28,6 +28,7 @@ import com.badlogic.gdx.graphics.glutils.FileTextureData;
 import com.badlogic.gdx.graphics.glutils.PixmapTextureData;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.GdxRuntimeException;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Nullable;
@@ -365,6 +366,6 @@ public class Texture extends GLTexture {
    * @return the number of managed textures currently loaded
    */
   public static int getNumManagedTextures() {
-    return managedTextures.get(Gdx.app).size;
+    return Nullability.castToNonnull(managedTextures.get(Gdx.app)).size;
   }
 }
