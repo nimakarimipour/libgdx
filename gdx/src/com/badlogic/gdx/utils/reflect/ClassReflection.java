746512963184cce5ad91b56e28fc1cafbe9514cc
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 06:45:05 -0700

    Revert "fix: 424 - PASS_NULLABLE - return c1.isAssignableFrom(c2); - passing @Nullable parameter 'c2' where @NonNull is required"
    This reverts commit c1f4057d9a914ba7821eea2d1f1c2b4fe610a190.

--- gdx/src/com/badlogic/gdx/utils/reflect/ClassReflection.java
+++ gdx/src/com/badlogic/gdx/utils/reflect/ClassReflection.java
@@ -16,7 +16,6 @@
 
 package com.badlogic.gdx.utils.reflect;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.lang.reflect.Modifier;
 import javax.annotation.Nullable;
 
@@ -57,7 +56,7 @@ public final class ClassReflection {
    * Class parameter.
    */
   public static boolean isAssignableFrom(Class c1, @Nullable Class c2) {
-    return c1.isAssignableFrom(Nullability.castToNonnull(c2));
+    return c1.isAssignableFrom(c2);
   }
 
   /** Returns true if the class or interface represented by the supplied Class is a member class. */
