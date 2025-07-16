32e984b2b721acbfc0ba7bf9c4360dee5e704e77
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-16 05:18:33 -0700

    fix: 202 - PASS_NULLABLE - return padTop.get(actor) + padBottom.get(actor); - passing @Nullable parameter 'actor' where @NonNull is required

--- gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
+++ gdx/src/com/badlogic/gdx/scenes/scene2d/ui/Cell.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Null;
 import com.badlogic.gdx.utils.Pool.Poolable;
 import com.uber.nullaway.annotations.Initializer;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import javax.annotation.Nullable;
 
 /**
@@ -905,7 +906,8 @@ public class Cell<T extends Actor> implements Poolable {
 
   /** Returns {@link #getPadTop()} plus {@link #getPadBottom()}. */
   public float getPadY() {
-    return padTop.get(actor) + padBottom.get(actor);
+    return padTop.get(Nullability.castToNonnull(actor))
+        + padBottom.get(Nullability.castToNonnull(actor));
   }
 
   @Nullable
